fab728180471bce3da6791289cc4a7122febaf69
Jonas Jelten <jj@sft.mx>
2013-08-24 04:49:50 +0200

    engine: disabled depth test

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -45,6 +45,10 @@ void init(draw_method_ptr draw_method, input_handler_ptr input_handler) {
 
 	//enable alpha blending
 	glEnable(GL_BLEND);
+
+	//order of drawind relevant for depth
+	glDisable(GL_DEPTH_TEST);
+
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	engine::draw_method = draw_method;
