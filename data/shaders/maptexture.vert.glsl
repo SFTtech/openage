b2c119716e667d8a137095a3a80bf6bc6c75135d
Jonas Jelten <jj@sft.mx>
2013-10-28 22:59:21 +0100

    data/maptexture: expanded ftransform()

--- data/shaders/maptexture.vert.glsl
+++ data/shaders/maptexture.vert.glsl
@@ -1,6 +1,7 @@
 //total basic standard texture mapping vertex shader
+#version 120
 
-void main() {
+void main(void) {
 	gl_TexCoord[0] = gl_MultiTexCoord0;
-	gl_Position = ftransform();
+	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 }
