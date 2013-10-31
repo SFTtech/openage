c27f684e96b1cce0c7bab4a7768dd66511be3622
Jonas Jelten <jj@sft.mx>
2013-11-01 00:26:33 +0100

    main: added test code for rendering texture atlases of grass

--- src/openage.cpp
+++ src/openage.cpp
@@ -40,7 +40,7 @@ void init() {
 	//sync this with media-convert/age2media.py !
 
 	university = new engine::Texture("age/raw/Data/graphics.drs/3836.slp.0.png", true, true);
-	grass = new engine::Texture("age/raw/Data/terrain.drs/15008.slp.0.png", true, true);
+	grass = new engine::Texture("age/raw/Data/terrain.drs/15008.slp.png", true, true);
 
 	char *texturevshader_code = util::read_whole_file("shaders/maptexture.vert.glsl");
 	engine::teamcolor_shader::vert = new engine::shader::Shader(GL_VERTEX_SHADER, texturevshader_code);
@@ -152,16 +152,19 @@ void input_handler(SDL_Event *e) {
 	}
 }
 
+int lol = 0;
 void draw_method() {
 	gaben->draw(0, 0, 0);
 
 	int middle_x = engine::window_x/2;
 	int middle_y = engine::window_y/2;
 
-	grass->draw(middle_x, middle_y, false, 0);
-	grass->draw(middle_x+(97/2), middle_y-(49/2), false, 0);
-	grass->draw(middle_x+(97/2), middle_y+(49/2), false, 0);
-	grass->draw(middle_x+97, middle_y, false, 0);
+	grass->draw(middle_x, middle_y, false, lol);
+	grass->draw(middle_x+(97/2), middle_y-(49/2), false, 1);
+	grass->draw(middle_x+(97/2), middle_y+(49/2), false, 2);
+	grass->draw(middle_x+97, middle_y, false, 3);
+
+	lol = ++lol % grass->get_subtexture_count();
 
 	university->draw(lmbx, lmby, false, 0, 1);
 	university->draw(rmbx, rmby, true, 0, 2);
