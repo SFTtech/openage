f25678eb95b247d7e6b91526473e9caabfe3928f
Jonas Jelten <jj@sft.mx>
2013-11-02 22:54:21 +0100

    shaders: texmapping vert shader now transforming texture coordinates

--- data/shaders/maptexture.vert.glsl
+++ data/shaders/maptexture.vert.glsl
@@ -2,6 +2,7 @@
 #version 120
 
 void main(void) {
-	gl_TexCoord[0] = gl_MultiTexCoord0;
+	gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;
 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	gl_FrontColor = gl_Color;
 }
