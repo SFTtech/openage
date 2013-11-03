6e7cfce2fb309d9a6b81451116586c46dfa6b12f
Jonas Jelten <jj@sft.mx>
2013-11-03 18:53:40 +0100

    engine: better opengl error messages

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -286,8 +286,54 @@ void loop() {
 
 
 		glerrorstate = glGetError();
-		if (glerrorstate != 0) {
-			throw util::Error("OpenGL error state after running draw method: %d", glerrorstate);
+		if (glerrorstate != GL_NO_ERROR) {
+
+			const char *errormsg;
+
+			//generate error message
+			switch (glerrorstate) {
+			case GL_INVALID_ENUM:
+				//An unacceptable value is specified for an enumerated argument.
+				//The offending command is ignored
+				//and has no other side effect than to set the error flag.
+				errormsg = "invalid enum passed to opengl call";
+				break;
+			case GL_INVALID_VALUE:
+				//A numeric argument is out of range.
+				//The offending command is ignored
+				//and has no other side effect than to set the error flag.
+				errormsg = "invalid value passed to opengl call";
+				break;
+			case GL_INVALID_OPERATION:
+				//The specified operation is not allowed in the current state.
+				//The offending command is ignored
+				//and has no other side effect than to set the error flag.
+				errormsg = "invalid operation performed during some state";
+				break;
+			case GL_INVALID_FRAMEBUFFER_OPERATION:
+				//The framebuffer object is not complete. The offending command
+				//is ignored and has no other side effect than to set the error flag.
+				errormsg = "invalid framebuffer operation";
+				break;
+			case GL_OUT_OF_MEMORY:
+				//There is not enough memory left to execute the command.
+				//The state of the GL is undefined,
+				//except for the state of the error flags,
+				//after this error is recorded.
+				errormsg = "out of memory, wtf?";
+				break;
+			case GL_STACK_UNDERFLOW:
+				//An attempt has been made to perform an operation that would
+				//cause an internal stack to underflow.
+				errormsg = "stack underflow";
+				break;
+			case GL_STACK_OVERFLOW:
+				//An attempt has been made to perform an operation that would
+				//cause an internal stack to overflow.
+				errormsg = "stack overflow";
+				break;
+			}
+			throw util::Error("OpenGL error state after running draw method: %d\n\t%s", glerrorstate, errormsg);
 		}
 
 		SDL_GL_SwapWindow(window);
