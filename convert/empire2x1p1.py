42cffade5c4094f733e055eb9fdf0bf2265af582
Frank Schmidt <rubiccuber@googlemail.com>
2013-11-19 14:11:28 +0100

    convert:fixed uncompressing, extracts "header"

--- convert/empire2x1p1.py
+++ convert/empire2x1p1.py
@@ -6,6 +6,13 @@ from struct import Struct, unpack_from
 from util import dbg, file_get_path, file_open
 
 class Empires2X1P1Convert:
+	#version, NumTerRest, NumTer
+	empires_header = Struct("< 8s H H")
+	#TerRestOff:Int[NumTerRest]
+	#TerRestOff2:Int[NumTerRest]
+	#DatTerRest: float[NumTer]TerAccessibles, TerPassGraphic[NumTer]
+	#TerPassGraphic: Buildable, GraphicIdF, GraphicIdS, ReplicationNum
+	terrain_pass_graphics = Struct("< i i i i")
 
 	def __init__(self,fname):
 		self.fname = fname
@@ -17,9 +24,16 @@ class Empires2X1P1Convert:
 
 		content = f.read()
 		print( "length of compressed data: %d" % sys.getsizeof(content) )
-		zlib.decompress( content, -15)
+		content = zlib.decompress( content, -15)
 		print("decompressed data from %s" % fname)
 		print( "length of that data: %d" % sys.getsizeof(content) )
+
+		#do the extracting
+		buf = content[0:Empires2X1P1Convert.empires_header.size]
+		self.empires_header = Empires2X1P1Convert.empires_header.unpack_from(buf)
+		version, terrain_restriction_count, terrain_count = self.empires_header
+		print("version: %s, TerRestCount: %d, TerCount: %d" % (version, terrain_restriction_count, terrain_count))
+
 
 
 if __name__ == "__main__":
