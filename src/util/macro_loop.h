422fb94b9a2078242c5a1262cecae52c159ee402
Michael Ensslin <michael@ensslin.cc>
2013-12-01 02:58:51 +0100

    util: added macro_loop preprocessor utility

---
+++ src/util/macro_loop.h
@@ -0,0 +1,15 @@
+#ifndef __LOOP_H_
+#define __LOOP_H_
+
+#define LOOP_1(MACRO, X) MACRO(X)
+#define LOOP_2(MACRO, X, ...) MACRO(X), LOOP_1(MACRO, __VA_ARGS__)
+#define LOOP_3(MACRO, X, ...) MACRO(X), LOOP_2(MACRO, __VA_ARGS__)
+#define LOOP_4(MACRO, X, ...) MACRO(X), LOOP_3(MACRO, __VA_ARGS__)
+#define LOOP_5(MACRO, X, ...) MACRO(X), LOOP_4(MACRO, __VA_ARGS__)
+
+#define GET_MACRO(_1, _2, _3, _4, _5, NAME, ...) NAME
+#define LOOP(MACRO, ...) GET_MACRO(__VA_ARGS__, \
+	LOOP_5, LOOP_4, LOOP_3, LOOP_2, LOOP_1 \
+	) (MACRO, __VA_ARGS__)
+
+#endif //__LOOP_H_
