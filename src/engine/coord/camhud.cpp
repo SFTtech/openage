f8e19e2c90c5906745cb7af06b8bc27909e538bb
Michael Ensslin <michael@ensslin.cc>
2013-12-06 17:38:04 +0100

    coord: added term coordinate system

--- src/engine/coord/camhud.cpp
+++ src/engine/coord/camhud.cpp
@@ -1,8 +1,11 @@
 #define GEN_IMPL_CAMHUD_CPP
 #include "camhud.h"
 
+#include "term.h"
 #include "window.h"
 #include "../engine.h"
+#include "../console/console.h"
+#include "../util/misc.h"
 
 namespace engine {
 namespace coord {
@@ -14,6 +17,13 @@ window camhud::to_window() {
 window_delta camhud_delta::to_window() {
 	//the direction of the y axis is flipped
 	return window_delta {(pixel_t) x, (pixel_t) -y};
+}
+
+term camhud::to_term() {
+	term result;
+	result.x = util::div<pixel_t>(x - console::bottomleft.x, console::charsize.x);
+	result.y = util::div<pixel_t>(console::topright.y - y, console::charsize.x);
+	return result;
 }
 
 } //namespace coord
