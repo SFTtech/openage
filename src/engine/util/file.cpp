3607652d178657c909c796682b85fe2203c08f0e
Jonas Jelten <jj@sft.mx>
2013-12-12 15:42:45 +0100

    engine/util: implemented generic csv reader function

--- src/engine/util/file.cpp
+++ src/engine/util/file.cpp
@@ -7,37 +7,102 @@
 #include <stdlib.h>
 
 #include "../log.h"
+#include "error.h"
 
 namespace engine {
 namespace util {
 
+
 char *read_whole_file(const char *filename) {
+	return read_whole_file_s(filename).content;
+}
+
+struct file_data read_whole_file_s(const char *filename) {
 
 	//get the file size
 	struct stat st;
 	if (stat(filename, &st) < 0) {
-		log::fatal("failed getting filesize of %s", filename);
-		exit(1);
+		throw Error("failed getting filesize of %s", filename);
 	}
 
 	//open the file
 	FILE *filehandle = fopen(filename, "r");
 	if (filehandle == NULL) {
-		log::fatal("failed opening file %s", filename);
-		exit(1);
+		throw Error("failed opening file %s", filename);
 	}
+
+	size_t content_length = (size_t) st.st_size;
 
 	//read the whole content
-	char *str = new char[st.st_size + 1];
-	if (((unsigned long) st.st_size) != fread(str, 1, st.st_size, filehandle)) {
-		log::fatal("failed reading the file %s", filename);
-		exit(1);
+	char *str = new char[content_length + 1];
+	if (content_length != fread(str, 1, st.st_size, filehandle)) {
+		throw Error("failed reading the file %s", filename);
 	}
 
-	str[(size_t) st.st_size] = '\0';
+	//make sure 0-byte is at the end
+	str[content_length] = '\0';
 
 	//return the file contents
-	return str;
+	struct file_data ret = {content_length, str};
+	return ret;
+}
+
+template <class filestructure>
+ssize_t read_structured_file(util::File<filestructure> *result, const char *fname) {
+	struct file_data file = util::read_whole_file_s(fname);
+
+	char *file_seeker = file.content;
+	char *currentline = file.content;
+
+	size_t line_count;
+	size_t linepos = 0;
+
+	bool wanting_count = true;
+
+	while ((size_t)file_seeker <= ((size_t)file.content + file.size)
+	       && *file_seeker != '\0'
+	       && linepos < line_count) {
+
+		if (*file_seeker == '\n') {
+			*file_seeker = '\0';
+
+			//ignore lines starting with #, that's a comment.
+			if (*currentline != '#') {
+
+				//scan for the entry count definition
+				if (sscanf(currentline, "n=%lu", &line_count) == 1) {
+					wanting_count = false;
+					//allocate memory for entries
+					result->content = new filestructure*[line_count];
+				}
+				else {
+					unsigned int idx;
+					if (wanting_count) {
+						throw Error("meta info line found, but no entry count (n=...) found so far in %s", fname);
+					}
+
+					filestructure *line_data = new filestructure();
+					if (0 == line_data->fill(currentline)) {
+						if (linepos != line_data->idx) {
+							throw Error("line index %u mismatch! should be %u.", idx, linepos);
+						}
+						result->content[idx] = line_data;
+						linepos += 1;
+					}
+					else {
+						throw Error("unknown line content reading texture meta file %s", meta_filename);
+					}
+				}
+			}
+			currentline = file_seeker + 1;
+		}
+
+		file_seeker++;
+	}
+
+	delete[] file_content;
+
+	return line_count;
 }
 
 } //namespace util
--- src/engine/util/file.h
+++ src/engine/util/file.h
@@ -1,10 +1,20 @@
 #ifndef _ENGINE_UTIL_FILE_H_
 #define _ENGINE_UTIL_FILE_H_
 
+#include <stdlib.h>
+
 namespace engine {
 namespace util {
 
+struct file_data {
+	size_t size;
+	char *content;
+};
+
+struct file_data read_whole_file_s(const char *filename);
 char *read_whole_file(const char *filename);
+template <class filestructure> ssize_t read_structured_file(util::File<filestructure> *result, const char *fname);
+
 
 } //namespace util
 } //namespace engine
