37472589d68130cc6dff568fa4265a5c901e2f57
Jonas Jelten <jj@sft.mx>
2013-12-13 16:37:59 +0100

    engine/util: moved templated implementation of file-linereader to header

--- src/engine/util/file.h
+++ src/engine/util/file.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 
+#include "error.h"
+
 namespace engine {
 namespace util {
 
@@ -12,18 +14,21 @@ struct file_data {
 };
 
 struct line_data {
-	int fill(const char *by_line);
+	virtual ~line_data() {};
+
+	//fancy inheritance stuff, =0 means this method is abstract
+	virtual int fill(const char *by_line) =0;
 	unsigned int idx;
 };
 
-template <class filestructure>
+template <class lineformat>
 class File {
 public:
 	File();
 	~File();
 
 	size_t size;
-	filestructure **line;
+	lineformat **content;
 
 	void free_content();
 };
@@ -31,7 +36,85 @@ public:
 struct file_data read_whole_file_s(const char *filename);
 char *read_whole_file(const char *filename);
 
-template <class filestructure> ssize_t read_structured_file(util::File<filestructure> *result, const char *fname);
+template <class lineformat>
+ssize_t read_structured_file(util::File<lineformat> *result, const char *fname);
+
+
+
+template <class lineformat>
+ssize_t read_structured_file(util::File<lineformat> *result, const char *fname) {
+	struct file_data file = util::read_whole_file_s(fname);
+
+	char *file_seeker = file.content;
+	char *currentline = file.content;
+
+	size_t line_count;
+	size_t linepos = 0;
+
+	bool wanting_count = true;
+
+	result->free_content();
+
+	while ((size_t)file_seeker <= ((size_t)file.content + file.size)
+	       && *file_seeker != '\0'
+	       && linepos < line_count) {
+
+		if (*file_seeker == '\n') {
+			*file_seeker = '\0';
+
+			//ignore lines starting with #, that's a comment.
+			if (*currentline != '#') {
+
+				//scan for the entry count definition
+				if (sscanf(currentline, "n=%lu", &line_count) == 1) {
+					wanting_count = false;
+					//allocate memory for entries
+					result->content = new lineformat*[line_count];
+				}
+				else {
+					if (wanting_count) {
+						throw Error("meta info line found, but no entry count (n=...) found so far in %s", fname);
+					}
+
+					auto *line_data = new lineformat{};
+					if (0 == line_data->fill(currentline)) {
+						if (linepos != line_data->idx) {
+							throw Error("line index %u mismatch! should be %lu.", line_data->idx, linepos);
+						}
+						result->content[line_data->idx] = line_data;
+						linepos += 1;
+					}
+					else {
+						throw Error("unknown line content reading meta file %s", fname);
+					}
+				}
+			}
+			currentline = file_seeker + 1;
+		}
+
+		file_seeker++;
+	}
+
+	delete file.content;
+
+	return line_count;
+}
+
+template <class lineformat>
+File<lineformat>::File() : size(0) {
+
+}
+template <class lineformat>
+File<lineformat>::~File() {
+	this->free_content();
+}
+
+template <class lineformat>
+void File<lineformat>::free_content() {
+	for (size_t i = 0; i < this->size; i++) {
+		delete this->content[i];
+	}
+}
 
 
 } //namespace util
