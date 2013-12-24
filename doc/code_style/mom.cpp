686f1dfabb6a28ed86ebf0c0af59ad5ca91bd8f5
Jonas Jelten <jj@sft.mx>
2013-12-24 15:18:43 +0100

    doc: added example for space-tab-mixing

--- doc/code_style/mom.cpp
+++ doc/code_style/mom.cpp
@@ -8,11 +8,51 @@
 #include "lolinclude.h"
 
 
+//use tabs to indent code, if you display that as 4 or 8 or 13.37 doesn't matter
+//use spaces for drawing ascii-art or adapting code positions after indentation
+//
+//=> this ensures custom tab-widths AND correct indentation levels.
+
+class UselessDemonstration {
+public:
+	//example for using tabs for indentation and spaces for adjustment
+	//the argument list is too long, therefore wrap it
+	void lol_useless_function(char *rofl_a_text,
+	                          int asdf,
+	                          bool another_argument,
+	                          char yeah_really_thats_many_arguments,
+	                          int you_wont_believe_me,
+	                          bool fak_u) {
+		if (fak_u) {
+			printf("%s: %d \n", rofl_a_text, asdf);
+		}
+		else if (another_argument) {
+			//the argument list is too long again, so wrap the args
+			//and indent them with spaces.
+			printf("%05d pls... %c %s\n",
+			       you_wont_believe_me,
+			       yeah_really_thats_many_arguments,
+			       rofl_a_text);
+		}
+		else {
+			printf("%s trololo\n", rofl_a_text);
+		}
+	}
+}
+
+//variables in lowercase with _ as word separator,
+//classes are written in camel case
 elts::YourMom *my_special_mom;
 
+
+//a namespace does not increase the indentation level.
 namespace elts {
 
-YourMom::YourMom(int her_mass) : mass(her_mass), number_of_moons(0) {
+//classes are written in camel case (structs are lowercase like variables)
+YourMom::YourMom(int her_mass) :
+	mass(her_mass),
+	number_of_moons(0) {
+	//use this-> for referencing member variables/functions.
 	this->number_of_moons = (mass - (mass % 10))/10;
 }
 
@@ -23,29 +63,33 @@ void YourMom::print_mass() {
 }
 
 bool YourMom::tick() {
-	this->mass += 1; //note that we're not using ++ here
+	this->mass += 1; //we're not using var++
 
+	//note the spaces
+	//|                  |
+	//v                  v
 	if (this->mass < 100) {
 		this->mass *= 2;
 	}
 
 	for (int i = 0; i < 1337; i++) {
 		this->mass += 1;
+		//always use {} for the conditional code block.
 		if (this->mass % 10000 == 0) {
 			break;
 		}
 	}
 
 	switch (this->mass) {
+	//the case label is at the same indentation level as its switch statement:
 	case 0:
 		throw openage::util::Error("mom's mass is %d, impossible.", this->mass);
-
-		//if you don't want to break, write a comment like this:
-		//fall through
+		break;
 
 	case 42:
 		this->mass *= 23;
-		break;
+		//if you don't want to break, write a comment like this:
+		//fall through
 
 	default:
 		//note that we're using printf even though we don't format anything
@@ -64,6 +108,9 @@ bool YourMom::tick() {
 int main(int argc, char **argv) {
 	my_special_mom = new elts::YourMom(1337);
 
+	//no space after function name
+	//                        |
+	//                        v
 	my_special_mom->print_mass();
 
 	for (int i = 0; i < 235; i++) {
