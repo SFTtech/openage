022221e591a513a434ab62d578866f73f0a5d689
Michael Ensslin <michael@ensslin.cc>
2014-10-12 18:16:15 +0200

    testing: added initial support for python tests
    renamed some cpp test methods in the buildsystem
    
    there is no python testing module yet

--- buildsystem/testing.cmake
+++ buildsystem/testing.cmake
@@ -10,16 +10,17 @@ function(init_testing)
 endfunction()
 
 # add a new test function to the list of available tests
-function(add_c_test binary_name name description functionname interactive)
-	list(FIND SFT_TESTS ${name} test_defined)
-	if(NOT test_defined EQUAL -1)
+function(add_test_cpp binary_name name description functionname interactive)
+	get_property(tests GLOBAL PROPERTY SFT_TESTS_CPP)
+	list_contains(contained ${name} tests)
+	if(contained)
 		message(FATAL_ERROR "The test ${name} was already defined!")
 	endif()
 
-	set_property(GLOBAL APPEND PROPERTY SFT_TESTS ${name})
-	set_property(GLOBAL PROPERTY SFT_TESTS_DESCRIPTION_${name} ${description})
-	set_property(GLOBAL PROPERTY SFT_TESTS_FUNCTION_${name} ${functionname})
-	set_property(GLOBAL PROPERTY SFT_TESTS_INTERACTIVE_${name} ${interactive})
+	set_property(GLOBAL APPEND PROPERTY SFT_TESTS_CPP ${name})
+	set_property(GLOBAL PROPERTY SFT_TESTS_CPP_DESCRIPTION_${name} ${description})
+	set_property(GLOBAL PROPERTY SFT_TESTS_CPP_FUNCTION_${name} ${functionname})
+	set_property(GLOBAL PROPERTY SFT_TESTS_CPP_INTERACTIVE_${name} ${interactive})
 
 	if(NOT ${interactive})
 		add_test(NAME ${name}
@@ -28,23 +29,32 @@ function(add_c_test binary_name name description functionname interactive)
 	endif()
 endfunction()
 
-# add a test call to python code
-function(add_py_test)
-	message(ERROR "python tests unimplemented")
+# add a test call to a python function
+# the relative path to the function needs to be given
+# e.g. to call openage.convert.cabextract.tests.testcab(),
+# add the following to py/openage/convert/cabextract/CMakeLists.txt:
+#
+#   add_py_test(tests.testcab "tests cabextract")
+#
+function(add_test_py name description)
+	get_py_module_name(name ${name})
+
+	set_property(GLOBAL APPEND PROPERTY SFT_TESTS_PY ${name})
+	set_property(GLOBAL APPEND PROPERTY SFT_TESTS_PY_DESCRIPTION_${name} ${description})
 endfunction()
 
 # add the gathered information to the given template file for
 # registration, this generates C++ code.
-function(register_test_cases binary_name register_file_in register_file)
+function(register_tests_cpp binary_name register_file_in register_file)
 	set(REGISTER_TESTS "// register all defined C++ tests\n")
 
 	message("tests for ${binary_name} (${register_file})")
 
-	get_property(test_names GLOBAL PROPERTY SFT_TESTS)
+	get_property(test_names GLOBAL PROPERTY SFT_TESTS_CPP)
 	foreach(test ${test_names})
-		get_property(description  GLOBAL PROPERTY SFT_TESTS_DESCRIPTION_${test})
-		get_property(functionname GLOBAL PROPERTY SFT_TESTS_FUNCTION_${test})
-		get_property(interactive  GLOBAL PROPERTY SFT_TESTS_INTERACTIVE_${test})
+		get_property(description  GLOBAL PROPERTY SFT_TESTS_CPP_DESCRIPTION_${test})
+		get_property(functionname GLOBAL PROPERTY SFT_TESTS_CPP_FUNCTION_${test})
+		get_property(interactive  GLOBAL PROPERTY SFT_TESTS_CPP_INTERACTIVE_${test})
 		set(REGISTER_TESTS "${REGISTER_TESTS}
 	register_test(
 		\"${test}\",
@@ -72,6 +82,19 @@ function(register_test_cases binary_name register_file_in register_file)
 
 	# add the configured file to the source list.
 	add_sources(${binary_name} ${CMAKE_CURRENT_BINARY_DIR}/${register_file})
+endfunction()
+
+function(register_tests_py)
+	get_property(test_names GLOBAL PROPERTY SFT_TESTS_PY)
+
+	set(testsfile "${CMAKE_BINARY_DIR}/tests_python")
+	file(WRITE ${testsfile} "")
+
+	foreach(test ${test_names})
+		get_property(description GLOBAL PROPERTY SFT_TESTS_PY_DESCRIPTION_${test})
+
+		file(APPEND ${testsfile} "${test}:${description}\n")
+	endforeach()
 endfunction()
 
 init_testing()
