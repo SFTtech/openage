4c9008af71637a40d1f696ec4a5c78eaf6613ee6
Michael Ensslin <michael@ensslin.cc>
2014-10-13 01:45:41 +0200

    buildsystem: project version now included
    buildsystem/version.cmake now correctly detects version from git describe/openage_version
    py/openage/config.py now contains version

--- buildsystem/version.cmake
+++ buildsystem/version.cmake
@@ -1,11 +1,24 @@
 # provides detect_version, which will store the project version to PROJECT_VERSION.
 
-function(detect_version)
-	execute_process(COMMAND git describe WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}" RESULT_VARIABLE res OUTPUT_VARIABLE out ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
-	if(res EQUAL 0)
-		set(PROJECT_VERSION ${out} PARENT_SCOPE)
-	else()
-		file(STRINGS "${CMAKE_SOURCE_DIR}/version" version_file)
-		set(PROJECT_VERSION ${version_file} PARENT_SCOPE)
+function(init_version)
+	set(res 1)
+	if(IS_DIRECTORY ${CMAKE_SOURCE_DIR}/.git)
+		# if .git exists, try running git describe
+		execute_process(COMMAND git describe WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}" RESULT_VARIABLE res OUTPUT_VARIABLE version ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 	endif()
+
+	if(NOT res EQUAL 0)
+		# if git describe didn't run or failed, read openage_version
+		file(STRINGS "${CMAKE_SOURCE_DIR}/openage_version" version)
+	endif()
+
+	if(NOT version)
+		message(FATAL_ERROR "failed to determine project version")
+	endif()
+
+	message("${PROJECT_NAME} ${version}\n")
+
+	set(PROJECT_VERSION ${version} PARENT_SCOPE)
 endfunction()
+
+init_version()
