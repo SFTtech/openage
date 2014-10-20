b40e724b523899afb4e2cb1b6eaa64d99d253ceb
Michael Ensslin <michael@ensslin.cc>
2014-10-20 14:25:32 +0200

    buildsystem: fixed minor issue with doxygen
    dot executable wouldn't be inserted correctly due to a scoping issue

--- buildsystem/doxygen.cmake
+++ buildsystem/doxygen.cmake
@@ -6,30 +6,30 @@ function(doxygen_init)
 	if(DOXYGEN_FOUND)
 		find_file(DOT dot HINTS /usr/bin/dot)
 
-		set(DOT_EXECUTABLE "/bin/false")
-		set(HAVE_DOT "NO")
-
 		if(NOT ${DOT} STREQUAL "DOT-NOTFOUND")
-			set(DOT_EXECUTABLE "${DOT}")
-			set(HAVE_DOT "YES")
+			set(DOT_EXECUTABLE "${DOT}" PARENT_SCOPE)
+			set(HAVE_DOT "YES" PARENT_SCOPE)
 		else()
+			set(DOT_EXECUTABLE "/bin/false" PARENT_SCOPE)
+			set(HAVE_DOT "NO" PARENT_SCOPE)
 			message(WARNING "graphviz dot couldn't be found, you won't have cool graphs in the docs.")
 		endif()
 	endif()
 
-	# add doc target
-	add_custom_target(doc
-		${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/Doxyfile
-		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-		COMMENT "generating docs (via Doxygen)"
-		VERBATIM
-	)
-
+	set(DOXYGEN_EXECUTABLE ${DOXYGEN_EXECUTABLE} PARENT_SCOPE)
 	set(DOXYGEN_FOUND ${DOXYGEN_FOUND} PARENT_SCOPE)
 endfunction()
 
 function(doxygen_configure)
 	if(DOXYGEN_FOUND)
+		# add doc target
+		add_custom_target(doc
+			${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/Doxyfile
+			WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+			COMMENT "generating docs (via Doxygen)"
+			VERBATIM
+		)
+
 		# create doc folder name list
 		foreach(folder ${ARGN})
 			set(DOXYGEN_SCAN_FOLDERS "${DOXYGEN_SCAN_FOLDERS} ${CMAKE_CURRENT_SOURCE_DIR}/${folder}")
