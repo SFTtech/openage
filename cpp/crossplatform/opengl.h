387e3700ac6470899f0c80fe272a7916e34bc9e3
Michael Ensslin <michael@ensslin.cc>
2014-10-29 00:58:53 +0100

    cpp: added headerguards to crossplatform headers

--- cpp/crossplatform/opengl.h
+++ cpp/crossplatform/opengl.h
@@ -1,5 +1,10 @@
+#ifndef OPENAGE_CROSSPLATFORM_OPENGL_H_
+#define OPENAGE_CROSSPLATFORM_OPENGL_H_
+
 #ifdef __APPLE__
 #  include <OpenGL/gl.h>
 #else
 #  include <GL/gl.h>
+#endif
+
 #endif
--- cpp/crossplatform/pty.h
+++ cpp/crossplatform/pty.h
@@ -1,5 +1,10 @@
+#ifndef OPENAGE_CROSSPLATFORM_PTY_H_
+#define OPENAGE_CROSSPLATFORM_PTY_H_
+
 #ifdef __APPLE__
 #  include <util.h>
 #else
 #  include <pty.h>
+#endif
+
 #endif
