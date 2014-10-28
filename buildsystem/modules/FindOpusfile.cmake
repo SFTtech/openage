b0ec73edf1b74d5594488ce53b54c7561039414c
Jonas Jelten <jj@sft.mx>
2014-10-28 14:28:57 +0100

    buildsystem: prettified findopusfile

--- buildsystem/modules/FindOpusfile.cmake
+++ buildsystem/modules/FindOpusfile.cmake
@@ -1,23 +1,25 @@
-# - Try to find Opusfile
-# Once done this will define
-#  OPUSFILE_FOUND - System has Opusfile
+# Try to find Opusfile
+#
+# Once done this will define:
+#  OPUSFILE_FOUND        - System has Opusfile
 #  OPUSFILE_INCLUDE_DIRS - The Opusfile include directories
-#  OPUSFILE_LIBRARIES - The libraries needed to use Opusfile
+#  OPUSFILE_LIBRARIES    - The libraries needed to use Opusfile
 
 find_package(PkgConfig)
 
 find_path(OPUSFILE_INCLUDE_DIR opusfile.h
-  HINTS
-  /usr/include/opus
-  /usr/local/include/opus
+	HINTS
+	/usr/include/opus
+	/usr/local/include/opus
 )
 
 find_library(OPUSFILE_LIBRARY NAMES opusfile)
 
-set(OPUSFILE_LIBRARIES ${OPUSFILE_LIBRARY} )
-set(OPUSFILE_INCLUDE_DIRS ${OPUSFILE_INCLUDE_DIR} )
+set(OPUSFILE_LIBRARIES "${OPUSFILE_LIBRARY}")
+set(OPUSFILE_INCLUDE_DIRS "${OPUSFILE_INCLUDE_DIR}")
 
 include(FindPackageHandleStandardArgs)
+
 # handle the QUIETLY and REQUIRED arguments and set OPUSFILE_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(Opusfile DEFAULT_MSG
