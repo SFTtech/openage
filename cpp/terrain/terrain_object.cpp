675d7dd85cfd232f99068d96bb9c2f7d74889950
Francisco Demartino <demartino.francisco@gmail.com>
2014-11-07 05:22:27 -0300

    misc: improve building alignment with the foundation

--- cpp/terrain/terrain_object.cpp
+++ cpp/terrain/terrain_object.cpp
@@ -187,27 +187,19 @@ bool TerrainObject::fits(Terrain *terrain, coord::tile pos) {
 
 void TerrainObject::set_position(coord::tile pos) {
 
-	// adjust the start position so that the mouse
-	// click happened more centered
 	this->start_pos     = pos;
-	//this->start_pos.ne -= (this->size.ne - 1) / 2;
-	//this->start_pos.se -= (this->size.se - 1) / 2;
 
 	this->end_pos     = this->start_pos;
 	this->end_pos.ne += this->size.ne;
 	this->end_pos.se += this->size.se;
 
-	// the tile position where the graphic hotspot
-	// will be placed centered.
 	coord::tile drawpos_tile = this->start_pos;
-	coord::tile drawpos_mod2;
-	drawpos_mod2.ne = util::mod<coord::tile_t, 2>(drawpos_tile.ne);
-	drawpos_mod2.se = util::mod<coord::tile_t, 2>(drawpos_tile.se);
 
-	drawpos_tile.ne += ((this->size.ne - drawpos_mod2.ne) / 2);
-	drawpos_tile.se += ((this->size.se - drawpos_mod2.se) / 2);
+	drawpos_tile.ne += this->size.ne / 2;
+	drawpos_tile.se += this->size.se / 2;
 
 	this->draw_pos = drawpos_tile.to_phys2().to_phys3();
+
 }
 
 
