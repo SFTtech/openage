39d9656bf4cdef176c0f0346d9c86c87eed9f1b7
Michael Ensslin <michael@ensslin.cc>
2014-11-16 04:54:43 +0100

    buildsystem: put Inotify detection into module

---
+++ buildsystem/modules/FindInotify.cmake
@@ -0,0 +1,13 @@
+# Copyright 2014-2014 the openage authors. See copying.md for legal info.
+
+# This module defines
+#
+# INOTIFY_INCLUDE_DIR
+# INOTIFY_FOUND
+
+find_path(INOTIFY_INCLUDE_DIR sys/inotify.h HINTS /usr/include/${CMAKE_LIBRARY_ARCHITECTURE})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(INOTIFY DEFAULT_MSG INOTIFY_INCLUDE_DIR)
+
+mark_as_advanced(INOTIFY_INCLUDE_DIR)
