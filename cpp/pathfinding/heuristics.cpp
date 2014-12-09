96a6297e6695439da95730ccd73f37a76c095ad3
Krishna <krishna.nm86@gmail.com>
2014-12-09 12:57:28 +0100

    Introduced curly braces around if statements to satisfy coding conventions

--- cpp/pathfinding/heuristics.cpp
+++ cpp/pathfinding/heuristics.cpp
@@ -20,10 +20,13 @@ cost_t costFunction(const coord::phys3& start, const coord::phys3& end,
 	cost_t dx = start.ne - end.ne;
 	cost_t dy = start.se - end.se;
 	if (chebyshev)
+	{
 		return std::max(dx, dy);
-
+	}
 	if (euclidean)
+	{
 		return std::hypot(dx, dy);
+	}
 
 	//Default is Euclidean. Include another if clause if this function is to be used for a new type of cost calculation with similar body 
 	return std::hypot(dx, dy)
