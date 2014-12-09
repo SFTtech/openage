b748b1a1d381e59fb343cceb75a2c66d649a32b8
Krishna <krishna.nm86@gmail.com>
2014-12-09 10:25:09 +0100

    Source code refactoring, merged methods with redundant functionality, euclidean_cost and chebyshev_cost

--- cpp/pathfinding/heuristics.cpp
+++ cpp/pathfinding/heuristics.cpp
@@ -1,5 +1,3 @@
-// Copyright 2014-2014 the openage authors. See copying.md for legal info.
-
 #include "heuristics.h"
 
 #include <cmath>
@@ -8,22 +6,32 @@
 namespace openage {
 namespace path {
 
-cost_t manhattan_cost(const coord::phys3 &start, const coord::phys3 &end) {
-	cost_t dx = std::abs((cost_t)start.ne - end.ne);
-	cost_t dy = std::abs((cost_t)start.se - end.se);
+cost_t manhattan_cost(const Node &start, const Node &end) {
+	cost_t dx = std::abs((cost_t)start.position.ne - end.position.ne);
+	cost_t dy = std::abs((cost_t)start.position.se - end.position.se);
 	return dx + dy;
 }
 
-cost_t chebyshev_cost(const coord::phys3 &start, const coord::phys3 &end) {
-	cost_t dx = start.ne - end.ne;
-	cost_t dy = start.se - end.se;
-	return std::max(dx, dy);
+#pragma astleft
+
+cost_t costFunction(const Node& start, const Node& end, bool chebyshev,
+		bool euclidean) {
+	cost_t dx = start.position.ne - end.position.ne;
+	cost_t dy = start.position.se - end.position.se;
+	if (chebyshev)
+		return std::max(dx, dy);
+
+	if (euclidean)
+		return std::hypot(dx, dy);
 }
 
-cost_t euclidean_cost(const coord::phys3 &start, const coord::phys3 &end) {
-	cost_t dx = start.ne - end.ne;
-	cost_t dy = start.se - end.se;
-	return std::hypot(dx, dy);
+cost_t chebyshev_cost(const Node &start, const Node &end) {
+	costFunction(start, end, true, false);
+}
+
+#pragma astright
+cost_t euclidean_cost(const Node &start, const Node &end) {
+	costFunction(start, end, false, true);
 }
 
 } // namespace path
