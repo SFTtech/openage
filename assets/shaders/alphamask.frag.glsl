b17b4f8452e23b13800f94738669d84d7bc0f173
Jonas Jelten <jj@sft.mx>
2015-01-08 16:58:19 +0100

    shaders: code style fixes

--- assets/shaders/alphamask.frag.glsl
+++ assets/shaders/alphamask.frag.glsl
@@ -1,25 +1,24 @@
 #version 120
 
-//alpha masking shader
+// alpha masking shader
 //
-//applies an alpha mask texture to a base texture,
-//then draws the masked texture.
+// applies an alpha mask texture to a base texture,
+// then draws the masked texture.
 
-//the base and mask texture, base is the plain terrain tile
+// the base and mask texture, base is the plain terrain tile
 uniform sampler2D base_texture;
 uniform sampler2D mask_texture;
 
-//disable blending and show the mask instead
+// disable blending and show the mask instead
 uniform bool show_mask;
 
-//get those interpolated texture position from vertexshader
+// get those interpolated texture position from vertexshader
 varying vec2 base_tex_position;
 varying vec2 mask_tex_position;
 
 
-void main()
-{
-	//get the texel from the uniform texture.
+void main() {
+	// get the texel from the uniform texture.
 	vec4 base_pixel = texture2D(base_texture, base_tex_position);
 	vec4 mask_pixel = texture2D(mask_texture, mask_tex_position);
 
@@ -27,8 +26,6 @@ void main()
 
 	vec4 blended_pixel = vec4(base_pixel.r, base_pixel.g, base_pixel.b, base_pixel.a - factor);
 
-	//force to pink
-	//blended_pixel = vec4(255.0/255.0, 20.0/255.0, 147.0/255.0, 1.0);
 	if (show_mask) {
 		gl_FragColor = mask_pixel;
 	} else {
