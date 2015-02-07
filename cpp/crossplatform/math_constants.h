97d2ba447f3fb68ded33f05ec3a1eb60865c572b
Francisco Demartino <demartino.francisco@gmail.com>
2015-02-07 20:35:27 -0300

    crossplatform: hardcode the math constants

--- cpp/crossplatform/math_constants.h
+++ cpp/crossplatform/math_constants.h
@@ -3,26 +3,22 @@
 #ifndef OPENAGE_CROSSPLATFORM_MATH_CONSTANTS_H_
 #define OPENAGE_CROSSPLATFORM_MATH_CONSTANTS_H_
 
-// instruct the windows math header to define the constants.
-#define _USE_MATH_DEFINES
-#include <cmath>
-
 namespace openage {
 namespace math {
 
-constexpr double E            = M_E;        //!< e
-constexpr double LOG2E        = M_LOG2E;    //!< log_2 e
-constexpr double LOG10E       = M_LOG10E;   //!< log_10 e
-constexpr double LN2          = M_LN2;      //!< log_e 2
-constexpr double LN10         = M_LN10;     //!< log_e 10
-constexpr double PI           = M_PI;       //!< pi
-constexpr double PI_2         = M_PI_2;     //!< pi/2
-constexpr double PI_4         = M_PI_4;     //!< pi/4
-constexpr double INV_PI       = M_1_PI;     //!< 1/pi
-constexpr double INV2_PI      = M_2_PI;     //!< 2/pi
-constexpr double INV2_SQRT_PI = M_2_SQRTPI; //!< 2/sqrt(pi)
-constexpr double SQRT_2       = M_SQRT2;    //!< sqrt(2)
-constexpr double INV_SQRT_2   = M_SQRT1_2;  //!< 1/sqrt(2)
+constexpr double E            = 2.71828182845904523536;  //!< e
+constexpr double LOG2E        = 1.44269504088896340736;  //!< log_2 e
+constexpr double LOG10E       = 0.434294481903251827651; //!< log_10 e
+constexpr double LN2          = 0.693147180559945309417; //!< log_e 2
+constexpr double LN10         = 2.30258509299404568402;  //!< log_e 10
+constexpr double PI           = 3.14159265358979323846;  //!< pi
+constexpr double PI_2         = 1.57079632679489661923;  //!< pi/2
+constexpr double PI_4         = 0.785398163397448309616; //!< pi/4
+constexpr double INV_PI       = 0.318309886183790671538; //!< 1/pi
+constexpr double INV2_PI      = 0.636619772367581343076; //!< 2/pi
+constexpr double INV2_SQRT_PI = 1.12837916709551257390;  //!< 2/sqrt(pi)
+constexpr double SQRT_2       = 1.41421356237309504880;  //!< sqrt(2)
+constexpr double INV_SQRT_2   = 0.707106781186547524401; //!< 1/sqrt(2)
 
 } // namespace math
 } // namespace openage
