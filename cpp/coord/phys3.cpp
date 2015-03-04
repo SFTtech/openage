55dd28d796bd0f779b222e864598dbd077667482
Francisco Demartino <demartino.francisco@gmail.com>
2015-03-04 17:20:06 -0300

    refactor: fix a bunch of copyright years. sed -ri!
    sed -ri 's/-2014/-2015/' cpp/main.cpp
    sed -ri 's/-2014/-2015/' cpp/handlers.h
    sed -ri 's/-2014/-2015/' cpp/coord/phys3.cpp
    sed -ri 's/-2014/-2015/' cpp/coord/camhud.
    sed -ri 's/-2014/-2015/' cpp/coord/camhud.cpp
    sed -ri 's/-2014/-2015/' cpp/coord/window.cpp
    sed -ri 's/-2014/-2015/' cpp/coord/camgame.cpp
    sed -ri 's/-2014/-2015/' cpp/coord/console.h
    sed -ri 's/-2014/-2015/' cpp/console/console.h
    sed -ri 's/-2014/-2015/' cpp/console/console.cpp
    sed -ri 's/-2014/-2015/' cpp/terrain/terrain_chunk.cpp
    sed -ri 's/-2014/-2015/' cpp/log.cpp

--- cpp/coord/phys3.cpp
+++ cpp/coord/phys3.cpp
@@ -1,4 +1,4 @@
-// Copyright 2013-2014 the openage authors. See copying.md for legal info.
+// Copyright 2013-2015 the openage authors. See copying.md for legal info.
 
 #define GEN_IMPL_PHYS3_CPP
 #include "phys3.h"
