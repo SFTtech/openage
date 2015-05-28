3a47296cd6f156e13435e34cb9be3b90a83fbcdf
TEXIER Lucas <l.texier10@gmail.com>
2015-05-28 14:00:23 +0200

    add test for log::FileSink

--- cpp/log/test.cpp
+++ cpp/log/test.cpp
@@ -1,6 +1,8 @@
 // Copyright 2014-2015 the openage authors. See copying.md for legal info.
 
 #include "log.h"
+#include "file_logsink.h"
+#include "level.h"
 
 #include <iostream>
 #include <iomanip>
@@ -34,8 +36,10 @@ private:
 	}
 };
 
-
-void test() {
+/**
+ * Tests log Messages through the standard output.
+ */
+void std_out_log_sink () {
 	TestLogSource logger;
 	TestLogSink sink{std::cout};
 
@@ -59,6 +63,33 @@ void test() {
 
 	t0.join();
 	t1.join();
+}
+
+/**
+ * Creates a FileSink to also write log Messages
+ * into /tmp/openage-log-test.
+ * Only Messages with a level equals or higher
+ * than level::err will be printed in that file.
+ */
+void file_log_sink () {
+	TestLogSource logger;
+	FileSink filelog("/tmp/openage-log-test", true);
+
+	filelog.loglevel = log::level::err;
+
+	// filelog should ignore these Messages
+	logger.log(MSG(dbg) << "This message should not appear in openage-log-test");
+	logger.log(MSG(info) << "This message should not appear in openage-log-test");
+	logger.log(MSG(warn) << "This message should not appear in openage-log-test");
+
+	// filelog should write these Messages
+	logger.log(MSG(err) << "This message should appear in openage-log-test");
+	logger.log(MSG(crit) << "This message should appear in openage-log-test");
+}
+
+void test() {
+	std_out_log_sink();
+	file_log_sink();
 }
 
 }}} // openage::log::tests
