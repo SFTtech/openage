e996bd7143eaeca2c0ae716911f3791235b483a1
Prashanth Jonnala <prashanth.neo@gmail.com>
2015-08-28 20:29:25 -0400

    buildsystem: refactored project version detection

--- buildsystem/version.cmake
+++ buildsystem/version.cmake
@@ -1,31 +1,5 @@
 # Copyright 2014-2015 the openage authors. See copying.md for legal info.
 
-# provides detect_version, which will store the project version to PROJECT_VERSION.
-
-function(init_version)
-	set(res 1)
-	if(IS_DIRECTORY "${CMAKE_SOURCE_DIR}/.git")
-		# if .git exists, try running git describe
-		execute_process(
-			COMMAND git describe
-			WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-			RESULT_VARIABLE res
-			OUTPUT_VARIABLE version
-			ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
-	endif()
-
-	if(NOT res EQUAL 0)
-		# if git describe didn't run or failed, read openage_version
-		file(STRINGS "${CMAKE_SOURCE_DIR}/openage_version" version)
-	endif()
-
-	if(NOT version)
-		message(FATAL_ERROR "failed to determine project version")
-	endif()
-
-	set(PROJECT_VERSION "${version}" PARENT_SCOPE)
-endfunction()
-
 # logs whether the option NAME is enabled
 # sets WITH_${VARNAME} to HAVE
 # errors if WANT_${VARNAME} conflicts with HAVE
@@ -93,5 +67,3 @@ function(get_config_option_string)
 
 	set(CONFIG_OPTION_STRING "${CONFIG_OPTION_STRING}" PARENT_SCOPE)
 endfunction()
-
-init_version()
