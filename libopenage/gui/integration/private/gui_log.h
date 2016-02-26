3944f8e4d3d47251515e292448b972aa03d7cafd
velorums <velorums@gmail.com>
2016-02-26 01:59:21 +0100

    guilink: qt log integration

---
+++ libopenage/gui/integration/private/gui_log.h
@@ -0,0 +1,12 @@
+// Copyright 2015-2016 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <QtGlobal>
+
+namespace openage {
+namespace gui {
+
+void gui_log(QtMsgType type, const QMessageLogContext &context, const QString &msg);
+
+}} // namespace openage::gui
