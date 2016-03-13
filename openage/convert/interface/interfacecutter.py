01b4832fcff1e64949ad9e425e3191385f2fa7a4
velorums <velorums@gmail.com>
2016-03-13 16:26:42 +0100

    convert: explicit search area for hud cutter

--- openage/convert/interface/interfacecutter.py
+++ openage/convert/interface/interfacecutter.py
@@ -6,6 +6,7 @@ from ..texture import TextureImage
 
 from .interfacehardcoded import (is_ingame_hud_background,
                                  TOP_STRIP_PATTERN_CORNERS,
+                                 TOP_STRIP_PATTERN_SEARCH_AREA_CORNERS,
                                  KNOWN_SUBTEX_CORNER_COORDS)
 
 from .visgrep import visgrep
@@ -24,34 +25,38 @@ class InterfaceCutter(object):
         if is_ingame_hud_background(self.filename):
             pil_image = image.get_pil_image()
 
-            yield self.__cut_top_strip(pil_image)
+            yield self.cut_strip(pil_image,
+                                 TOP_STRIP_PATTERN_CORNERS,
+                                 TOP_STRIP_PATTERN_SEARCH_AREA_CORNERS)
 
             for coords in KNOWN_SUBTEX_CORNER_COORDS:
                 yield TextureImage(pil_image.crop(coords))
         else:
             yield image
 
-    def __cut_top_strip(self, pil_image):
+    def cut_strip(self, pil_image, pattern_corners, search_area_corners):
         """
-        Finds a horisontally tilable piece of the strip that's at the top of the HUD.
+        Finds a horisontally tilable piece of the strip (ex. the top of the HUD).
 
         ||----///////////-------------///////////-------------///////////-------------///////////||
-                  ^TOP_STRIP_PATTERN_CORNERS ^                    ^  where it is found last  ^
+                  ^      pattern_corners     ^                    ^  where it is found last  ^
                   ^           this piece is tileable              ^
 
         so, cut out a subtexture:
                   ///////-------------///////////-------------////
         """
 
-        pattern = pil_image.crop(TOP_STRIP_PATTERN_CORNERS)
-        matches = visgrep(pil_image, pattern, 100000, TOP_STRIP_PATTERN_CORNERS[3])
+        pattern = pil_image.crop(pattern_corners)
+        search_area = pil_image.crop(search_area_corners)
 
-        if len(matches) < 2 or matches[-1].point[1] != TOP_STRIP_PATTERN_CORNERS[1]:
+        matches = visgrep(search_area, pattern, 100000)
+
+        if len(matches) < 2:
             raise Exception("visgrep failed to find repeating pattern (%s)\n" % self.filename)
 
         return TextureImage(pil_image.crop((
-            TOP_STRIP_PATTERN_CORNERS[0],
-            TOP_STRIP_PATTERN_CORNERS[1],
-            matches[-1].point[0],
-            TOP_STRIP_PATTERN_CORNERS[3]
+            pattern_corners[0],
+            pattern_corners[1],
+            search_area_corners[0] + matches[-1].point[0],
+            pattern_corners[3]
         )))
