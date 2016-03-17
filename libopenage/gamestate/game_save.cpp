985475beddf58f955313a6e40db9e5015172c756
Timo Haas <haastimo@gmx.de>
2016-03-17 18:04:10 +0100

    load savegame from json

--- libopenage/gamestate/game_save.cpp
+++ libopenage/gamestate/game_save.cpp
@@ -4,6 +4,7 @@
 
 #include <fstream>
 #include <vector>
+#include <string>
 
 #include "../log/log.h"
 #include "../unit/producer.h"
@@ -16,6 +17,15 @@
 
 namespace openage {
 namespace gameio {
+
+picojson::value save_player(int player_number,openage::GameMain *game) {
+	picojson::object player;
+	player["civilisation"] = picojson::value(game->get_player(player_number)->civ->civ_name );
+	player["player-id"]    = picojson::value( (double) player_number );
+	player["color"]        = picojson::value( (double) game->get_player(player_number)->color );
+
+	return picojson::value(player);
+}
 
 picojson::value save_unit(Unit *unit) {
 	picojson::object unitj;
@@ -31,32 +41,33 @@ picojson::value save_unit(Unit *unit) {
 	picojson::object properties;
 	bool has_building_attr = unit->has_attribute(attr_type::building);
 	if (has_building_attr) {
-	  unitj["isbuilding"] = picojson::value(true);
-	  properties["iscompleted"] = picojson::value(unit->get_attribute<attr_type::building>().completed);
+		unitj["isbuilding"] = picojson::value(true);
+		properties["completion"] = picojson::value(unit->get_attribute<attr_type::building>().completed);
 	} else {
-	  unitj["isbuilding"] = picojson::value(false);
+		unitj["isbuilding"] = picojson::value(false);
 	}
 	unitj["properties"] = picojson::value(properties);
 	return picojson::value(unitj);
 }
 
-void load_unit(std::ifstream &file, openage::GameMain *game) {
-	int pr_id;
-	int player_no;
-	coord::phys_t ne, se;
-	file >> pr_id;
-	file >> player_no;
-	file >> ne;
-	file >> se;
+void load_unit(picojson::object unit, openage::GameMain *game) {
 
+	picojson::value unitv = picojson::value(unit);
+	int pr_id	 = unitv.get("type").get<double>();
+	int player_no	 = unitv.get("player").get<double>();
+	coord::phys_t ne = unitv.get("position-ne").get<double>();
+	coord::phys_t se = unitv.get("position-se").get<double>();
+
+	picojson::object properties = unitv.get("properties").get<picojson::object>();
+
+	// place unit on screen
 	UnitType &saved_type = *game->get_player(player_no)->get_type(pr_id);
 	auto ref = game->placed_units.new_unit(saved_type, game->players[player_no], coord::tile{ne, se}.to_phys2().to_phys3());
 
-	bool has_building_attr;
-	file >> has_building_attr;
+	// construct building
+	bool has_building_attr = unitv.get("isbuilding").get<bool>();
 	if (has_building_attr) {
-		float completed;
-		file >> completed;
+		float completed = (float) picojson::value(properties).get("completion").get<double>();
 		if (completed >= 1.0f && ref.is_valid()) {
 			complete_building(*ref.get());
 		}
@@ -66,15 +77,16 @@ void load_unit(std::ifstream &file, openage::GameMain *game) {
 picojson::value save_tile_content( openage::TileContent *content) {
 	picojson::object tile;
 	tile["terrain-id"] = picojson::value((double) content->terrain_id);
+	//TODO do we need this? see load_tile_content
 	tile["size"]       = picojson::value((double) content->obj.size());
 	return picojson::value(tile);
 }
-TileContent load_tile_content(std::ifstream &file) {
+TileContent load_tile_content(picojson::object tile) {
 	openage::TileContent content;
-	file >> content.terrain_id;
+	content.terrain_id = picojson::value(tile).get("terrain-id").get<double>();
 
-	unsigned int o_size;
-	file >> o_size;
+	//TODO do we need this? see save_tile_content
+	//unsigned int o_size = picojson::value(tile).get("size").get<double>();
 	return content;
 }
 
@@ -83,7 +95,7 @@ void save(openage::GameMain *game, std::string fname) {
 
 	picojson::object savegame;
 	//metadata
-	savegame["save_label"] = picojson::value(save_label);
+	savegame["label"] = picojson::value(save_label);
 	savegame["version"]    = picojson::value(save_version);
 	savegame["build"]      = picojson::value(config::version);
 
@@ -96,24 +108,24 @@ void save(openage::GameMain *game, std::string fname) {
 
 	// loop through chunks
 	for (coord::chunk &position : used) {
-	  openage::TerrainChunk *chunk = game->terrain->get_chunk(position);
-	  picojson::object chunkj;
+		openage::TerrainChunk *chunk = game->terrain->get_chunk(position);
+		picojson::object chunkj;
 
-	  //chunk metadata
-	  chunkj["position-se"] = picojson::value((double) position.se);
-	  chunkj["position-ne"] = picojson::value((double) position.ne);
-	  chunkj["tile-count"] = picojson::value((double) chunk->tile_count);
+		//chunk metadata
+		chunkj["position-se"] = picojson::value((double) position.se);
+		chunkj["position-ne"] = picojson::value((double) position.ne);
+		chunkj["tile-count"]  = picojson::value((double) chunk->tile_count);
 
-	  // saving tiles
-	  picojson::array tiles;
-	  for (size_t p = 0; p < chunk->tile_count; ++p) {
-	    tiles.push_back( save_tile_content( chunk->get_data(p) ));
-	  }
-	  chunkj["tiles"] = picojson::value(tiles);
+		// saving tiles
+		picojson::array tiles;
+		for (size_t p = 0; p < chunk->tile_count; ++p) {
+			tiles.push_back( save_tile_content( chunk->get_data(p) ));
+		}
 
-	  chunks.push_back(picojson::value(chunkj));
+		chunkj["tiles"] = picojson::value(tiles);
+		chunks.push_back(picojson::value(chunkj));
 	}
-	terrain["chunks"] = picojson::value(chunks);
+	terrain["chunks"]   = picojson::value(chunks);
 	savegame["terrain"] = picojson::value(terrain);
 
 	// save units
@@ -123,6 +135,14 @@ void save(openage::GameMain *game, std::string fname) {
 		unitsj.push_back(save_unit(u));
 	}
 	savegame["units"] = picojson::value(unitsj);
+
+	// save player info
+	picojson::array players;
+	int player_count = game->player_count();
+	for(int i=0;i<player_count;i++) {
+		players.push_back( save_player(i,game) );
+	}
+	savegame["players"] = picojson::value(players);
 
 	// save to file
 	std::ofstream file(fname, std::ofstream::out);
@@ -137,42 +157,62 @@ void load(openage::GameMain *game, std::string fname) {
 	}
 	log::log(MSG(dbg) << "loading " + fname);
 
+	//TODO json-schema check
+
+	// to std::string
+	std::istreambuf_iterator<char> first(file);
+	std::istreambuf_iterator<char> last;
+	std::string json_str(first, last);
+
+	// parse json
+	picojson::value savegame;
+	std::string err;
+	picojson::parse(savegame, json_str.begin(), json_str.end(), &err);
+
+	//loading to json
+	//picojson::object &savegame = json.get<picojson::object>();
+
 	// load metadata
-	std::string file_label;
-	file >> file_label;
+	std::string file_label = savegame.get("label").to_str();
 	if (file_label != save_label) {
 		log::log(MSG(warn) << fname << " is not a savefile");
 		return;
 	}
-	std::string version;
-	file >> version;
+	std::string version = savegame.get("version").to_str();
 	if (version != save_version) {
 		log::log(MSG(warn) << "savefile has different version");
 	}
-	std::string build;
-	file >> build;
+	std::string build = savegame.get("build").to_str();
 
 	// read terrain chunks
-	unsigned int num_chunks;
-	file >> num_chunks;
-	for (unsigned int c = 0; c < num_chunks; ++c) {
-		coord::chunk_t ne, se;
-		size_t tile_count;
-		file >> ne;
-		file >> se;
-		file >> tile_count;
+	//picojson::object &terrain = picojson::value(savegame).get("terrain").get<picojson::object>();
+	picojson::object &terrain = savegame.get("terrain").get<picojson::object>();
+
+	picojson::array chunks = picojson::value(terrain).get("chunks").get<picojson::array>();
+	for (picojson::array::iterator iter = chunks.begin(); iter != chunks.end(); ++iter) {
+		picojson::object &chunkjson = (*iter).get<picojson::object>();
+
+		// chunk position
+		coord::chunk_t ne = (int32_t) picojson::value(chunkjson).get("position-ne").get<double>();
+		coord::chunk_t se = (int32_t) picojson::value(chunkjson).get("position-se").get<double>();
 		openage::TerrainChunk *chunk = game->terrain->get_create_chunk(coord::chunk{ne, se});
-		for (size_t p = 0; p < tile_count; ++p) {
-			*chunk->get_data(p) = load_tile_content( file );
+
+		// tiles
+		picojson::array tiles = picojson::value(chunkjson).get("tiles").get<picojson::array>();
+		size_t p = 0;
+		for (picojson::array::iterator iter = tiles.begin(); iter != tiles.end(); ++iter,++p) {
+			*chunk->get_data(p) = load_tile_content( (*iter).get<picojson::object>() );
 		}
+
 	}
 
+	// load units
 	game->placed_units.reset();
-	unsigned int num_units;
-	file >> num_units;
-	for (unsigned int u = 0; u < num_units; ++u) {
-		load_unit( file, game );
+	picojson::array units = picojson::value(savegame).get("units").get<picojson::array>();
+	for (picojson::array::iterator iter = units.begin(); iter != units.end(); ++iter) {
+		load_unit( (*iter).get<picojson::object>(), game );
 	}
+
 }
 
 }} // openage::gameio
