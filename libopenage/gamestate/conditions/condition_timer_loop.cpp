b6271f3e82738d3fdaf7e34e4a040718b1d85fc6
Timo Haas <haastimo@gmx.de>
2016-03-19 20:51:22 +0100

    added name to copying file

--- libopenage/gamestate/conditions/condition_timer_loop.cpp
+++ libopenage/gamestate/conditions/condition_timer_loop.cpp
@@ -12,7 +12,10 @@ namespace openage {
 
 	bool ConditionTimerLoop::check(uint32_t gametime,uint32_t update) {
 		if( gametime - this->last > this->ms ) {
-			this->last += this->ms;
+			// needed if check is not called every tick
+			while(gametime - this->last > this->ms) {
+				this->last += this->ms;
+			}
 			return true;
 		}
 		return false;
