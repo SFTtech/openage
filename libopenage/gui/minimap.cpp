d90e82042db513cca155c44b1c45c298663c7998
velorums <velorums@gmail.com>
2016-05-28 19:36:31 +0200

    guilink: minimap

---
+++ libopenage/gui/minimap.cpp
@@ -0,0 +1,438 @@
+// Copyright 2016-2016 the openage authors. See copying.md for legal info.
+
+#include "minimap.h"
+
+#include <cmath>
+
+#include <QtQml>
+
+// Must be included before the Qt GL headers because of epoxy.
+#include "game_main_link.h"
+
+#include <QSGGeometry>
+#include <QSGGeometryNode>
+#include <QSGSimpleTextureNode>
+
+#include "../coord/tile3.h"
+#include "../coord/camgame.h"
+
+#include "guisys/link/qtsdl_checked_static_cast.h"
+#include "integration/private/gui_texture.h"
+#include "integration/private/gui_texture_from_img_data.h"
+
+#include "engine_link.h"
+
+#include "gui_textured_points_shader.h"
+#include "minimap_background.h"
+#include "minimap_marker_type.h"
+
+namespace openage {
+namespace gui {
+
+namespace {
+const int registration = qmlRegisterType<Minimap>("yay.sfttech.openage", 1, 0, "Minimap");
+}
+
+BackgroundUpdate::BackgroundUpdate(std::tuple<std::unique_ptr<uint32_t[]>, int, int>&& background_tex, std::tuple<coord::chunk, coord::chunk_t> bounding_square)
+	:
+	background_tex{std::move(background_tex)},
+	bounding_square{bounding_square} {
+}
+
+bool BackgroundUpdate::consumed() const {
+	return !std::get<0>(this->background_tex).operator bool();
+}
+
+std::tuple<std::unique_ptr<GuiTexture>, QSizeF, std::tuple<coord::chunk, coord::chunk_t>> BackgroundUpdate::consume() {
+	ENSURE(!this->consumed(), "Trying to create a background texture from the same terrain color data multiple times.");
+
+	return std::tuple_cat(
+		gui_texture_from_img_data(std::move(std::get<0>(this->background_tex)), std::get<1>(this->background_tex), std::get<2>(this->background_tex)),
+		std::tie(this->bounding_square)
+	);
+}
+
+Minimap::Minimap(QQuickItem *parent)
+	:
+	QQuickItem{parent},
+	game{},
+	game_dirty{},
+	terrain_usage_hash{},
+	need_fresh_bg{},
+	marker_size{4},
+	marker_atlas{},
+	marker_atlas_dirty{},
+	marker_atlas_spacing{0.f},
+	marker_atlas_spacing_dirty{} {
+	Q_UNUSED(registration);
+
+	this->setFlag(QQuickItem::ItemHasContents, true);
+}
+
+Minimap::~Minimap() {
+}
+
+namespace {
+std::tuple<std::vector<gamedata::palette_color>, std::vector<gamedata::palette_color>> get_palettes(GameMainLink *game, const std::string &p0, const std::string &p1) {
+	ENSURE(game->current_thread_is_game_logic_thread(), "Palette loading allowed only in the game logic thread.");
+	ENSURE(unwrap(game)->is_game_running(), "Game must be running to access palettes from the correct asset manager.");
+
+	auto spec = unwrap(game)->get_game()->get_spec();
+
+	return std::make_tuple(spec->get_palette(p0), spec->get_palette(p1));
+}
+
+std::tuple<std::string, std::string> get_shaders(GameMainLink *game) {
+	ENSURE(game->current_thread_is_game_logic_thread(), "Shader loading allowed only in the game logic thread.");
+	ENSURE(unwrap(game)->is_game_running(), "Game must be running to access shaders from the correct asset manager.");
+
+	const auto shaders_dir = unwrap(game)->get_game()->get_spec()->get_asset_manager()->get_asset_dir() / "shaders";
+
+	return std::make_tuple(shaders_dir["atlasgrid.vert.glsl"].open().read(), shaders_dir["atlasgrid.frag.glsl"].open().read());
+}
+
+size_t get_terrain_usage_hash(GameMainLink *game) {
+	ENSURE(game->current_thread_is_game_logic_thread(), "Terrain hash calculation is allowed only in the game logic thread.");
+	ENSURE(unwrap(game)->is_game_running(), "Game must be running to do terrain hash calculation.");
+
+	return terrain_used_chunks_hash(*unwrap(game)->get_game()->terrain);
+}
+
+BackgroundUpdate get_background_update(GameMainLink *game, const std::vector<gamedata::palette_color> &palette) {
+	ENSURE(game->current_thread_is_game_logic_thread(), "Creation of the background texture is allowed only in the game logic thread.");
+
+	auto &terrain = unwrap(game)->get_game()->terrain;
+	return BackgroundUpdate{generate_minimap_background(*terrain, palette), terrain_bounding_square(*terrain)};
+}
+
+std::tuple<coord_data, bool> get_engine_coord_data(GameMainLink *game) {
+	ENSURE(game->current_thread_is_game_logic_thread(), "Operations with Engine are allowed only in the game logic thread.");
+
+	if (Engine *engine = unwrap(game->get_engine()))
+		return std::make_tuple(*engine->get_coord_data(), true);
+
+	return std::tuple<coord_data, bool>{};
+}
+
+std::vector<ColoredTexturedPoint2D> get_marker_points(GameMainLink *game, const std::vector<gamedata::minimap_modes> &markers_types, const std::vector<gamedata::palette_color> &player_palette) {
+	ENSURE(game->current_thread_is_game_logic_thread(), "Extracting of the unit positions is allowed only in the game logic thread.");
+	return get_unit_marker_points(unwrap(game)->get_game()->placed_units, markers_types, player_palette);
+}
+
+const QSGGeometry::AttributeSet& defaultAttributes_ColoredTexturedPoint2D()
+{
+	static QSGGeometry::Attribute data[] = {
+		QSGGeometry::Attribute::create(0, 2, GL_FLOAT, true),
+		QSGGeometry::Attribute::create(1, 1, GL_FLOAT),
+		QSGGeometry::Attribute::create(2, 4, GL_UNSIGNED_BYTE),
+		QSGGeometry::Attribute::create(3, 1, GL_UNSIGNED_BYTE)
+	};
+	static QSGGeometry::AttributeSet attrs = {3, sizeof(ColoredTexturedPoint2D), data};
+	return attrs;
+}
+}
+
+std::unique_ptr<QSGNode> Minimap::create_tree() {
+	this->last_bounding_rect = QRectF{};
+	this->need_fresh_bg = true;
+	this->marker_atlas_dirty = true;
+	this->marker_atlas_spacing_dirty = true;
+
+	auto world_to_background_ptr = std::make_unique<QSGTransformNode>();
+	auto world_to_background = world_to_background_ptr.get();
+
+	auto geometry_ptr = std::make_unique<QSGGeometry>(defaultAttributes_ColoredTexturedPoint2D(), 0);
+	geometry_ptr->setDrawingMode(GL_POINTS);
+	auto geometry = geometry_ptr.get();
+
+	auto material_ptr = std::unique_ptr<QSGSimpleMaterial<Icon>>(TexturedPointsShader::createMaterial(this->shaders));
+	glEnable(GL_POINT_SPRITE);
+	material_ptr->setFlag(QSGMaterial::Blending);
+
+	if (!this->hidden_foreground)
+		this->hidden_foreground = std::make_unique<QSGGeometryNode>();
+
+	this->hidden_foreground->setFlag(QSGNode::OwnsGeometry);
+	this->hidden_foreground->setFlag(QSGNode::OwnsMaterial);
+	auto foreground = this->hidden_foreground.get();
+
+	auto turn_ptr = std::make_unique<QSGTransformNode>();
+	auto turn = turn_ptr.get();
+
+	auto background_ptr = std::make_unique<QSGSimpleTextureNode>();
+	background_ptr->setOwnsTexture(true);
+	auto background = background_ptr.get();
+
+	this->update_marker_atlas = [this, world_to_background, foreground, geometry] {
+		if (this->marker_atlas_dirty) {
+			const bool markers_disabled = !this->marker_atlas || !this->marker_atlas->isTextureProvider();
+
+			if (markers_disabled && this->marker_atlas)
+				qWarning() << "MinimapImpl.markerAtlas must have 'layer.enabled' or be a texture provider.";
+
+			if (markers_disabled && world_to_background->childCount()) {
+				ENSURE(world_to_background->firstChild() == foreground, "Scenegraph has a node that we have not attached to it. Expected our foreground node.");
+				world_to_background->removeChildNode(foreground);
+				this->hidden_foreground.reset(foreground);
+			}
+
+			if (!markers_disabled && !world_to_background->childCount())
+				world_to_background->appendChildNode(this->hidden_foreground.release());
+
+			if (!markers_disabled) {
+				qtsdl::checked_static_cast<QSGSimpleMaterial<Icon>*>(foreground->material())->state()->marker_atlas = this->marker_atlas;
+				qtsdl::checked_static_cast<QSGSimpleMaterial<Icon>*>(foreground->material())->state()->columns = std::ceil(std::sqrt(this->markers_types.size()));
+				foreground->markDirty(QSGNode::DirtyMaterial);
+			}
+
+			this->marker_atlas_dirty = false;
+		}
+
+		if (this->marker_atlas_spacing_dirty) {
+			qtsdl::checked_static_cast<QSGSimpleMaterial<Icon>*>(foreground->material())->state()->spacing = this->marker_atlas_spacing;
+			foreground->markDirty(QSGNode::DirtyMaterial);
+
+			this->marker_atlas_spacing_dirty = false;
+		}
+
+		if (this->marker_size > 0 && geometry->lineWidth()) {
+			geometry->setLineWidth(this->marker_size);
+			foreground->markDirty(QSGNode::DirtyGeometry);
+		}
+	};
+
+	this->update_backround = [this, background, world_to_background, turn] (BackgroundUpdate &&bg_update, const coord_data &engine_coord_data) {
+		if (!bg_update.consumed()) {
+			const bool width_bigger = this->boundingRect().width() >= this->boundingRect().height();
+			auto big_side = std::max(this->boundingRect().width(), this->boundingRect().height());
+			auto small_side = std::min(this->boundingRect().width(), this->boundingRect().height());
+
+			auto background_gui_tex = bg_update.consume();
+
+			background->setTexture(std::get<std::unique_ptr<GuiTexture>>(background_gui_tex).release());
+			background->setSourceRect({QPointF{}, std::get<QSizeF>(background_gui_tex)});
+
+			auto terrain_bounding_square_max_side_len = terrain_square_dimensions(std::get<std::tuple<coord::chunk, coord::chunk_t>>(background_gui_tex));
+			coord::phys_t max_terrain_side_len = coord::chunk{terrain_bounding_square_max_side_len, 0}.to_tile({0, 0}).to_phys2({0, 0}).as_relative().ne;
+
+			/**
+			 * Setup transformation from the world coordinates to the coordinates on the minimap background texture.
+			 */
+			{
+				QMatrix4x4 downscale_world;
+				downscale_world.translate(big_side / 2, big_side / 2);
+				downscale_world.scale(big_side / max_terrain_side_len, big_side / max_terrain_side_len);
+				auto terrain_bounding_rect_center_phys = terrain_square_center(std::get<std::tuple<coord::chunk, coord::chunk_t>>(background_gui_tex)).to_tile3().to_phys3();
+				downscale_world.translate(-terrain_bounding_rect_center_phys.ne, -terrain_bounding_rect_center_phys.se);
+				world_to_background->setMatrix(downscale_world);
+			}
+
+			if (this->last_bounding_rect != this->boundingRect()) {
+				QMatrix4x4 trans_to_center(QTransform::fromTranslate(-big_side / 2., -big_side / 2.));
+
+				auto iv = (coord::tile3{1, 0, 0}.to_phys3() - coord::tile3{0, 0, 0}.to_phys3()).to_camgame().to_window();
+				auto jv = (coord::tile3{0, 1, 0}.to_phys3() - coord::tile3{0, 0, 0}.to_phys3()).to_camgame().to_window();
+
+				/**
+				 * Background texture uses square pixels for tiles, so need to undo the tile sizing.
+				 */
+				auto i = QVector2D(iv.x / engine_coord_data.tile_halfsize.x, iv.y / engine_coord_data.tile_halfsize.y);
+				auto j = QVector2D(jv.x / engine_coord_data.tile_halfsize.x, jv.y / engine_coord_data.tile_halfsize.y);
+
+				QMatrix4x4 cam_rotate(
+					i.x(), j.x(), 0.f, 0.f,
+					i.y(), j.y(), 0.f, 0.f,
+					0.f, 0.f, 1.f, 0.f,
+					0.f, 0.f, 0.f, 1.f
+				);
+
+				/**
+				 * Fit a rotated square into the same area.
+				 */
+				auto diagonal = cam_rotate * QVector4D(1.f, 1.f, 0.f, 0.f);
+				auto diagonal_max_coord = std::max(diagonal.x(), diagonal.y());
+				QMatrix4x4 scale_fit(QTransform::fromScale(1. / diagonal_max_coord, 1. / diagonal_max_coord));
+
+				auto ratio = width_bigger ? std::make_tuple(1., small_side / big_side) : std::make_tuple(small_side / big_side, 1.);
+				QMatrix4x4 aspect(QTransform::fromScale(std::get<0>(ratio), std::get<1>(ratio)));
+
+				QMatrix4x4 trans_from_center(QTransform::fromTranslate(this->boundingRect().width() / 2., this->boundingRect().height() / 2.));
+
+				turn->setMatrix(trans_from_center * aspect * scale_fit * cam_rotate * trans_to_center);
+
+				background->setRect(0., 0., big_side, big_side);
+
+				this->last_bounding_rect = this->boundingRect();
+			}
+		}
+
+		ENSURE(background->texture(), "Minimap background texture is undefined.");
+	};
+
+	this->update_view_frame_rect = [this, world_to_background, turn] (const coord_data &engine_coord_data) {
+		auto world_cam_left_top = coord::window{0, 0}.to_camgame().to_phys3();
+		auto world_cam_right_bottom = engine_coord_data.window_size.to_camgame().to_phys3();
+
+		QVector3D cam_frame_lt = turn->matrix() * world_to_background->matrix() * QVector3D(world_cam_left_top.ne, world_cam_left_top.se, 0);
+		QVector3D cam_frame_rb = turn->matrix() * world_to_background->matrix() * QVector3D(world_cam_right_bottom.ne, world_cam_right_bottom.se, 0);
+
+		auto cam_frame_size = cam_frame_rb - cam_frame_lt;
+
+		QRect new_view_frame_rect(cam_frame_lt.x(), cam_frame_lt.y(), cam_frame_size.x(), cam_frame_size.y());
+
+		if (this->view_frame_rect != new_view_frame_rect) {
+			this->view_frame_rect = new_view_frame_rect;
+			emit this->viewFrameRectChanged();
+		}
+	};
+
+	this->update_marker_points = [this, foreground, geometry] (const std::vector<ColoredTexturedPoint2D> &unit_marker_points) {
+		geometry->allocate(unit_marker_points.size());
+		std::copy(std::begin(unit_marker_points), std::end(unit_marker_points), static_cast<ColoredTexturedPoint2D*>(geometry->vertexData()));
+		foreground->markDirty(QSGNode::DirtyGeometry);
+	};
+
+	foreground->setMaterial(material_ptr.release());
+	foreground->setGeometry(geometry_ptr.release());
+	background->appendChildNode(world_to_background_ptr.release());
+	turn->appendChildNode(background_ptr.release());
+
+	return std::move(turn_ptr); // clang workaround
+}
+
+QSGNode* Minimap::updatePaintNode(QSGNode *node, UpdatePaintNodeData*) {
+	std::unique_ptr<QSGNode> root{node};
+
+	if (this->game && this->game->get_state() != GameMainLink::State::Null) {
+		if (root || this->hidden_root || !this->bg_update.consumed()) {
+			if (!root) {
+				if (std::get<0>(this->shaders).empty())
+					emit this->game->in_game_logic_thread_blocking([this] {
+						if (unwrap(this->game)->is_game_running())
+							this->shaders = get_shaders(this->game);
+					});
+
+				/**
+				 * No link to the asset manager yet.
+				 */
+				if (std::get<0>(this->shaders).empty()) {
+					this->update();
+					return root.release();
+				}
+
+				root = this->hidden_root ? std::move(this->hidden_root) : create_tree();
+			}
+
+			if (std::get<bool>(this->engine_coord_data)) {
+				auto& engine_coord_data = std::get<coord_data>(this->engine_coord_data);
+				this->update_backround(std::move(this->bg_update), engine_coord_data);
+				this->update_view_frame_rect(engine_coord_data);
+			}
+
+			this->update_marker_atlas();
+			this->update_marker_points(this->unit_marker_points);
+		} else {
+			this->need_fresh_bg = true;
+		}
+
+		this->polish();
+	} else {
+		if (root)
+			this->hidden_root = std::move(root);
+	}
+
+	this->update();
+
+	return root.release();
+}
+
+void Minimap::updatePolish() {
+	emit this->game->in_game_logic_thread_blocking([this] {
+		if (unwrap(this->game)->is_game_running()) {
+			if (this->game_dirty)
+				std::tie(this->player_palette, this->palette) = get_palettes(this->game, "converted/player_palette.docx", "converted/general_palette.docx");
+
+			auto new_terrain_usage_hash = get_terrain_usage_hash(this->game);
+			if (this->game_dirty || this->need_fresh_bg || this->last_bounding_rect != this->boundingRect() || this->terrain_usage_hash != new_terrain_usage_hash) {
+				this->bg_update = get_background_update(this->game, this->palette);
+				this->terrain_usage_hash = new_terrain_usage_hash;
+			}
+
+			this->engine_coord_data = get_engine_coord_data(this->game);
+			this->unit_marker_points = get_marker_points(this->game, this->markers_types, this->player_palette);
+
+			this->game_dirty = false;
+			this->need_fresh_bg = false;
+		}
+	});
+}
+
+GameMainLink* Minimap::get_game() const {
+	return this->game;
+}
+
+void Minimap::set_game(GameMainLink *game) {
+	if (this->game != game) {
+		this->game = game;
+		this->game_dirty = true;
+	}
+}
+
+QQuickItem* Minimap::get_marker_atlas() const {
+	return this->marker_atlas;
+}
+
+void Minimap::set_marker_atlas(QQuickItem *marker_atlas) {
+	if (this->marker_atlas != marker_atlas) {
+		this->marker_atlas = marker_atlas;
+		this->marker_atlas_dirty = true;
+	}
+}
+
+float Minimap::get_marker_atlas_spacing() const {
+	return this->marker_atlas_spacing;
+}
+
+void Minimap::set_marker_atlas_spacing(float marker_atlas_spacing) {
+	if (this->marker_atlas_spacing != marker_atlas_spacing) {
+		this->marker_atlas_spacing = marker_atlas_spacing;
+		this->marker_atlas_spacing_dirty = true;
+	}
+}
+
+QVariantList Minimap::get_markers_types() const {
+	QVariantList result;
+
+	for (auto &marker_type : this->markers_types)
+		result.push_back(QVariant::fromValue(MinimapMarkerAttachedPropertyProvider::minimap_modes_from_gamedata(marker_type)));
+
+	return result;
+}
+
+void Minimap::set_markers_types(const QVariantList &markers_types) {
+	std::vector<gamedata::minimap_modes> new_markers_types;
+
+	for (auto &marker_type_var : markers_types) {
+		bool ok = false;
+
+		const int marker_type_int = marker_type_var.toInt(&ok);
+
+		if (ok) {
+			gamedata::minimap_modes marker_type;
+			std::tie(marker_type, ok) = MinimapMarkerAttachedPropertyProvider::int_to_minimap_modes_gamedata(marker_type_int);
+
+			if (ok)
+				new_markers_types.push_back(marker_type);
+		}
+
+		if (!ok)
+			qWarning() << "'MinimapImpl.markersTypes' property must be an array of 'MinimapMarker' enum values, but found a" << marker_type_var << "element inside.";
+	}
+
+	if (this->markers_types != new_markers_types) {
+		this->markers_types = new_markers_types;
+		this->marker_atlas_dirty = true;
+	}
+}
+
+}} // namespace openage::gui
---
+++ libopenage/gui/minimap.h
@@ -0,0 +1,209 @@
+// Copyright 2016-2016 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <functional>
+#include <tuple>
+
+#include <QQuickItem>
+
+#include "../coord/chunk.h"
+#include "../engine.h"
+
+#include "minimap_unit_marker_points.h"
+
+class QSGGeometryNode;
+
+namespace openage {
+namespace gui {
+
+class GameMainLink;
+class GuiTexture;
+
+/**
+ * Holds terrain color data until it's transformed into a background texture.
+ */
+class BackgroundUpdate {
+public:
+	explicit BackgroundUpdate() = default;
+
+	/**
+	 * @param background_tex terrain texture, size and bottom/right spacing in it in pixels
+	 * @param bounding_square sw corner and side length
+	 */
+	explicit BackgroundUpdate(std::tuple<std::unique_ptr<uint32_t[]>, int, int>&& background_tex, std::tuple<coord::chunk, coord::chunk_t> bounding_square);
+
+	/**
+	 * @return true if array of terrain color data has just been generated and is ready to be consumed by a texture in the scenegraph
+	 */
+	bool consumed() const;
+
+	/**
+	 * Create a texture from array of terrain color data
+	 *
+	 * @return texture, size in pixels, sw corner and side length in chunks
+	 */
+	std::tuple<std::unique_ptr<GuiTexture>, QSizeF, std::tuple<coord::chunk, coord::chunk_t>> consume();
+
+private:
+	std::tuple<std::unique_ptr<uint32_t[]>, int, int> background_tex;
+	std::tuple<coord::chunk, coord::chunk_t> bounding_square;
+};
+
+/**
+ * QML item to render a minimap.
+ *
+ * Accepts another QML item and uses it as atlas: source of icons for each marker type.
+ */
+class Minimap : public QQuickItem {
+	Q_OBJECT
+
+	Q_PROPERTY(openage::gui::GameMainLink* game READ get_game WRITE set_game)
+	Q_PROPERTY(int markerSize MEMBER marker_size)
+	Q_PROPERTY(QRect viewFrameRect MEMBER view_frame_rect NOTIFY viewFrameRectChanged)
+	Q_PROPERTY(QQuickItem* markerAtlas READ get_marker_atlas WRITE set_marker_atlas)
+	Q_PROPERTY(QVariantList markersTypes READ get_markers_types WRITE set_markers_types)
+	Q_PROPERTY(float markerAtlasSpacing READ get_marker_atlas_spacing WRITE set_marker_atlas_spacing)
+
+public:
+	explicit Minimap(QQuickItem *parent=nullptr);
+	virtual ~Minimap();
+
+	/**
+	 * Update textures and geometry buffers.
+	 * Running in the render thread while the GUI thread is blocked.
+	 */
+	virtual QSGNode* updatePaintNode(QSGNode *node, UpdatePaintNodeData*) override;
+
+	/**
+	 * Extract necessary data from the game logic.
+	 * Running in the GUI thread.
+	 */
+	virtual void updatePolish() override;
+
+	GameMainLink* get_game() const;
+	void set_game(GameMainLink *game);
+
+	/**
+	 * A texture provider item with a square grid of icons for the markers.
+	 * Define it in QML.
+	 * Must be a texture provider (set 'layer.enabled: true' in its QML definition).
+	 *
+	 * For 5 marker types:
+	 *  _______________________
+	 * |  _____|  _____|  _____|
+	 * | |     | |     | |     |
+	 * | |  0  | |  1  | |  2  |
+	 * |_|_____|_|_____|_|_____|
+	 * |  _____|  _____|  _____|
+	 * | |     | |     | |     |
+	 * | |  3  | |  4  | |     |
+	 * |_|_____|_|_____|_|_____|
+	 * |  _____|  _____|  _____|
+	 * | |     | |     | |     |
+	 * | |     | |     | |     |
+	 * |_|_____|_|_____|_|_____|
+	 */
+	QQuickItem* get_marker_atlas() const;
+	void set_marker_atlas(QQuickItem *marker_atlas);
+
+	/**
+	 * Width and height of the empty space to the left and top of each cell (in tex coords).
+	 */
+	float get_marker_atlas_spacing() const;
+	void set_marker_atlas_spacing(float marker_atlas_spacing);
+
+	/**
+	 * The gamedata::minimap_mode enum value for each icon in the marker atlas.
+	 * The atlas is a square grid, indexed from left-top to bottom-right.
+	 */
+	QVariantList get_markers_types() const;
+	void set_markers_types(const QVariantList &markers_types);
+
+signals:
+	void viewFrameRectChanged();
+
+private:
+	/**
+	 * Create a subtree that draws a minimap for Qt scenegraph.
+	 */
+	std::unique_ptr<QSGNode> create_tree();
+
+	GameMainLink *game;
+	bool game_dirty;
+
+	std::vector<gamedata::palette_color> player_palette;
+
+	/**
+	 * Palette used in the minimap background generation.
+	 */
+	std::vector<gamedata::palette_color> palette;
+
+	/**
+	 * Shader code for drawing markers on the minimap.
+	 */
+	std::tuple<std::string, std::string> shaders;
+
+	/**
+	 * Check if the QQuickItem that provides marker atlas was replaced.
+	 */
+	std::function<void()> update_marker_atlas;
+
+	/**
+	 * Generate background texture if terrain data has changed.
+	 */
+	std::function<void(BackgroundUpdate&&, const coord_data&)> update_backround;
+
+	/**
+	 * Update the coordinates of the frame that shows the field of view of the camera.
+	 */
+	std::function<void(const coord_data&)> update_view_frame_rect;
+
+	/**
+	 * Update positions of the markers.
+	 */
+	std::function<void(const std::vector<ColoredTexturedPoint2D>&)> update_marker_points;
+
+	/**
+	 * Don't destroy the minimap's scenegraph subtree when the minimap becomes inactive.
+	 */
+	std::unique_ptr<QSGNode> hidden_root;
+
+	/**
+	 * Don't destroy the markers scenegraph subtree when the marker atlas is not set.
+	 */
+	std::unique_ptr<QSGGeometryNode> hidden_foreground;
+
+	QRectF last_bounding_rect;
+
+	size_t terrain_usage_hash;
+	BackgroundUpdate bg_update;
+	bool need_fresh_bg;
+
+	/**
+	 * Field of view of the camera in the minimap coordinates.
+	 * Use it to position the QML item that represents the camera's field of view.
+	 */
+	QRect view_frame_rect;
+
+	/**
+	 * Size of the markers in pixels.
+	 */
+	int marker_size;
+
+	QQuickItem *marker_atlas;
+	std::vector<gamedata::minimap_modes> markers_types;
+	bool marker_atlas_dirty;
+
+	float marker_atlas_spacing;
+	bool marker_atlas_spacing_dirty;
+
+	std::vector<ColoredTexturedPoint2D> unit_marker_points;
+
+	// TODO: maybe replace with something like std::optional
+	std::tuple<coord_data, bool> engine_coord_data;
+};
+
+}} // namespace openage::gui
