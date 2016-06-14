5a527e70ea17629ccba6552c9bbe85cc320428a8
Pierre Hallot <hallotpierre@gmail.com>
2016-06-15 00:36:39 +0200

    Fix copyright years and add address in copying.md

--- libopenage/audio/audio_manager.cpp
+++ libopenage/audio/audio_manager.cpp
@@ -1,4 +1,4 @@
-// Copyright 2014-2015 the openage authors. See copying.md for legal info.
+// Copyright 2014-2016 the openage authors. See copying.md for legal info.
 
 #include "audio_manager.h"
 
