960170dcb45ec74613a85898ca49b3b53ea25a9a
velorums <velorums@gmail.com>
2016-07-09 21:29:09 +0200

    InputContextRecorder and InputContextPlayer

---
+++ libopenage/input/input_context_recorder_player.cpp
@@ -0,0 +1,99 @@
+// Copyright 2016-2016 the openage authors. See copying.md for legal info.
+
+#include "input_context_recorder_player.h"
+
+#include <fstream>
+
+#include "../error/error.h"
+
+#include "input_context.h"
+
+namespace openage {
+namespace input {
+
+namespace {
+std::chrono::microseconds current_moment() {
+	return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch());
+}
+}
+
+InputContextRecorderPlayer::InputContextRecorderPlayer(qtsdl::GuiItemLink *gui_link, bool recorder)
+	:
+	stream{std::make_unique<std::iostream>(recorder ? std::cerr.rdbuf() : std::cin.rdbuf())},
+	recorder{recorder},
+	gui_link{gui_link} {
+}
+
+std::string InputContextRecorderPlayer::get_file_name() const {
+	return this->file_name;
+}
+
+void InputContextRecorderPlayer::set_file_name(const std::string &file_name) {
+	if (this->file_name != file_name) {
+		this->stream = std::make_unique<std::fstream>(file_name, this->recorder ? std::ios_base::out : std::ios_base::in);
+		this->file_name = file_name;
+	}
+}
+
+InputContextRecorder::InputContextRecorder(qtsdl::GuiItemLink *gui_link)
+	:
+	InputContextRecorderPlayer{gui_link, true},
+	started{} {
+}
+
+void InputContextRecorder::save_action(const action_arg_t &arg, const ActionManager &action_manager) {
+	if (!this->started) {
+		this->start_time = current_moment();
+		this->started = true;
+	}
+
+	*this->stream << (current_moment() - this->start_time).count() << ' ' << std::forward_as_tuple(arg, action_manager) << '\n';
+}
+
+InputContextPlayer::InputContextPlayer(qtsdl::GuiItemLink *gui_link)
+	:
+	InputContextRecorderPlayer{gui_link, false},
+	started{},
+	next_action{DeserializedEvent{DeserializedClassCode{event_class::ANY, 0}, modset_t{}, ""}, coord::window{}, coord::window_delta{}, std::vector<action_id_t>{}},
+	ended{} {
+}
+
+namespace {
+std::tuple<std::chrono::microseconds, deserialized_action_arg_t, bool> read_action(std::istream &i, const ActionManager &action_manager) {
+	std::chrono::microseconds::rep next_action_time;
+	deserialized_action_arg_t next_action{DeserializedEvent{DeserializedClassCode{event_class::ANY, 0}, modset_t{}, ""}, coord::window{}, coord::window_delta{}, std::vector<action_id_t>{}};
+	const bool ended = !(i >> next_action_time >> std::forward_as_tuple(next_action, action_manager)).good();
+
+	return std::make_tuple(std::chrono::microseconds{next_action_time}, next_action, ended);
+}
+}
+
+void InputContextPlayer::perform(InputContext &context, const ActionManager &action_manager) {
+	ENSURE(this->started || (!this->started && !this->ended), "Set ended to false when restarting.");
+
+	if (!this->started) {
+		this->start_time = current_moment();
+		this->started = true;
+
+		std::tie(this->next_action_time, this->next_action, this->ended) = read_action(*this->stream, action_manager);
+		std::cerr << "got0 " << std::forward_as_tuple(to_action(this->next_action), action_manager) << "\n";
+	}
+
+	if (!this->ended) {
+		const auto current = current_moment() - this->start_time;
+
+		while (!this->ended) {
+			if (this->next_action_time < current) {
+				std::cerr << "performing " << std::forward_as_tuple(to_action(this->next_action), action_manager) << "\n";
+				context.execute_if_bound(to_action(this->next_action));
+			}
+			else
+				break;
+
+			std::tie(this->next_action_time, this->next_action, this->ended) = read_action(*this->stream, action_manager);
+			std::cerr << "got1 " << std::forward_as_tuple(to_action(this->next_action), action_manager) << "\n";
+		}
+	}
+}
+
+}} // openage::input
---
+++ libopenage/input/input_context_recorder_player.h
@@ -0,0 +1,74 @@
+// Copyright 2016-2016 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <chrono>
+
+#include "action.h"
+#include "action_serialization.h"
+
+namespace qtsdl {
+class GuiItemLink;
+} // qtsdl
+
+namespace openage {
+namespace input {
+
+class InputContext;
+class ActionManager;
+
+class InputContextRecorderPlayer {
+public:
+	explicit InputContextRecorderPlayer(qtsdl::GuiItemLink *gui_link, bool recorder);
+#ifndef NDEBUG
+	virtual ~InputContextRecorderPlayer() {}
+#endif
+
+	std::string get_file_name() const;
+	void set_file_name(const std::string &file_name);
+
+protected:
+	std::string file_name;
+	std::unique_ptr<std::iostream> stream;
+	const bool recorder;
+
+public:
+	qtsdl::GuiItemLink *gui_link;
+};
+
+class InputContextRecorder : public InputContextRecorderPlayer {
+public:
+	explicit InputContextRecorder(qtsdl::GuiItemLink *gui_link);
+
+	void save_action(const action_arg_t &arg, const ActionManager &action_manager);
+
+private:
+	/**
+	 * Moment when the recording has started.
+	 */
+	std::chrono::microseconds start_time;
+	bool started;
+};
+
+class InputContextPlayer : public InputContextRecorderPlayer {
+public:
+	explicit InputContextPlayer(qtsdl::GuiItemLink *gui_link);
+
+	void perform(InputContext &context, const ActionManager &action_manager);
+
+private:
+	/**
+	 * Moment when the playback has started.
+	 */
+	std::chrono::microseconds start_time;
+	bool started;
+
+	std::chrono::microseconds next_action_time;
+	deserialized_action_arg_t next_action;
+
+	bool ended;
+};
+
+}} // openage::input
