498253d3ce8952f7998c960ef734d3b6ecdacd9b
MaanooAk <akritasak@gmail.com>
2016-12-05 17:08:07 +0200

    ResourceBoundle first draft

---
+++ libopenage/gamestate/resource.cpp
@@ -0,0 +1,12 @@
+// Copyright 2016 the openage authors. See copying.md for legal info.
+
+#include "resource.h"
+
+namespace openage {
+
+ResourceBoundle::ResourceBoundle()
+	:
+	value{0} {
+}
+
+} // openage
--- libopenage/gamestate/resource.h
+++ libopenage/gamestate/resource.h
@@ -17,6 +17,66 @@ enum class game_resource {
 	RESOURCE_TYPE_COUNT
 };
 
+class ResourceBoundle {
+public:
+
+	ResourceBoundle();
+
+	float& operator[] (game_resource res) { return value[(int) res]; }
+	float& operator[] (int index) { return value[index]; }
+
+	float get(game_resource res) const { return value[(int) res]; }
+	float get(int index) const { return value[index]; }
+
+	bool operator> (const ResourceBoundle& other) const {
+		for (int i=0; i<(int) game_resource::RESOURCE_TYPE_COUNT; i++) {
+			if (!(this->get(i) > other.get(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	bool operator>= (const ResourceBoundle& other) const {
+		for (int i=0; i<(int) game_resource::RESOURCE_TYPE_COUNT; i++) {
+			if (!(this->get(i) >= other.get(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	ResourceBoundle& operator+= (const ResourceBoundle& other) {
+		for (int i=0; i<(int) game_resource::RESOURCE_TYPE_COUNT; i++) {
+			(*this)[i] += other.get(i);
+		}
+		return *this;
+	}
+	ResourceBoundle& operator-= (const ResourceBoundle& other) {
+		for (int i=0; i<(int) game_resource::RESOURCE_TYPE_COUNT; i++) {
+			(*this)[i] -= other.get(i);
+		}
+		return *this;
+	}
+	
+	ResourceBoundle& operator*= (float a) {
+		for (int i=0; i<(int) game_resource::RESOURCE_TYPE_COUNT; i++) {
+			(*this)[i] *= a;
+		}
+		return *this;
+	}
+	
+	bool deduct(const ResourceBoundle& amount) {
+		if(*this >= amount) {
+			*this -= amount;
+			return true;
+		}
+		return false;
+	}
+
+private:
+	float value[(int) game_resource::RESOURCE_TYPE_COUNT];
+};
+
 } // namespace openage
 
 namespace std {
