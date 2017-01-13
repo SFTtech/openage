86ae1330da3facb45b617860f9636ff423fd3dd9
Jonas Jelten <jj@sft.mx>
2017-01-13 18:48:47 +0100

    nyan: remove obsolete nyan code

--- openage/nyan/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2013-2015 the openage authors. See copying.md for legal info.
-
-"""
-NYAN is openage's game specification language.
-
-Heavily in development; the contents of this package are subject to future
-replacement by entirely different code.
-"""
