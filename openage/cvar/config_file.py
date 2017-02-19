c7f3c31ab32b084c250fa3a669f054a9f20ceab3
velorums <velorums@gmail.com>
2017-02-19 21:36:11 +0100

    method to write back to config file

--- openage/cvar/config_file.py
+++ openage/cvar/config_file.py
@@ -4,7 +4,12 @@
 Load and save the configuration : file <-> console var system
 """
 
-from ..log import info
+import os
+import tempfile
+import shutil
+from pathlib import Path
+
+from ..log import info, dbg
 
 
 def load_config_file(path, set_cvar_func, loaded_files=None):
@@ -48,3 +53,123 @@ def load_config_file(path, set_cvar_func, loaded_files=None):
                 for sub_path in split[1:]:
                     new_path = path.parent / sub_path
                     load_config_file(new_path, set_cvar_func, loaded_files)
+
+
+def config_file_set_existing_option(path, name, value, lineno):
+    """
+    Set a value in a configuration file if the value exists already.
+    """
+
+    temp_dir = tempfile.mkdtemp()
+    temp_path = Path(temp_dir, 'tmp_openage_cvar')
+
+    dest = temp_path.open("w")
+
+    try:
+        with path.open() as config:
+            for _ in range(0, lineno):
+                line = config.readline()
+
+                if not line:
+                    return False
+
+                dest.write(line)
+
+            line = config.readline()
+
+            if not line:
+                return False
+
+            lstrip = line.lstrip()
+            if not lstrip or lstrip.startswith("#"):
+                return False
+
+            strip = lstrip.rstrip()
+            split = strip.split()
+
+            if split[0] == "set" and len(split) >= 3 and split[1] == name:
+                dest.write(str.join("", ("set ", name, " ", value, "\n")))
+            else:
+                return False
+
+            for line in config:
+                dest.write(line)
+
+        dest.close()
+
+        shutil.copy2(str(temp_path), str(path))
+
+        dbg("saving cvar %s: '%s'" % (name, value))
+
+        return True
+
+    finally:
+        os.remove(str(temp_path))
+        os.rmdir(temp_dir)
+
+    return False
+
+
+def config_file_set_option(path, name, value):
+    """
+    Set a value in a configuration file, with possible subfile.
+    """
+
+    if not path.is_file():
+        return False
+
+    lineno = -1
+
+    with path.open() as config:
+        for line in config:
+            lineno += 1
+
+            if not line:
+                break
+
+            lstrip = line.lstrip()
+            if not lstrip or lstrip.startswith("#"):
+                continue
+
+            strip = lstrip.rstrip()
+            split = strip.split()
+
+            if split[0] == "set" and len(split) >= 3 and split[1] == name:
+                temp_dir = tempfile.mkdtemp()
+                temp_path = Path(temp_dir, 'tmp_openage_cvar')
+
+                shutil.copy2(str(path), str(temp_path))
+
+                success = False
+
+                try:
+                    success = config_file_set_existing_option(temp_path, name, value, lineno)
+                    shutil.copy2(str(temp_path), str(path))
+                finally:
+                    os.remove(str(temp_path))
+                    os.rmdir(temp_dir)
+
+                if success:
+                    return True
+
+            elif split[0] == "load" and len(split) >= 2:
+                for sub_path in split[1:]:
+                    new_path = path.parent / sub_path
+                    if config_file_set_option(new_path, name, value):
+                        return True
+
+    return False
+
+
+def config_file_append_option(path, name, value):
+    """
+    Append a value to a configuration file.
+    """
+
+    if not path.is_file():
+        return
+
+    dbg("adding cvar %s: '%s'" % (name, value))
+
+    with path.open("a+") as config:
+        config.write(str.join("", ("set ", name, " ", value, "\n")))
--- openage/cvar/cvar.pyx
+++ openage/cvar/cvar.pyx
@@ -7,12 +7,16 @@ from libcpp.string cimport string
 
 from libopenage.cvar.cvar cimport (
     CVarManager,
-    pyx_load_config_file
+    pyx_load_config_file,
+    pyx_config_file_set_option
 )
 from libopenage.util.path cimport Path as Path
 from openage.util.fslike.cpp cimport cpppath_to_pypath
 
-from .config_file import load_config_file
+from .config_file import (
+    load_config_file,
+    config_file_set_option,
+    config_file_append_option)
 from .. import default_dirs
 
 
@@ -34,5 +38,22 @@ cdef void cy_load_config_file(CVarManager *manager, const Path &path) except * w
     load_config_file(py_path, set_func)
 
 
+cdef void cy_config_file_set_option(const Path &path, string name,
+                                    string value) except * with gil:
+    """
+    Set a value in a configuration file.
+    """
+
+    py_path = cpppath_to_pypath(path)
+
+    if not config_file_set_option(py_path,
+                                  name.decode('UTF-8'),
+                                  value.decode('UTF-8')):
+        config_file_append_option(py_path,
+                                  name.decode('UTF-8'),
+                                  value.decode('UTF-8'))
+
+
 def setup():
     pyx_load_config_file.bind0(cy_load_config_file)
+    pyx_config_file_set_option.bind0(cy_config_file_set_option)
