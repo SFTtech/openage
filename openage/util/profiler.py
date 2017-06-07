e810056ecee8c1700dd5eae8975ad697f4b7be9e
ohn0 <silverskinx@gmail.com>
2017-06-07 18:11:05 -0400

    Linting issues fixed and added methods to allow profiling to be directly enabled and disabled.

--- openage/util/profiler.py
+++ openage/util/profiler.py
@@ -1,44 +1,60 @@
-import cProfile, pstats, io
+# Copyright 2017-2017 the openage authors. See copying.md for legal info.
+
+"""
+Profiling utilities
+"""
+
+import cProfile
+import pstats
+import io
+
+
 class Profiler:
-    '''
-        A class for quick and easy profiling.
-        Usage:
+    """
+    A class for quick and easy profiling.
+    Usage:
             p = Profiler()
             with p:
                 #Call methods that need to be profiled.
             p.report()
-    '''
-    
+    The 'with' statement can be replaced with calls to
+    p.begin_profiling() and p.end_profiling().
+    """
+
     profile = None
     profile_stats = None
     profile_stream = None
-    
-    def __init__(self, oStream = None):
-        #oStream can be a file if the profile results want to be saved.
+
+    def __init__(self, oStream=None):
+        # oStream can be a file if the profile results want to be saved.
         self.profile = cProfile.Profile()
-        self.profile_stream = oStream                        
+        self.profile_stream = oStream
 
     def __enter__(self):
-        #begins profiling
-        self.profile.clear()
-        self.profile.enable()
-        
-    def __exit__(self, exc_type, exc_value, traceback):
-        #Stops profiling and saves results into profile_stats.
-        self.profile.disable()
+        # begins profiling
+        self.enable()
 
-    def write_report(self, sortby = 'calls'):
-        #Write the profile stats to profile_stream's file.
-        self.profile_stats = pstats.Stats(self.profile, stream = self.profile_stream)
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Stops profiling
+        self.disable()
+
+    def write_report(self, sortby='calls'):
+        """Write the profile stats to profile_stream's file."""
+        self.profile_stats = pstats.Stats(self.profile, stream=self.profile_stream)
         self.profile_stats.sort_stats(sortby)
         self.profile_stats.print_stats()
 
-    def report(self, sortby = 'calls'):
-        #Return the profile_stats to the console.
-        self.profile_stats = pstats.Stats(self.profile, stream = io.StringIO())
+    def report(self, sortby='calls'):
+        """Return the profile_stats to the console."""
+        self.profile_stats = pstats.Stats(self.profile, stream=io.StringIO())
         self.profile_stats.sort_stats(sortby)
         self.profile_stats.print_stats()
         return self.profile_stats.stream.getvalue()
 
+    def enable(self):
+        """Begins profiling calls."""
+        self.profile.enable()
 
-
+    def disable(self):
+        """Stop profiling calls."""
+        self.profile.disable()
