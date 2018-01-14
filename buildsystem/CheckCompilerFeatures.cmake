0d9e145c13a98cca0143f480b84b77c61bdf7329
Tushar Maheshwari <tushar27192@gmail.com>
2018-01-14 17:50:11 +0530

    buildsystem: add known compiler versions to error message

--- buildsystem/CheckCompilerFeatures.cmake
+++ buildsystem/CheckCompilerFeatures.cmake
@@ -28,6 +28,10 @@ if(NOT HAVE_REQUIRED_CXX17_SUPPORT)
 The compiler doesn't support required C++17 features:
   * Nested namespace definition (N4230)
   * Structured Bindings (P0217R3)
+The following versions support these features:
+  * clang++ >= 5
+  * g++ >= 7
+  * Microsoft Visual Studio 2017 >= 15.5
 Please upgrade your compiler to build openage.
 
 ")
