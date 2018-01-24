3c4f7547781809a2b506cfb7dbd4dcab21bc37b0
Michael Ensslin <michael@ensslin.cc>
2018-01-24 23:15:44 +0100

    util: added error handler for illegal calls to exit()

---
+++ libopenage/error/handlers.h
@@ -0,0 +1,23 @@
+// Copyright 2016-2018 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+// pxd: from libcpp cimport bool as cppbool
+
+namespace openage {
+namespace error {
+
+
+/**
+ * Call this to set or unset the atexit error handler.
+ *
+ * In order to capture any stray calls to exit() from any
+ * place in the game (including libraries), set this to true
+ * on startup, and back to false right before termination.
+ *
+ * pxd: void set_exit_ok(cppbool value) except +
+ */
+void set_exit_ok(bool value);
+
+
+}} // openage::error
