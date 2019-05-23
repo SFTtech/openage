86301b12320fcc1b49ff5076a0258127779f6b48
simonsan <14062932+simonsan@users.noreply.github.com>
2019-05-23 16:33:46 +0200

    Breathe-Sphinx-Toolchain including Doxygen XML, beware: Draft for index.rst, only half-working

---
+++ doc/sphinx/_sources/index.rst.txt
@@ -0,0 +1,51 @@
+.. openage documentation master file, created by
+   sphinx-quickstart on Thu May 23 07:57:59 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to openage's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+Index
+==================
+.. doxygenindex::
+   :project: libopenage
+   :path: libopenage
+   :outline:
+   :no-link:
+
+Functions
+==================
+.. doxygenfunction::
+   :project: libopenage
+   :path: libopenage
+   :outline:
+   :no-link:
+
+Structs
+==================
+.. doxygenstruct::
+
+Enums
+==================
+.. doxygenenum::
+
+Classes
+==================
+.. doxygenclass::
+
+Typedefs
+==================
+.. doxygentypedef::
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ openage/conf.py
@@ -0,0 +1,73 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+#import os
+#import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'openage'
+copyright = '2019, openage-Team'
+author = 'openage-Team'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [ 'breathe', 'sphinx.ext.autodoc' ]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+html_logo = '../assets/logo/banner.svg'
+
+html_theme_options = {
+
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Breathe
+#sys.path.append( "C:/Program Files (x86)/Microsoft Visual Studio/Shared/Python37_64/Lib/site-packages/breathe" )
+breathe_projects = { "libopenage":"../doc/doxygen/xml" }
+breathe_default_project = "libopenage"
