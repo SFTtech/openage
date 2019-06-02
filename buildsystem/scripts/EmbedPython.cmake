4144d06dab403bc0495cc5fbb1a6879c0a7c1eb2
Tushar Maheshwari <tushar27192@gmail.com>
2019-06-02 17:23:19 +0530

    buildsystem: fix packaged python architecture

--- buildsystem/scripts/EmbedPython.cmake
+++ buildsystem/scripts/EmbedPython.cmake
@@ -1,11 +1,16 @@
-# Copyright 2017-2017 the openage authors. See copying.md for legal info.
+# Copyright 2017-2019 the openage authors. See copying.md for legal info.
 
 if(NOT forward_variables)
 	message(FATAL_ERROR "CMake configuration variables not available. Please include(ForwardVariables.cmake)")
 endif()
 
 include("${buildsystem_dir}/modules/DownloadCache.cmake")
-set(python_embed_zip "python-${py_version}-embed-win32.zip")
+if(sizeof_void_p EQUAL 8)
+	set(py_arch "amd64")
+else()
+	set(py_arch "win32")
+endif()
+set(python_embed_zip "python-${py_version}-embed-${py_arch}.zip")
 set(python_zip_download_path "${downloads_dir}/${python_embed_zip}")
 # TODO: disable downloading for CI builds. <Need CI first>
 download_cache(
