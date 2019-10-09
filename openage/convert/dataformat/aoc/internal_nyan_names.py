a2e727420f84f6c082e66bb9971bacbd20d9dc0b
heinezen <heinezen@hotmail.de>
2019-10-09 06:21:30 +0200

    convert: Lookup dicts for nyan object names.

---
+++ openage/convert/dataformat/aoc/internal_nyan_names.py
@@ -0,0 +1,92 @@
+# Copyright 2019-2019 the openage authors. See copying.md for legal info.
+
+"""
+Age of Empires games do not necessarily come with an english
+translation. Therefore, we use the strings in this file to
+figure out the names for a nyan object.
+"""
+
+# key: line_id; value: nyan object name
+unit_line_lookups = {
+    3:  "FishingShip",
+    4:  "Swordsman",
+    22: "Archer",
+    23: "TradeCog",
+    24: "Spearman",
+    26: "Galley",
+    27: "TradeCart",
+    28: "Skirmisher",
+    29: "TransportShip",
+    49: "Ram",
+    50: "Cataphract",
+    51: "ChoKoNu",
+    52: "Conquistador",
+    53: "CamelRider",
+    54: "HorseArcher",
+    55: "Mameluke",
+    56: "EagleWarrior",
+    57: "FireTrireme",
+    58: "Huscarl",
+    59: "JaguarWarrior",
+    60: "Janissary",
+    61: "Knight",
+    62: "Longboat",
+    63: "Longbowman",
+    64: "Mangonel",
+    66: "Missionary",
+    67: "Mangudai",
+    68: "WarElephant",
+    69: "Petard",
+    70: "PlumedArcher",
+    71: "DemolitionShip",
+    72: "Scorpion",
+    73: "Samurai",
+    74: "Tarkan",
+    75: "ThrowingAxeman",
+    76: "TeutonicKnight",
+    77: "TurtleShip",
+    78: "Berserk",
+    79: "WarWaggon",
+    80: "WoadRaider",
+    113: "BombardCannon",
+    114: "CannonGalleon",
+    115: "HandCannoneer",
+}
+
+building_line_lookups = {
+    12: "Barracks",
+    45: "Harbor",
+    49: "SiegeWorkshop",
+    50: "Farm",
+    68: "Mill",
+    70: "House",
+    72: "PalisadeWall",
+    79: "Tower",
+    82: "Castle",
+    84: "Market",
+    87: "ArcheryRange",
+    101: "Stable",
+    103: "Blacksmith",
+    104: "Monastery",
+    117: "StoneWall",
+    199: "FishingTrap",
+    209: "University",
+    236: "BombardTower",
+    276: "Wonder",
+    487: "StoneGate",
+    562: "LumberCamp",
+    584: "MiningCamp",
+    598: "Outpost"
+}
+
+transform_group_lookups = {
+    116: "Trebuchet",
+}
+
+villager_group_lookups = {
+    5: "Villager",
+}
+
+monk_group_lookups = {
+    65: "Monk",
+}
