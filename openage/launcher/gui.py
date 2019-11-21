d30eee8898af11ce69c879c4ad58b216d444a82b
Lorenzo Gaifas <brisvag@gmail.com>
2019-11-21 13:14:56 +0100

    better layouts, added content

--- openage/launcher/gui.py
+++ openage/launcher/gui.py
@@ -19,6 +19,9 @@ icon_path = '../../assets/logo/favicon.ico'
 
 
 class ImageLabel(QLabel):
+    """
+    image widget with antialiased scaling
+    """
     def __init__(self, image_path, max_x, max_y, *args, **kwargs):
         super(ImageLabel, self).__init__(*args, **kwargs)
         pixmap = QPixmap(image_path).scaled(max_x, max_y, aspectRatioMode=Qt.KeepAspectRatio,
@@ -67,25 +70,29 @@ class NewsBox(QTextBrowser):
             self.append(dummy)
 
 
-class ModTable(QTableWidget):
-    def __init__(self, *args, **kwargs):
-        super(ModTable, self).__init__(*args, **kwargs)
+class ContentTable(QTableWidget):
+    # create and add table
+    def __init__(self, *args, noselect=False, **kwargs):
+        super(ContentTable, self).__init__(*args, **kwargs)
+
+        # table settings
         self.setColumnCount(4)
         self.setHorizontalHeaderLabels(['Activate', 'Name', 'Version', 'Author'])
         self.horizontalHeader().setStretchLastSection(True)
         self.verticalHeader().hide()
-
-        # disable editability and selectability
-        self.setSelectionMode(QTableWidget.NoSelection)
+        # disable editability, enable row selection
+        if noselect:
+            self.setSelectionMode(QAbstractItemView.NoSelection)
+        else:
+            self.setSelectionMode(QTableWidget.SingleSelection)
+            self.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.setFocusPolicy(Qt.NoFocus)
         self.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
-        self.mods = []
-
-        self.add_mod('testmod0', '2.0', 'Someone', False)
-        self.add_mod('testmod1', '1.0', 'Someone Else', True)
-
-    def add_mod(self, name, version, author, active):
+    def add_entry(self, name, version, author, active):
+        """
+        add an entry to the table and display its activation state
+        """
         idx = self.rowCount()
         self.insertRow(idx)
         check = QTableWidgetItem()
@@ -99,21 +106,66 @@ class ModTable(QTableWidget):
         self.setItem(idx, 3, QTableWidgetItem(author))
 
 
-class ModEditButtons(QWidget):
+class ContentBox(QWidget):
     def __init__(self, *args, **kwargs):
-        super(ModEditButtons, self).__init__(*args, **kwargs)
-        self.setLayout(QHBoxLayout())
+        super(ContentBox, self).__init__(*args, **kwargs)
+        self.setLayout(QVBoxLayout())
 
-        self.up = QPushButton('Up')
-        self.down = QPushButton('Down')
-        self.add = QPushButton('Add')
-        self.delete = QPushButton('Delete')
+        self.table = ContentTable(noselect=True)
+        self.layout().addWidget(self.table)
 
-        self.layout().addWidget(self.up)
-        self.layout().addWidget(self.down)
-        self.layout().addStretch(1)
-        self.layout().addWidget(self.add)
-        self.layout().addWidget(self.delete)
+        # test content
+        self.table.add_entry('testcontent0', '2.0', 'Someone', False)
+        self.table.add_entry('testcontent1', '1.0', 'Someone Else', True)
+
+class ModBox(QWidget):
+    def __init__(self, *args, **kwargs):
+        super(ModBox, self).__init__(*args, **kwargs)
+        self.setLayout(QVBoxLayout())
+
+        # create and add table
+        self.table = ContentTable()
+        self.layout().addWidget(self.table)
+
+        # create and add button layout
+        self.buttons = QWidget()
+        self.layout().addWidget(self.buttons)
+        self.buttons.setLayout(QHBoxLayout())
+
+        # create and add buttons
+        self.buttons.up = QPushButton('Up')
+        self.buttons.down = QPushButton('Down')
+        self.buttons.add = QPushButton('Add')
+        self.buttons.delete = QPushButton('Delete')
+
+        self.buttons.layout().addWidget(self.buttons.up)
+        self.buttons.layout().addWidget(self.buttons.down)
+        self.buttons.layout().addStretch(1)
+        self.buttons.layout().addWidget(self.buttons.add)
+        self.buttons.layout().addWidget(self.buttons.delete)
+
+        # connect buttons to methods
+        self.buttons.up.pressed.connect(self.mod_up)
+        self.buttons.down.pressed.connect(self.mod_down)
+
+        # test mods
+        self.table.add_entry('testmod0', '2.0', 'Someone', False)
+        self.table.add_entry('testmod1', '1.0', 'Someone Else', True)
+
+    def _move_mod(self, direction):
+        pass
+
+    def mod_up(self):
+        """
+        increase mod priority
+        """
+        self._move_mod('up')
+
+    def mod_down(self):
+        """
+        decrease mod priority
+        """
+        self._move_mod('down')
 
 
 class LauncherWindow(QMainWindow):
@@ -127,16 +179,17 @@ class LauncherWindow(QMainWindow):
 
         # main widget and main layout
         self.main = QWidget()
-        self.main_layout = QHBoxLayout()
-        self.main.setLayout(self.main_layout)
+        self.main.setLayout(QHBoxLayout())
         self.setCentralWidget(self.main)
 
         # various sub-layouts and their relative positioning
-        self.menu_layout = QVBoxLayout()
-        self.main_layout.addLayout(self.menu_layout)
+        self.left = QWidget()
+        self.left.setLayout(QVBoxLayout())
+        self.main.layout().addWidget(self.left)
 
-        self.tabs_layout = QVBoxLayout()
-        self.main_layout.addLayout(self.tabs_layout)
+        self.right = QWidget()
+        self.right.setLayout(QVBoxLayout())
+        self.main.layout().addWidget(self.right)
 
         self._init_menu()
         self._init_tabs()
@@ -145,63 +198,62 @@ class LauncherWindow(QMainWindow):
 
     def _init_menu(self):
         self.logo = ImageLabel(logo_path, 250, 250)
-        self.menu_layout.addWidget(self.logo)
+        self.left.layout().addWidget(self.logo)
 
-        self.menu_layout.addStretch(5)
+        self.left.layout().addStretch(5)
 
         buttons = [
-            ('Continue', self._continue),
-            ('Play', self._play),
-            ('Options', self._options),
-            ('Search for Updates', self._search_for_updates),
-            ('Quit', self._quit)
+            ('Continue', self.continue_),
+            ('Play', self.play),
+            ('Options', self.options),
+            ('Search for Updates', self.search_for_updates),
+            ('Quit', self.quit)
         ]
 
         background = None
         for button, action in buttons:
             button_widget = MenuButton(button, icon_path, background)
             button_widget.clicked.connect(action)
-            self.menu_layout.addWidget(button_widget)
-            self.menu_layout.addStretch(1)
+            self.left.layout().addWidget(button_widget)
+            self.left.layout().addStretch(1)
 
-        self.menu_layout.addStretch(10)
+        self.left.layout().addStretch(10)
 
         version = 'dummy version number'
 
         self.version = QLabel(version)
-        self.menu_layout.addWidget(self.version)
+        self.left.layout().addWidget(self.version)
 
     def _init_tabs(self):
         self.tabs = QTabWidget()
-        self.tabs_layout.addWidget(self.tabs)
+        self.right.layout().addWidget(self.tabs)
 
         self.news = NewsBox()
         self.tabs.addTab(self.news, 'News')
 
-        self.mods = QWidget()
-        self.mods.setLayout(QVBoxLayout())
-        self.mods.layout().addWidget(ModTable())
-        self.mods.layout().addWidget(ModEditButtons())
+        self.content = ContentBox()
+        self.tabs.addTab(self.content, 'Content')
 
+        self.mods = ModBox()
         self.tabs.addTab(self.mods, 'Mods')
 
     def fake_action(self, text):
         dialog = QErrorMessage(self)
         dialog.showMessage(text)
 
-    def _continue(self):
+    def continue_(self):
         self.fake_action('Continue!')
 
-    def _play(self):
+    def play(self):
         self.fake_action('Play!')
 
-    def _options(self):
+    def options(self):
         self.fake_action('Options!')
 
-    def _search_for_updates(self):
+    def search_for_updates(self):
         self.fake_action('Update!')
 
-    def _quit(self):
+    def quit(self):
         self.close()
 
 
