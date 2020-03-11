daf40258dd64100a38f99908c2bdf4ee7efdaaec
heinezen <heinezen@hotmail.de>
2020-03-11 16:54:09 +0100

    convert: Pregenerate resource types food, wood, stone and gold.

--- openage/convert/processor/aoc/processor.py
+++ openage/convert/processor/aoc/processor.py
@@ -33,6 +33,7 @@ from ...dataformat.converter_object import RawAPIObject,\
 from ...dataformat.aoc.expected_pointer import ExpectedPointer
 from .modpack_subprocessor import AoCModpackSubprocessor
 from openage.convert.processor.aoc.media_subprocessor import AoCMediaSubprocessor
+from openage.nyan.nyan_structs import MemberSpecialValue
 
 
 class AoCProcessor:
@@ -565,61 +566,239 @@ class AoCProcessor:
         pregen_converter_group.add_raw_api_object(unit_raw_api_object)
         pregen_nyan_objects.update({unit_ref_in_modpack: unit_raw_api_object})
 
-        # TODO: Wait for API version 0.3.0
+        # =======================================================================
+        # Resources
+        # =======================================================================
+        resource_parent = "engine.aux.resource.Resource"
+        resources_location = "data/aux/resource/"
+
+        # =======================================================================
+        # Food
+        # =======================================================================
+        food_ref_in_modpack = "aux.resource.types.Food"
+        food_raw_api_object = RawAPIObject(food_ref_in_modpack,
+                                           "Food", api_objects,
+                                           resources_location)
+        food_raw_api_object.set_filename("types")
+        food_raw_api_object.add_raw_parent(resource_parent)
+
+        pregen_converter_group.add_raw_api_object(food_raw_api_object)
+        pregen_nyan_objects.update({food_ref_in_modpack: food_raw_api_object})
+
+        food_raw_api_object.add_raw_member("max_storage",
+                                           MemberSpecialValue.NYAN_INF,
+                                           resource_parent)
+
+        name_value_parent = "engine.aux.translated.type.TranslatedString"
+        food_name_ref_in_modpack = "aux.attribute.types.Food.FoodName"
+        food_name_value = RawAPIObject(food_name_ref_in_modpack, "FoodName",
+                                       api_objects, resources_location)
+        food_name_value.set_filename("types")
+        food_name_value.add_raw_parent(name_value_parent)
+        food_name_value.add_raw_member("translations", [], name_value_parent)
+
+        name_expected_pointer = ExpectedPointer(pregen_converter_group,
+                                                food_name_ref_in_modpack)
+        food_raw_api_object.add_raw_member("name",
+                                           name_expected_pointer,
+                                           resource_parent)
+
+        pregen_converter_group.add_raw_api_object(food_name_value)
+        pregen_nyan_objects.update({food_name_ref_in_modpack: food_name_value})
+
+        # =======================================================================
+        # Wood
+        # =======================================================================
+        wood_ref_in_modpack = "aux.resource.types.Wood"
+        wood_raw_api_object = RawAPIObject(wood_ref_in_modpack,
+                                           "Wood", api_objects,
+                                           resources_location)
+        wood_raw_api_object.set_filename("types")
+        wood_raw_api_object.add_raw_parent(resource_parent)
+
+        pregen_converter_group.add_raw_api_object(wood_raw_api_object)
+        pregen_nyan_objects.update({wood_ref_in_modpack: wood_raw_api_object})
+
+        wood_raw_api_object.add_raw_member("max_storage",
+                                           MemberSpecialValue.NYAN_INF,
+                                           resource_parent)
+
+        name_value_parent = "engine.aux.translated.type.TranslatedString"
+        wood_name_ref_in_modpack = "aux.attribute.types.Wood.WoodName"
+        wood_name_value = RawAPIObject(wood_name_ref_in_modpack, "WoodName",
+                                       api_objects, resources_location)
+        wood_name_value.set_filename("types")
+        wood_name_value.add_raw_parent(name_value_parent)
+        wood_name_value.add_raw_member("translations", [], name_value_parent)
+
+        name_expected_pointer = ExpectedPointer(pregen_converter_group,
+                                                wood_name_ref_in_modpack)
+        wood_raw_api_object.add_raw_member("name",
+                                           name_expected_pointer,
+                                           resource_parent)
+
+        pregen_converter_group.add_raw_api_object(wood_name_value)
+        pregen_nyan_objects.update({wood_name_ref_in_modpack: wood_name_value})
+
+        # =======================================================================
+        # Stone
+        # =======================================================================
+        stone_ref_in_modpack = "aux.resource.types.Stone"
+        stone_raw_api_object = RawAPIObject(stone_ref_in_modpack,
+                                            "Stone", api_objects,
+                                            resources_location)
+        stone_raw_api_object.set_filename("types")
+        stone_raw_api_object.add_raw_parent(resource_parent)
+
+        pregen_converter_group.add_raw_api_object(stone_raw_api_object)
+        pregen_nyan_objects.update({stone_ref_in_modpack: stone_raw_api_object})
+
+        stone_raw_api_object.add_raw_member("max_storage",
+                                            MemberSpecialValue.NYAN_INF,
+                                            resource_parent)
+
+        name_value_parent = "engine.aux.translated.type.TranslatedString"
+        stone_name_ref_in_modpack = "aux.attribute.types.Stone.StoneName"
+        stone_name_value = RawAPIObject(stone_name_ref_in_modpack, "StoneName",
+                                        api_objects, resources_location)
+        stone_name_value.set_filename("types")
+        stone_name_value.add_raw_parent(name_value_parent)
+        stone_name_value.add_raw_member("translations", [], name_value_parent)
+
+        name_expected_pointer = ExpectedPointer(pregen_converter_group,
+                                                stone_name_ref_in_modpack)
+        stone_raw_api_object.add_raw_member("name",
+                                            name_expected_pointer,
+                                            resource_parent)
+
+        pregen_converter_group.add_raw_api_object(stone_name_value)
+        pregen_nyan_objects.update({stone_name_ref_in_modpack: stone_name_value})
+
+        # =======================================================================
+        # Gold
+        # =======================================================================
+        gold_ref_in_modpack = "aux.resource.types.Gold"
+        gold_raw_api_object = RawAPIObject(gold_ref_in_modpack,
+                                           "Gold", api_objects,
+                                           resources_location)
+        gold_raw_api_object.set_filename("types")
+        gold_raw_api_object.add_raw_parent(resource_parent)
+
+        pregen_converter_group.add_raw_api_object(gold_raw_api_object)
+        pregen_nyan_objects.update({gold_ref_in_modpack: gold_raw_api_object})
+
+        gold_raw_api_object.add_raw_member("max_storage",
+                                           MemberSpecialValue.NYAN_INF,
+                                           resource_parent)
+
+        name_value_parent = "engine.aux.translated.type.TranslatedString"
+        gold_name_ref_in_modpack = "aux.attribute.types.Gold.GoldName"
+        gold_name_value = RawAPIObject(gold_name_ref_in_modpack, "GoldName",
+                                       api_objects, resources_location)
+        gold_name_value.set_filename("types")
+        gold_name_value.add_raw_parent(name_value_parent)
+        gold_name_value.add_raw_member("translations", [], name_value_parent)
+
+        name_expected_pointer = ExpectedPointer(pregen_converter_group,
+                                                gold_name_ref_in_modpack)
+        gold_raw_api_object.add_raw_member("name",
+                                           name_expected_pointer,
+                                           resource_parent)
+
+        pregen_converter_group.add_raw_api_object(gold_name_value)
+        pregen_nyan_objects.update({gold_name_ref_in_modpack: gold_name_value})
+
         # =======================================================================
         # Generic Death Condition (HP<=0)
         #    sidenote: Apparently this is actually HP<1 in Genie
         #              (https://youtu.be/FdBk8zGbE7U?t=7m16s)
         #
         # =======================================================================
-#         clause_parents = [api_objects["engine.aux.boolean.Clause"]]
-#
-#         clause_nyan_object = NyanObject("StandardHealthDeath", clause_parents)
-#
-#         # Clause will not default to 'True' when it was fulfilled once
-#         only_once = clause_nyan_object.get_member_by_name("only_once",
-#                                                           api_objects["engine.aux.boolean.Clause"])
-#         only_once.set_value(False, MemberOperator.ASSIGN)
-#
-#         # Requirement mode does not matter, so we use ANY
-#         clause_requirement_value = api_objects["engine.aux.requirement_mode.type.any"]
-#         clause_requirement = clause_nyan_object.get_member_by_name("clause_requirement",
-#                                                                    api_objects["engine.aux.boolean.Clause"])
-#         clause_requirement.set_value(clause_requirement_value, MemberOperator.ASSIGN)
-#
-#         # Literal
-#         literal_parents = [api_objects["engine.aux.boolean.literal.type.AttributeBelowValue"]]
-#
-#         literal_nyan_object = NyanObject("HPBelowZero", literal_parents)
-#
-#         attribute_value = pregen_nyan_objects["aux.attribute.types.Health"]
-#         attribute = literal_nyan_object.get_member_by_name("attribute",
-#                                                            api_objects["engine.aux.boolean.literal.type.AttributeBelowValue"])
-#         attribute.set_value(attribute_value, MemberOperator.ASSIGN)
-#
-#         value = literal_nyan_object.get_member_by_name("value",
-#                                                        api_objects["engine.aux.boolean.literal.type.AttributeBelowValue"])
-#         value.set_value(0, MemberOperator.ASSIGN)
-#
-#         mode = literal_nyan_object.get_member_by_name("mode",
-#                                                       api_objects["engine.aux.boolean.Literal"])
-#         mode.set_value(True, MemberOperator.ASSIGN)
-#
-#         scope_parents = [api_objects["engine.aux.literal_scope.type.Self"]]
-#
-#         scope_nyan_object = NyanObject("StandardHealthDeathScope", scope_parents)
-#
-#         stances = scope_nyan_object.get_member_by_name("diplomatic_stances",
-#                                                        api_objects["engine.aux.literal_scope.LiteralScope"])
-#         stances.set_value([], MemberOperator.ASSIGN)
-#
-#         clause_ref_in_modpack = "aux.boolean.death.standard.StandardHealthDeath"
-#         literal_ref_in_modpack = "aux.boolean.death.standard.StandardHealthDeath.HPBelowZero"
-#         scope_ref_in_modpack = "aux.boolean.death.standard.StandardHealthDeath.HPBelowZero.StandardHealthDeathScope"
-#         pregen_nyan_objects.update({clause_ref_in_modpack: clause_nyan_object,
-#                                     literal_ref_in_modpack: literal_nyan_object,
-#                                     scope_ref_in_modpack: scope_nyan_object})
+        clause_parent = "engine.aux.boolean.Clause"
+        clause_location = "data/aux/boolean/clause/death/"
 
+        death_ref_in_modpack = "aux.boolean.clause.death.StandardHealthDeath"
+        clause_raw_api_object = RawAPIObject(death_ref_in_modpack,
+                                             "StandardHealthDeath",
+                                             api_objects,
+                                             clause_location)
+        clause_raw_api_object.set_filename("death")
+        clause_raw_api_object.add_raw_parent(clause_parent)
+
+        # Literals (see below)
+        literals_expected_pointer = [ExpectedPointer(pregen_converter_group,
+                                                     "aux.boolean.clause.death.StandardHealthDeathLiteral")]
+        clause_raw_api_object.add_raw_member("literals",
+                                             literals_expected_pointer,
+                                             clause_parent)
+
+        # Requirement mode does not matter, so we use ANY
+        requirement_mode = api_objects["engine.aux.boolean.requirement_mode.type.Any"]
+        clause_raw_api_object.add_raw_member("clause_requirement",
+                                             requirement_mode,
+                                             clause_parent)
+
+        # Clause will not default to 'True' when it was fulfilled once
+        clause_raw_api_object.add_raw_member("only_once", False, clause_parent)
+
+        pregen_converter_group.add_raw_api_object(clause_raw_api_object)
+        pregen_nyan_objects.update({death_ref_in_modpack: clause_raw_api_object})
+
+        # Literal
+        literal_parent = "engine.aux.boolean.Literal"
+        interval_parent = "engine.aux.boolean.literal.type.AttributeBelowValue"
+
+        death_literal_ref_in_modpack = "aux.boolean.clause.death.StandardHealthDeathLiteral"
+        literal_raw_api_object = RawAPIObject(death_literal_ref_in_modpack,
+                                              "StandardHealthDeathLiteral",
+                                              api_objects,
+                                              clause_location)
+        literal_location = ExpectedPointer(pregen_converter_group, death_ref_in_modpack)
+        literal_raw_api_object.set_location(literal_location)
+        literal_raw_api_object.add_raw_parent(interval_parent)
+
+        health_expected_pointer = ExpectedPointer(pregen_converter_group,
+                                                  "aux.attribute.types.Health")
+        literal_raw_api_object.add_raw_member("mode",
+                                              False,
+                                              literal_parent)
+        scope_expected_pointer = ExpectedPointer(pregen_converter_group,
+                                                 "aux.boolean.clause.death.StandardHealthDeathScope")
+        literal_raw_api_object.add_raw_member("scope",
+                                              scope_expected_pointer,
+                                              literal_parent)
+        literal_raw_api_object.add_raw_member("attribute",
+                                              health_expected_pointer,
+                                              interval_parent)
+        literal_raw_api_object.add_raw_member("threshold",
+                                              0,
+                                              interval_parent)
+
+        pregen_converter_group.add_raw_api_object(literal_raw_api_object)
+        pregen_nyan_objects.update({death_literal_ref_in_modpack: literal_raw_api_object})
+
+        # LiteralScope
+        scope_parent = "engine.aux.boolean.literal_scope.LiteralScope"
+        self_scope_parent = "engine.aux.boolean.literal_scope.type.Self"
+
+        death_scope_ref_in_modpack = "aux.boolean.clause.death.StandardHealthDeathScope"
+        scope_raw_api_object = RawAPIObject(death_scope_ref_in_modpack,
+                                            "StandardHealthDeathScope",
+                                            api_objects,
+                                            clause_location)
+        scope_location = ExpectedPointer(pregen_converter_group, death_ref_in_modpack)
+        scope_raw_api_object.set_location(scope_location)
+        scope_raw_api_object.add_raw_parent(self_scope_parent)
+
+        scope_diplomatic_stances = [api_objects["engine.aux.diplomatic_stance.type.Self"]]
+        scope_raw_api_object.add_raw_member("diplomatic_stances",
+                                            scope_diplomatic_stances,
+                                            scope_parent)
+
+        pregen_converter_group.add_raw_api_object(scope_raw_api_object)
+        pregen_nyan_objects.update({death_scope_ref_in_modpack: scope_raw_api_object})
+
+        # ============================================================================
         for pregen_object in pregen_nyan_objects.values():
             pregen_object.create_nyan_object()
 
@@ -940,7 +1119,7 @@ class AoCProcessor:
 
         for index in range(len(genie_techs)):
             tech_id = index
-            civ_id = genie_techs[index].get_member("civilisation_id").get_value()
+            civ_id = genie_techs[index].get_member("civilization_id").get_value()
 
             # Civ ID must be positive and non-zero
             if civ_id > 0:
