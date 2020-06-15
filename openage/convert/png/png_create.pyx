ca5e762b50d2a61d09e8acd3ad84c3464537dae9
heinezen <heinezen@hotmail.de>
2020-06-15 04:49:59 +0200

    png: Document compression optimization methods.

--- openage/convert/png/png_create.pyx
+++ openage/convert/png/png_create.pyx
@@ -2,7 +2,11 @@
 #
 # cython: profile=False
 
-from libc.stdio cimport _IOFBF, SEEK_END, SEEK_SET, fprintf, fclose, fread, fseek, ftell, rewind, setvbuf, tmpfile
+"""
+Creates valid PNG files as bytearrays by utilizing libpng.
+"""
+
+from libc.stdio cimport SEEK_END, fclose, fread, fseek, ftell, rewind
 from libc.stdint cimport uint8_t
 from libc.stdlib cimport malloc, free
 from libc.string cimport memcpy, memset
@@ -53,8 +57,12 @@ cdef int GREEDY_FILTER_5 = libpng.PNG_ALL_FILTERS
 def save(numpy.ndarray[numpy.uint8_t, ndim=3, mode="c"] imagedata not None,
          compr_method=CompressionMethod.COMPR_DEFAULT, compr_settings=None):
     """
-    Save an image as a PNG file.
-    
+    Convert an image matrix with RGBA colors to a PNG. The PNG is returned
+    as a bytearray.
+
+    The function provides the option to reduce the resulting PNG size by
+    doing multiple compression trials.
+
     :param imagedata: A 3-dimensional array with RGBA color values for pixels.
     :type imagedata: numpy.ndarray
     :param compr_method: The compression optimization method.
@@ -63,6 +71,10 @@ def save(numpy.ndarray[numpy.uint8_t, ndim=3, mode="c"] imagedata not None,
                            memory level, strategy and filter method (in that
                            order) used for encoding the PNG.
     :type compr_settings: tuple
+    :returns: A bytearray containing the generated PNG file as well as the
+              settings that generate the smallest PNG, if the compression
+              method COMPR_GREEDY was chosen.
+    :rtype: tuple
     """
     cdef unsigned int width = imagedata.shape[1]
     cdef unsigned int height = imagedata.shape[0]
@@ -78,12 +90,15 @@ def save(numpy.ndarray[numpy.uint8_t, ndim=3, mode="c"] imagedata not None,
             replay.filters = compr_settings[3]
 
         else:
+            # Assign invalid values. This will trigger the optimization loop.
             replay.compr_lvl = 0xFF
             replay.mem_lvl = 0xFF
             replay.strat = 0xFF
             replay.filters = 0xFF
 
-        outdata, best_settings = optimize_greedy(mview, width, height, replay)
+        outdata, used_settings = optimize_greedy(mview, width, height, replay)
+        best_settings = (used_settings["compr_lvl"], used_settings["mem_lvl"],
+                         used_settings["strat"], used_settings["filters"])
 
     else:
         outdata = optimize_default(mview, width, height)
@@ -96,7 +111,18 @@ def save(numpy.ndarray[numpy.uint8_t, ndim=3, mode="c"] imagedata not None,
 @cython.wraparound(False)
 cdef bytearray optimize_default(numpy.uint8_t[:,:,::1] imagedata, int width, int height):
     """
-    Create a PNG file with libpng and write it to file.
+    Create an in-memory PNG with the default libpng compression level and copy it to
+    a bytearray.
+
+    :param imagedata: A memory view of a 3-dimensional array with RGBA color
+                      values for pixels. The array is expected to be C-aligned.
+    :type imagedata: uint8_t[:,:,::1]
+    :param width: Width of the image in pixels.
+    :type width: int
+    :param height: Height of the image in pixels.
+    :type height: int
+    :returns: A bytearray containing the generated PNG file.
+    :rtype: bytearray
     """
     # Define basic image data
     cdef libpng.png_image write_image
@@ -141,11 +167,31 @@ cdef bytearray optimize_default(numpy.uint8_t[:,:,::1] imagedata, int width, int
 
     return outdata
 
+
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cdef optimize_greedy(numpy.uint8_t[:,:,::1] imagedata, int width, int height, greedy_replay_param replay):
     """
-    Reimplementation of OptiPNG.
+    Create an in-memory PNG by greedily searching for the result with the
+    smallest file size and copying it to a bytearray.
+
+    The function provides the option to run the PNG generation with a fixed set of
+    (optimal) compression parameters that were found in a previous run. In this
+    case the search for the best parameters is skipped.
+
+    :param imagedata: A memory view of a 3-dimensional array with RGBA color
+                      values for pixels. The array is expected to be C-aligned.
+    :type imagedata: uint8_t[:,:,::1]
+    :param width: Width of the image in pixels.
+    :type width: int
+    :param height: Height of the image in pixels.
+    :type height: int
+    :param replay: A struct containing compression parameters for the PNG generation. Pass
+                   a struct with all values intialized to 0xFF to run the greedy search.
+    :type replay: greedy_replay_param
+    :returns: A bytearray containing the generated PNG file as well as the
+              settings that generate the smallest PNG.
+    :rtype: tuple
     """
     if replay.compr_lvl == 0xFF:
         replay = optimize_greedy_iterate(imagedata, width, height)
@@ -185,7 +231,24 @@ cdef optimize_greedy(numpy.uint8_t[:,:,::1] imagedata, int width, int height, gr
 @cython.wraparound(False)
 cdef greedy_replay_param optimize_greedy_iterate(numpy.uint8_t[:,:,::1] imagedata, int width, int height):
     """
-    Reimplementation of OptiPNG.
+    Try several different compression settings and choose the settings
+    that generate the smallest PNG. The function tries 8 different
+    settings in total.
+
+    The algorithm is a reimplementation of a method used by OptiPNG.
+    Specifically, our function should be equivalent to the command
+
+    optipng -nx -o2 <filename>.png
+
+    :param imagedata: A memory view of a 3-dimensional array with RGBA color
+                      values for pixels. The array is expected to be C-aligned.
+    :type imagedata: uint8_t[:,:,::1]
+    :param width: Width of the image in pixels.
+    :type width: int
+    :param height: Height of the image in pixels.
+    :type height: int
+    :returns: Settings that generate the smallest PNG.
+    :rtype: greedy_replay_param
     """
     cdef int     best_filesize = 0x7fffffff
     cdef int     current_filesize = 0x7fffffff
@@ -196,7 +259,7 @@ cdef greedy_replay_param optimize_greedy_iterate(numpy.uint8_t[:,:,::1] imagedat
 
     cdef greedy_replay_param result
 
-    # Create an in-memory stream of a file
+    # Create a memory buffer that the PNG trials are written into
     cdef char *buf
     cdef size_t len
     cdef libpng.png_FILE_p fp
@@ -208,17 +271,19 @@ cdef greedy_replay_param optimize_greedy_iterate(numpy.uint8_t[:,:,::1] imagedat
         for strategy in range(GREEDY_COMPR_STRAT_MIN, GREEDY_COMPR_STRAT_MAX + 1):
             for compr_lvl in range(GREEDY_COMPR_LVL_MIN, GREEDY_COMPR_LVL_MAX + 1):
                 for mem_lvl in range(GREEDY_COMPR_MEM_LVL_MIN, GREEDY_COMPR_MEM_LVL_MAX + 1):
+                    # Create an in-memory stream of a file
                     fp = open_memstream(&buf, &len)
 
-                    # Write the file
+                    # Write the file to the memory stream
                     write_to_file(imagedata, fp, compr_lvl, mem_lvl,
                                    strategy, filters, width, height)
 
-                    # Check how big the result is
+                    # Check the size of the resulting file
                     fseek(fp, 0, SEEK_END)
                     current_filesize = ftell(fp)
 
                     if current_filesize < best_filesize:
+                        # Save the settings if we found a better result
                         best_compr_lvl = compr_lvl
                         best_compr_mem_lvl = mem_lvl
                         best_compr_strat = strategy
@@ -245,7 +310,27 @@ cdef void write_to_file(numpy.uint8_t[:,:,::1] imagedata,
                           int compression_strategy, int filters,
                           int width, int height):
     """
-    Write the PNG to a file.
+    Write an image matrix with RGBA color values to a file.
+
+    :param imagedata: A memory view of a 3-dimensional array with RGBA color
+                      values for pixels. The array is expected to be C-aligned.
+    :type imagedata: uint8_t[:,:,::1]
+    :param fp: Pointer to the file. For greedy compression trials it is recommended
+               to use an in-memory file stream created with posix.open_memstream()
+               to avoid costly I/O operations.
+    :type fp: libpng.png_FILE_p
+    :param compression_level: libpng compression level setting. (allowed: 1-9)
+    :type compression_level: int
+    :param memory_level: libpng compression memory level setting. (allowed: 1-9)
+    :type memory_level: int
+    :param compression_strategy: libpng compression strategy setting.  (allowed: 0-3)
+    :type compression_strategy: int
+    :param filters: libpng filter flags bitfield. (allowed: 0x08, 0x10, 0x20, 0x40, 0x80, 0xF8)
+    :type filters: int
+    :param width: Width of the image in pixels.
+    :type width: int
+    :param height: Height of the image in pixels.
+    :type height: int
     """
     write_ptr = libpng.png_create_write_struct(libpng.PNG_LIBPNG_VER_STRING,
                                                NULL,
@@ -276,5 +361,5 @@ cdef void write_to_file(numpy.uint8_t[:,:,::1] imagedata,
 
     libpng.png_write_end(write_ptr, write_info_ptr)
 
-    # Destroy the write struct    
+    # Destroy the write struct
     libpng.png_destroy_write_struct(&write_ptr, &write_info_ptr)
