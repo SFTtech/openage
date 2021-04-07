5cac76ffd22ee7deb2de97346eba0565e84a2f30
heinezen <heinezen@hotmail.de>
2021-04-07 23:29:38 +0200

    gamestate: Create component skeleton classes.

---
+++ libopenage/gamestate/component/internal_component.h
@@ -0,0 +1,18 @@
+// Copyright 2021-2021 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+# include "base_component.h"
+
+namespace openage::gamestate::component {
+
+/**
+ * Component that is used to track ingame information about
+ * a game entity, without having a nyan object representation.
+ */
+class InternalComponent : Component {
+
+
+};
+
+} // openage::gamestate::component
