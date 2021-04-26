e5baa36ddbbb8b531746a261dbbddf3cdee535f9
xarianthe <83218278+xarianthe@users.noreply.github.com>
2021-04-26 14:46:17 +0000

    Added a .cmd script to move .pyd files on Windows
    .cmd script should move pyd files into the correct directories on WIndows.

---
+++ movepyd.cmd
@@ -0,0 +1,14 @@
+@IF [%1] == [] echo "ERROR: Include the path to your site-packages folder as argument to this command" 
+@IF [%1] == [] exit /b 
+@cd \openage\build\openage\game\RelWithDebInfo
+@copy *.pyd %1
+@cd \openage\build\openage\cppinterface\RelWithDebInfo
+@copy *.pyd \openage\build\openage\cppinterface\
+@cd \openage\build\openage\cvar\RelWithDebInfo
+@copy *.pyd \openage\build\openage\cvar
+@cd \openage\build\openage\util\filelike\RelWithDebInfo
+@copy *.pyd \openage\build\openage\util\filelike\
+@cd \openage\build\openage\util\fslike\RelWithDebInfo
+@copy *.pyd \openage\build\openage\util\fslike\
+@echo "Successfully moved pyd files"
+@cd \openage\build
