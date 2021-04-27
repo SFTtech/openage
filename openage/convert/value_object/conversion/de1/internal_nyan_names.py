4e4733d744069240aa4068c6b0305d5696ed92d6
heinezen <heinezen@hotmail.de>
2021-04-27 02:35:06 +0200

    convert: Add DE1 specific internal nyan names.

---
+++ openage/convert/value_object/conversion/de1/internal_nyan_names.py
@@ -0,0 +1,15 @@
+# Copyright 2021-2021 the openage authors. See copying.md for legal info.
+#
+# pylint: disable=line-too-long
+
+"""
+Age of Empires games do not necessarily come with an english
+translation. Therefore, we use the strings in this file to
+figure out the names for a nyan object.
+"""
+
+# key: armor class; value: Gather ability name
+# contains only new armor classes of DE1
+ARMOR_CLASS_LOOKUPS = {
+    17: "Wall",
+}
