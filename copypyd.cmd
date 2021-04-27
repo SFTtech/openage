43d0656bbaf455d1e2d697a71e4d304730fdb8b0
xarianthe <83218278+xarianthe@users.noreply.github.com>
2021-04-27 14:11:16 +0000

    removed commands to copy files into site-packages
    copypyd.cmd won't copy files into `site-packages`

--- copypyd.cmd
+++ copypyd.cmd
@@ -1,7 +1,3 @@
-@IF [%1] == [] echo "ERROR: Include the path to your site-packages folder as argument to this command" 
-@IF [%1] == [] exit /b 
-@cd \openage\build\openage\game\RelWithDebInfo
-@copy *.pyd %1
 @cd \openage\build\openage\log\RelWithDebInfo
 @copy *.pyd \openage\build\openage\log\
 @cd \openage\build\openage\cppinterface\RelWithDebInfo
