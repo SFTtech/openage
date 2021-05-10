f278944ce047756165bd0ae267a415201eb6e9dd
xarianthe <83218278+xarianthe@users.noreply.github.com>
2021-05-10 09:34:39 +0000

    update copyright year

--- libopenage/main/tests/gamestate.cpp
+++ libopenage/main/tests/gamestate.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019-2019 the openage authors. See copying.md for legal info.
+// Copyright 2019-2021 the openage authors. See copying.md for legal info.
 #define SDL_MAIN_HANDLED
 #include "gamestate.h"
 
