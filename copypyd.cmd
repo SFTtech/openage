e810a39205b184a3a423a4f06a36c4fc7b8c9b6a
xarianthe <83218278+xarianthe@users.noreply.github.com>
2021-05-15 07:41:47 +0000

    Fix directories

--- copypyd.cmd
+++ copypyd.cmd
@@ -3,7 +3,8 @@
 @robocopy %CD%\build\openage\cvar\RelWithDebInfo %CD%\build\openage\cvar\ *.pyd
 @robocopy %CD%\build\openage\util\filelike\RelWithDebInfo %CD%\build\openage\util\filelike\ *.pyd
 @robocopy %CD%\build\openage\util\fslike\RelWithDebInfo %CD%\build\openage\util\fslike\ *.pyd
-@robocopy %CD%\build\openage\convert\value_object\read\media\RelWithDebInfo %CD%\build\convert\value_object\read\media\ *.pyd
-@robocopy %CD%\build\openage\convert\processor\export\RelWithDebInfo %CD%\build\convert\processor\export\ *.pyd
-@robocopy %CD%\build\openage\convert\service\export\png\RelWithDebInfo %CD%\build\convert\service\export\png\ *.pyd
+@robocopy %CD%\build\openage\convert\value_object\read\media\RelWithDebInfo %CD%\build\openage\convert\value_object\read\media\ *.pyd
+@robocopy %CD%\build\openage\convert\processor\export\RelWithDebInfo %CD%\build\openage\convert\processor\export\ *.pyd
+@robocopy %CD%\build\openage\convert\service\export\png\RelWithDebInfo %CD%\build\openage\convert\service\export\png\ *.pyd
+
 
