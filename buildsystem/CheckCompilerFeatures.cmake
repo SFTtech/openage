63448b67e4c6c4997cbdadeb30cf145eec8cc87e
Jonas Jelten <jj@sft.lol>
2021-08-22 23:34:47 +0200

    buildsystem: check for some c++20 features

--- buildsystem/CheckCompilerFeatures.cmake
+++ buildsystem/CheckCompilerFeatures.cmake
@@ -1,6 +1,6 @@
-# Copyright 2015-2019 the openage authors. See copying.md for legal info.
+# Copyright 2015-2021 the openage authors. See copying.md for legal info.
 
-include(CheckCXXSourceCompiles)
+include(CheckCXXSourceRuns)
 
 if(cxx_thread_local IN_LIST CMAKE_CXX_COMPILE_FEATURES)
 	set(HAVE_THREAD_LOCAL_STORAGE true)
@@ -8,30 +8,49 @@ else()
 	set(HAVE_THREAD_LOCAL_STORAGE false)
 endif()
 
-check_cxx_source_compiles("
-namespace nested::check {
-struct A {
-	int i;
+check_cxx_source_runs("
+#include <compare>
+#include <concepts>
+
+class Cat {
+public:
+	virtual int maunz() const { return 42; };
+
+	auto operator <=>(const Cat &other) const = default;
 };
-} // namespace nested::check
+
+template <typename T>
+concept Meowable = std::same_as<T, Cat> || std::derived_from<T, Cat>;
+
+template <Meowable T>
+int meow(const T &cat) {
+	return cat.maunz();
+}
+
+class HouseCat : public Cat {
+public:
+	int maunz() const override { return 1337; };
+};
+
 int main() {
-	nested::check::A a{10};
-	auto [i] = a;
-	return 10 - i;
+	HouseCat mrr{};
+	Cat rrarrr{};
+	return meow(mrr) > meow(rrarrr) ? 0 : 1;
 }
 "
-HAVE_REQUIRED_CXX17_SUPPORT
+HAVE_REQUIRED_CXX20_SUPPORT
 )
-if(NOT HAVE_REQUIRED_CXX17_SUPPORT)
+
+if(NOT HAVE_REQUIRED_CXX20_SUPPORT)
 	message("
 
-The compiler doesn't support required C++17 features:
-  * Nested namespace definition (N4230)
-  * Structured Bindings (P0217R3)
+The compiler doesn't support required C++20 features:
+  * Concepts
+  * Default comparisons
 The following versions support these features:
-  * clang++ >= 5
-  * g++ >= 7
-  * Microsoft Visual Studio 2017 >= 15.5
+  * clang++ >= 10
+  * g++ >= 10
+  * Microsoft Visual Studio 2019 >= 16.8
 Please upgrade your compiler to build openage.
 
 ")
