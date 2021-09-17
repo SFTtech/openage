17d77eaae8df8338a686b4cc80af57f42e4445da
heinezen <heinezen@hotmail.de>
2021-09-17 23:24:03 +0200

    convert: Create processors for HD edition.

---
+++ openage/convert/processor/conversion/hd/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2021-2021 the openage authors. See copying.md for legal info.
+
+"""
+Drives the conversion process for AoE2: HD Edition.
+"""
---
+++ openage/convert/processor/conversion/hd/media_subprocessor.py
@@ -0,0 +1,130 @@
+# Copyright 2021-2021 the openage authors. See copying.md for legal info.
+
+"""
+Convert media information to metadata definitions and export
+requests. Subroutine of the main HD processor.
+"""
+from ....entity_object.export.formats.sprite_metadata import LayerMode
+from ....entity_object.export.media_export_request import MediaExportRequest
+from ....entity_object.export.metadata_export import SpriteMetadataExport
+from ....value_object.read.media_types import MediaType
+
+
+class HDMediaSubprocessor:
+    """
+    Creates the exports requests for media files from HD Edition.
+    """
+
+    @classmethod
+    def convert(cls, full_data_set):
+        """
+        Create all export requests for the dataset.
+        """
+        cls.create_graphics_requests(full_data_set)
+        cls.create_sound_requests(full_data_set)
+
+    @staticmethod
+    def create_graphics_requests(full_data_set):
+        """
+        Create export requests for graphics referenced by CombinedSprite objects.
+        """
+        combined_sprites = full_data_set.combined_sprites.values()
+        handled_graphic_ids = set()
+
+        for sprite in combined_sprites:
+            ref_graphics = sprite.get_graphics()
+            graphic_targetdirs = sprite.resolve_graphics_location()
+
+            metadata_filename = f"{sprite.get_filename()}.{'sprite'}"
+            metadata_export = SpriteMetadataExport(sprite.resolve_sprite_location(),
+                                                   metadata_filename)
+            full_data_set.metadata_exports.append(metadata_export)
+
+            for graphic in ref_graphics:
+                graphic_id = graphic.get_id()
+                if graphic_id in handled_graphic_ids:
+                    continue
+
+                targetdir = graphic_targetdirs[graphic_id]
+                source_filename = f"{str(graphic['slp_id'].get_value())}.slp"
+                target_filename = "%s_%s.png" % (sprite.get_filename(),
+                                                 str(graphic["slp_id"].get_value()))
+
+                export_request = MediaExportRequest(MediaType.GRAPHICS,
+                                                    targetdir,
+                                                    source_filename,
+                                                    target_filename)
+                full_data_set.graphics_exports.update({graphic_id: export_request})
+
+                # Metadata from graphics
+                sequence_type = graphic["sequence_type"].get_value()
+                if sequence_type == 0x00:
+                    layer_mode = LayerMode.OFF
+
+                elif sequence_type & 0x08:
+                    layer_mode = LayerMode.ONCE
+
+                else:
+                    layer_mode = LayerMode.LOOP
+
+                layer_pos = graphic["layer"].get_value()
+                frame_rate = round(graphic["frame_rate"].get_value(), ndigits=6)
+                if frame_rate < 0.000001:
+                    frame_rate = None
+
+                replay_delay = round(graphic["replay_delay"].get_value(), ndigits=6)
+                if replay_delay < 0.000001:
+                    replay_delay = None
+
+                frame_count = graphic["frame_count"].get_value()
+                angle_count = graphic["angle_count"].get_value()
+                mirror_mode = graphic["mirroring_mode"].get_value()
+                metadata_export.add_graphics_metadata(target_filename,
+                                                      layer_mode,
+                                                      layer_pos,
+                                                      frame_rate,
+                                                      replay_delay,
+                                                      frame_count,
+                                                      angle_count,
+                                                      mirror_mode)
+
+                # Notify metadata export about SLP metadata when the file is exported
+                export_request.add_observer(metadata_export)
+
+                handled_graphic_ids.add(graphic_id)
+
+        combined_terrains = full_data_set.combined_terrains.values()
+        for texture in combined_terrains:
+            slp_id = texture.get_terrain()["slp_id"].get_value()
+            srcfile_prefix = texture.get_terrain()["filename"].get_value()
+
+            targetdir = texture.resolve_graphics_location()
+            source_filename = f"{str(srcfile_prefix)}_00_color.png"
+            target_filename = f"{texture.get_filename()}.png"
+
+            export_request = MediaExportRequest(MediaType.TERRAIN,
+                                                targetdir,
+                                                source_filename,
+                                                target_filename)
+            full_data_set.graphics_exports.update({slp_id: export_request})
+
+    @staticmethod
+    def create_sound_requests(full_data_set):
+        """
+        Create export requests for sounds referenced by CombinedSound objects.
+        """
+        combined_sounds = full_data_set.combined_sounds.values()
+
+        for sound in combined_sounds:
+            sound_id = sound.get_file_id()
+
+            targetdir = sound.resolve_sound_location()
+            source_filename = f"{str(sound_id)}.wav"
+            target_filename = f"{sound.get_filename()}.opus"
+
+            export_request = MediaExportRequest(MediaType.SOUNDS,
+                                                targetdir,
+                                                source_filename,
+                                                target_filename)
+
+            full_data_set.sound_exports.update({sound_id: export_request})
