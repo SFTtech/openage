f2bb58621f77c89bbe5d09bd9b198fa1409f2125
heinezen <heinezen@hotmail.de>
2022-11-03 21:29:26 +0100

    gui: Use OpenGL context during all Qt rendering operations.

--- libopenage/renderer/gui/guisys/private/gui_renderer_impl.cpp
+++ libopenage/renderer/gui/guisys/private/gui_renderer_impl.cpp
@@ -36,6 +36,12 @@ GuiRendererImpl::GuiRendererImpl(std::shared_ptr<openage::renderer::Window> wind
 	// which associates the render controller with a qwindow.
 	this->target_window = std::make_unique<QQuickWindow>(&this->render_control);
 
+	// same format as main window
+	this->target_window->setFormat(window->get_qt_window()->format());
+
+	// make everything that's not part of the GUI transparent
+	this->target_window->setColor(Qt::transparent);
+
 	auto size = window->get_qt_window()->size();
 	this->resize(size.width(), size.height());
 
@@ -43,10 +49,13 @@ GuiRendererImpl::GuiRendererImpl(std::shared_ptr<openage::renderer::Window> wind
 	// TODO: switch for vulkan support
 	this->target_window->setGraphicsDevice(QQuickGraphicsDevice::fromOpenGLContext(this->gui_context.get_ctx().get()));
 	this->render_control.initialize();
+
+	this->gui_context.post_render();
 }
 
 
 void GuiRendererImpl::set_texture(const std::shared_ptr<openage::renderer::Texture2d> &texture) {
+	this->gui_context.pre_render();
 	this->texture = texture;
 	auto tex_info = texture->get_info();
 	QSize tex_size{tex_info.get_size().first, tex_info.get_size().second};
@@ -55,6 +64,7 @@ void GuiRendererImpl::set_texture(const std::shared_ptr<openage::renderer::Textu
 	auto tex_gl = std::dynamic_pointer_cast<openage::renderer::opengl::GlTexture2d>(texture);
 	auto tex_gl_id = tex_gl->get_handle();
 	this->target_window->setRenderTarget(QQuickRenderTarget::fromOpenGLTexture(tex_gl_id, tex_size));
+	this->gui_context.post_render();
 }
 
 
@@ -79,8 +89,10 @@ GuiRendererImpl *GuiRendererImpl::impl(GuiRenderer *renderer) {
 }
 
 void GuiRendererImpl::resize(const size_t width, const size_t height) {
+	this->gui_context.pre_render();
 	this->target_window->setGeometry(0, 0, width, height);
 	emit this->resized(QSize(width, height));
+	this->gui_context.post_render();
 }
 
 
