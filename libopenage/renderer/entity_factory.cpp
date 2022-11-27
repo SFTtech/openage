4f013778063223cc25ed75e8e425051e2914dff4
heinezen <heinezen@hotmail.de>
2022-11-27 03:06:34 +0100

    renderer: Render entity factory for engine.

---
+++ libopenage/renderer/entity_factory.cpp
@@ -0,0 +1,29 @@
+// Copyright 2022-2022 the openage authors. See copying.md for legal info.
+
+#include "entity_factory.h"
+
+#include "renderer/stages/terrain/terrain_render_entity.h"
+#include "renderer/stages/terrain/terrain_renderer.h"
+#include "renderer/stages/world/world_render_entity.h"
+#include "renderer/stages/world/world_renderer.h"
+
+namespace openage::renderer {
+RenderFactory::RenderFactory(const std::shared_ptr<terrain::TerrainRenderer> &terrain_renderer,
+                             const std::shared_ptr<world::WorldRenderer> &world_renderer) :
+	terrain_renderer{terrain_renderer},
+	world_renderer{world_renderer} {
+}
+
+std::shared_ptr<terrain::TerrainRenderEntity> RenderFactory::add_terrain_render_entity() {
+	auto entity = std::make_shared<terrain::TerrainRenderEntity>();
+	this->terrain_renderer->set_render_entity(entity);
+
+	return entity;
+}
+
+std::shared_ptr<world::WorldRenderEntity> add_world_render_entity() {
+	// TODO: registration
+	return std::make_shared<world::WorldRenderEntity>();
+}
+
+} // namespace openage::renderer
---
+++ libopenage/renderer/entity_factory.h
@@ -0,0 +1,63 @@
+// Copyright 2022-2022 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <memory>
+
+namespace openage::renderer {
+class Renderer;
+
+namespace terrain {
+class TerrainRenderer;
+class TerrainRenderEntity;
+} // namespace terrain
+
+namespace world {
+class WorldRenderer;
+class WorldRenderEntity;
+} // namespace world
+
+/**
+ * Creates render entities that push animation updates from the
+ * engine to the renderer. Also registers the render entities with
+ * the corresponding render stages.
+ */
+class RenderFactory {
+public:
+	/**
+     * Create a new factory for render entities.
+     * 
+     * @param terrain_renderer Terrain renderer.
+     * @param world_renderer World renderer.
+     */
+	RenderFactory(const std::shared_ptr<terrain::TerrainRenderer> &terrain_renderer,
+	              const std::shared_ptr<world::WorldRenderer> &world_renderer);
+	~RenderFactory() = default;
+
+	/**
+     * Create a new terrain render entity and register it at the terrain renderer.
+     * 
+     * @return Render entity for pushing terrain updates.
+     */
+	std::shared_ptr<terrain::TerrainRenderEntity> add_terrain_render_entity();
+
+	/**
+     * Create a new world render entity and register it at the world renderer.
+     * 
+     * @return Render entity for pushing terrain updates.
+     */
+	std::shared_ptr<world::WorldRenderEntity> add_world_render_entity();
+
+private:
+	/**
+     * Render stage for terrain drawing.
+     */
+	std::shared_ptr<terrain::TerrainRenderer> terrain_renderer;
+
+	/**
+     * Render stage for game entity drawing.
+     */
+	std::shared_ptr<world::WorldRenderer> world_renderer;
+};
+
+} // namespace openage::renderer
