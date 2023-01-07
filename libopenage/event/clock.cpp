dc5b3240f94b489d6c77fd2219fd3cca8d4927ff
heinezen <heinezen@hotmail.de>
2023-01-07 23:43:05 +0100

    event: Simulation clock.

---
+++ libopenage/event/clock.cpp
@@ -0,0 +1,38 @@
+// Copyright 2022-2022 the openage authors. See copying.md for legal info.
+
+#include "clock.h"
+
+namespace openage::event {
+
+Clock::Clock() :
+	state{ClockState::INIT},
+	ref_time{simclock_t::now()},
+	now{dt_ms_t::zero()} {
+}
+
+curve::time_t Clock::get_time() {
+	if (state == ClockState::RUNNING) {
+		this->now = std::chrono::duration_cast<std::chrono::milliseconds>(simclock_t::now() - this->ref_time);
+	}
+
+	return this->now.count();
+}
+
+void Clock::start() {
+	this->ref_time = simclock_t::now();
+	this->now = std::chrono::duration_cast<std::chrono::milliseconds>(simclock_t::now() - this->ref_time);
+	this->state = ClockState::RUNNING;
+}
+
+void Clock::stop() {
+	this->state = ClockState::STOPPED;
+}
+
+void Clock::resume() {
+	if (this->state == ClockState::STOPPED) [[likely]] {
+		this->ref_time = simclock_t::now() - this->now;
+		this->state = ClockState::RUNNING;
+	}
+}
+
+} // namespace openage::event
---
+++ libopenage/event/clock.h
@@ -0,0 +1,75 @@
+// Copyright 2022-2022 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <chrono>
+
+#include "curve/curve.h"
+
+namespace openage::event {
+
+using simclock_t = std::chrono::steady_clock;
+using timepoint_t = std::chrono::time_point<simclock_t>;
+
+using dt_s_t = std::chrono::duration<long, std::ratio<1>>;
+using dt_ms_t = std::chrono::duration<long, std::milli>;
+
+
+enum class ClockState {
+	INIT,
+	STOPPED,
+	RUNNING
+};
+
+/**
+ * Clock for timing simulation events.
+ */
+class Clock {
+public:
+	Clock();
+	~Clock() = default;
+
+	/**
+	 * Get the current simulation time.
+	 *
+	 * This signifies the time that has passed between the start of the clock
+	 * and the current time. Time ratios are given in milliseconds.
+	 *
+	 * @return Time passed (in milliseconds).
+	 */
+	curve::time_t get_time();
+
+	/**
+	 * Start the simulation timer.
+	 */
+	void start();
+
+	/**
+	 * Stop the simulation timer.
+	 */
+	void stop();
+
+	/**
+	 * Resume the simulation timer if the clock is stopped.
+	 */
+	void resume();
+
+private:
+	/**
+     * Status of the clock (init, running, stopped, ...).
+     */
+	ClockState state;
+
+	/**
+     * Reference to an absolute point in time. \p now is calculated
+	 * by diffing \p ref_time and \p simclock_t::now().
+     */
+	timepoint_t ref_time;
+
+	/**
+     * Current time, relative to the simulation start.
+     */
+	dt_ms_t now;
+};
+
+} // namespace openage::event
