77b90714ef36f4fdf19af1e70c8753c05766daa2
heinezen <heinezen@hotmail.de>
2023-03-16 22:07:24 +0100

    input: Store basic class/key combinatio in ClassCode.

--- libopenage/input/event.cpp
+++ libopenage/input/event.cpp
@@ -12,14 +12,53 @@ int event_class_hash::operator()(const event_class &c) const {
 }
 
 
+ClassCode::ClassCode(event_class cl, code_t code) :
+	cl{cl},
+	code{code} {}
+
+
+std::vector<event_class> ClassCode::get_classes() const {
+	std::vector<event_class> result;
+
+	// use event_base to traverse up the class tree
+	event_class c = this->cl;
+	result.push_back(c);
+	while (event_class_rel.count(c) > 0) {
+		c = event_class_rel.at(c);
+		result.push_back(c);
+	}
+	return result;
+}
+
+
+bool ClassCode::operator==(const ClassCode &other) const {
+	return this->cl == other.cl && this->code == other.code;
+}
+
+
+bool ClassCode::is_subclass(const event_class &other) const {
+	for (auto cl : this->get_classes()) {
+		if (cl == other) {
+			return true;
+		}
+	}
+	return false;
+}
+
+
+int class_code_hash::operator()(const ClassCode &cc) const {
+	return std::hash<int>()(static_cast<int>(cc.cl))
+	       ^ std::hash<int>()(cc.code) * 3664657;
+}
+
+
 Event::Event(const QEvent &ev) :
 	event{std::shared_ptr<QEvent>(ev.clone())} {
 	switch (this->event->type()) {
 	case QEvent::KeyPress:
 	case QEvent::KeyRelease: {
-		this->cl = event_class::KEYBOARD;
 		auto event = dynamic_pointer_cast<QKeyEvent>(this->event);
-		this->code = event->key();
+		this->cc = ClassCode(event_class::KEYBOARD, event->key());
 		this->mod_code = event->modifiers();
 		this->state = event->type();
 	} break;
@@ -27,22 +66,20 @@ Event::Event(const QEvent &ev) :
 	case QEvent::MouseButtonPress:
 	case QEvent::MouseButtonRelease:
 	case QEvent::MouseMove: {
-		this->cl = event_class::MOUSE;
 		auto event = dynamic_pointer_cast<QMouseEvent>(this->event);
-		this->code = event->button();
+		this->cc = ClassCode(event_class::MOUSE, event->button());
 		this->mod_code = event->modifiers();
 		this->state = event->type();
 	} break;
 	case QEvent::Wheel: {
-		this->cl = event_class::WHEEL;
 		auto event = dynamic_pointer_cast<QWheelEvent>(this->event);
 		if (event->angleDelta().y() > 0) {
 			// forward
-			this->code = 1;
+			this->cc = ClassCode(event_class::WHEEL, 1);
 		}
 		else {
 			// backwards
-			this->code = -1;
+			this->cc = ClassCode(event_class::WHEEL, -1);
 		}
 		this->mod_code = event->modifiers();
 		this->state = event->type();
@@ -54,8 +91,7 @@ Event::Event(const QEvent &ev) :
 }
 
 Event::Event(event_class cl, code_t code, modset_t mod, state_t state) :
-	cl{cl},
-	code{code},
+	cc{cl, code},
 	mod_code{mod},
 	state{state},
 	event{nullptr} {}
@@ -67,8 +103,7 @@ const std::shared_ptr<QEvent> &Event::get_event() const {
 
 
 bool Event::operator==(const Event &other) const {
-	return this->cl == other.cl
-	       && this->code == other.code
+	return this->cc == other.cc
 	       && this->mod_code == other.mod_code
 	       && this->state == other.state;
 }
@@ -78,8 +113,8 @@ std::string Event::info() const {
 	// TODO: human-readable info
 
 	std::string result = "[Event: ";
-	result += "class=" + std::to_string(static_cast<int>(this->cl)) + ", ";
-	result += "code=" + std::to_string(this->code) + ", ";
+	result += "class=" + std::to_string(static_cast<int>(this->cc.cl)) + ", ";
+	result += "code=" + std::to_string(this->cc.code) + ", ";
 	result += "modset=" + std::to_string(this->mod_code) + ", ";
 	result += "state=" + std::to_string(this->state) + "]";
 	return result;
@@ -87,8 +122,7 @@ std::string Event::info() const {
 
 
 int event_hash::operator()(const Event &e) const {
-	return std::hash<int>()(static_cast<int>(e.cl))
-	       ^ std::hash<int>()(e.code) * 3664657
+	return class_code_hash()(e.cc)
 	       ^ std::hash<int>()(e.mod_code)
 	       ^ std::hash<int>()(e.state);
 }
