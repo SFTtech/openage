af54cb8045b996d5be6f6d41ec5dd3e5aaf9d6ac
heinezen <heinezen@hotmail.de>
2023-03-27 23:45:03 +0200

    camera: Use correct values for bitfields.

--- libopenage/renderer/stages/camera/manager.cpp
+++ libopenage/renderer/stages/camera/manager.cpp
@@ -10,47 +10,47 @@ namespace openage::renderer::camera {
 
 CameraManager::CameraManager(const std::shared_ptr<renderer::camera::Camera> &camera) :
 	camera{camera},
-	move_directions{static_cast<unsigned char>(MoveDirection::NONE)},
-	zoom_direction{static_cast<unsigned char>(ZoomDirection::NONE)},
+	move_directions{static_cast<int>(MoveDirection::NONE)},
+	zoom_direction{static_cast<int>(ZoomDirection::NONE)},
 	move_speed{0.5f},
 	zoom_speed{0.05f} {
 }
 
 void CameraManager::update() {
-	if (this->move_directions != static_cast<unsigned char>(MoveDirection::NONE)) {
+	if (this->move_directions != static_cast<int>(MoveDirection::NONE)) {
 		Eigen::Vector3f move_dir{0.0f, 0.0f, 0.0f};
 
-		if (this->move_directions & static_cast<unsigned char>(MoveDirection::LEFT)) {
+		if (this->move_directions & static_cast<int>(MoveDirection::LEFT)) {
 			move_dir += Eigen::Vector3f(1.0f, 0.0f, -1.0f);
 		}
-		if (this->move_directions & static_cast<unsigned char>(MoveDirection::RIGHT)) {
+		if (this->move_directions & static_cast<int>(MoveDirection::RIGHT)) {
 			move_dir += Eigen::Vector3f(-1.0f, 0.0f, 1.0f);
 		}
-		if (this->move_directions & static_cast<unsigned char>(MoveDirection::TOP)) {
+		if (this->move_directions & static_cast<int>(MoveDirection::TOP)) {
 			move_dir += Eigen::Vector3f(1.0f, 0.0f, 1.0f);
 		}
-		if (this->move_directions & static_cast<unsigned char>(MoveDirection::BOTTOM)) {
+		if (this->move_directions & static_cast<int>(MoveDirection::BOTTOM)) {
 			move_dir += Eigen::Vector3f(-1.0f, 0.0f, -1.0f);
 		}
 
 		this->camera->move_rel(move_dir, this->move_speed);
 	}
 
-	if (this->zoom_direction != static_cast<unsigned char>(ZoomDirection::NONE)) {
-		if (this->zoom_direction & static_cast<unsigned char>(ZoomDirection::IN)) {
+	if (this->zoom_direction != static_cast<int>(ZoomDirection::NONE)) {
+		if (this->zoom_direction & static_cast<int>(ZoomDirection::IN)) {
 			this->camera->zoom_in(this->zoom_speed);
 		}
-		else if (this->zoom_direction & static_cast<unsigned char>(ZoomDirection::OUT)) {
+		else if (this->zoom_direction & static_cast<int>(ZoomDirection::OUT)) {
 			this->camera->zoom_out(this->zoom_speed);
 		}
 	}
 }
 
-void CameraManager::set_move_directions(unsigned char directions) {
+void CameraManager::set_move_directions(int directions) {
 	this->move_directions = directions;
 }
 
-void CameraManager::set_zoom_direction(unsigned char direction) {
+void CameraManager::set_zoom_direction(int direction) {
 	this->zoom_direction = direction;
 }
 
