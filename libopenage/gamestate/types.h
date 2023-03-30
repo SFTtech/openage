aa720872170e0a2ead4cb117381344d38d875423
heinezen <heinezen@hotmail.de>
2023-03-30 02:21:38 +0200

    gamestate: Entity factory.

---
+++ libopenage/gamestate/types.h
@@ -0,0 +1,14 @@
+// Copyright 2023-2023 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <cstdint>
+
+namespace openage::gamestate {
+
+/**
+ * Game entity IDs.
+ */
+using entity_id_t = uint32_t;
+
+} // namespace openage::gamestate
