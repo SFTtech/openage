fd2d38392f1e3b3a88e71fa7e535ab8c66c6e833
heinezen <heinezen@hotmail.de>
2023-04-11 18:12:15 +0200

    coord: Scene coordinate type.

---
+++ libopenage/coord/scene.cpp
@@ -0,0 +1,97 @@
+// Copyright 2016-2019 the openage authors. See copying.md for legal info.
+
+#include "scene.h"
+
+#include "../util/math.h"
+#include "coordmanager.h"
+#include "pixel.h"
+#include "tile.h"
+
+
+namespace openage::coord {
+
+double scene2_delta::length() const {
+	return std::hypot(this->ne.to_double(), this->se.to_double());
+}
+
+scene2_delta scene2_delta::normalize(double length) const {
+	return *this * (length / this->length());
+}
+
+scene3_delta scene2_delta::to_scene3() const {
+	return scene3_delta{this->ne, this->se, 0};
+}
+
+phys2_delta scene2_delta::to_phys2() const {
+	return phys2_delta(this->ne, this->se);
+}
+
+double scene2::distance(scene2 other) const {
+	return (*this - other).length();
+}
+
+scene3 scene2::to_scene3(phys_t altitude) const {
+	return scene3{this->ne, this->se, altitude};
+}
+
+phys2 scene2::to_phys2() const {
+	return phys2(this->ne, this->se);
+}
+
+Eigen::Vector3f scene2::to_gl() const {
+	return Eigen::Vector3f(this->se.to_float(), 0.0f, -this->ne.to_float());
+}
+
+Eigen::Vector3f scene2::to_vulkan() const {
+	return Eigen::Vector3f(this->se.to_float(), -0.0f, -this->ne.to_float());
+}
+
+double scene3_delta::length() const {
+	return math::hypot3(this->ne.to_double(), this->se.to_double(), this->up.to_double());
+}
+
+scene3_delta scene3_delta::normalize(double length) const {
+	return *this * (length / this->length());
+}
+
+scene2_delta scene3_delta::to_scene2() const {
+	return scene2_delta(this->ne, this->se);
+}
+
+phys3_delta scene3_delta::to_phys3() const {
+	return phys3_delta(this->ne, this->se, this->up);
+}
+
+camgame_delta scene3_delta::to_camgame(const CoordManager &mgr) const {
+	// TODO
+}
+
+scene2 scene3::to_scene2() const {
+	return scene2{this->ne, this->se};
+}
+
+phys3 scene3::to_phys3() const {
+	return phys3(this->ne, this->se, this->up);
+}
+
+Eigen::Vector3f scene3::to_gl() const {
+	return Eigen::Vector3f(this->se.to_float(), this->up.to_float(), -this->ne.to_float());
+}
+
+Eigen::Vector3f scene3::to_vulkan() const {
+	return Eigen::Vector3f(this->se.to_float(), -this->up.to_float(), -this->ne.to_float());
+}
+
+camgame scene3::to_camgame(const CoordManager &mgr) const {
+	// TODO
+}
+
+viewport scene3::to_viewport(const CoordManager &mgr) const {
+	return this->to_camgame(mgr).to_viewport(mgr);
+}
+
+camhud scene3::to_camhud(const CoordManager &mgr) const {
+	return this->to_viewport(mgr).to_camhud(mgr);
+}
+
+} // namespace openage::coord
