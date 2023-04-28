7be525a3e3e5633fcb4e4d8b73dcf9473a3cd7ba
heinezen <heinezen@hotmail.de>
2023-04-28 18:19:41 +0200

    gamestate: Get position from components.
    Also hardcodes some test values.

--- libopenage/gamestate/component/internal/position.cpp
+++ libopenage/gamestate/component/internal/position.cpp
@@ -11,11 +11,21 @@ Position::Position(const std::shared_ptr<openage::event::Loop> &loop,
                    const curve::time_t &creation_time) :
 	position(loop, 0) {
 	this->position.set_insert(creation_time, initial_pos);
+
+	// ASDF: testing
+	this->position.set_insert(creation_time + 3, initial_pos + coord::phys3_delta{0, 2, 0});
+	this->position.set_insert(creation_time + 6, initial_pos + coord::phys3_delta{2, 2, 0});
+	this->position.set_insert(creation_time + 9, initial_pos + coord::phys3_delta{2, 0, 0});
+	this->position.set_insert(creation_time + 12, initial_pos);
 }
 
 
 inline component_t Position::get_component_type() const {
 	return component_t::POSITION;
+}
+
+const curve::Continuous<coord::phys3> &Position::get_position() const {
+	return this->position;
 }
 
 } // namespace openage::gamestate::component
