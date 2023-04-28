ff880a99cd8677660f4bf319d14d8de0658d8cd3
heinezen <heinezen@hotmail.de>
2023-04-28 04:04:36 +0200

    gamestate: Use component for position.

--- libopenage/gamestate/component/internal/ownership.h
+++ libopenage/gamestate/component/internal/ownership.h
@@ -1,31 +1,34 @@
-// Copyright 2021-2021 the openage authors. See copying.md for legal info.
+// Copyright 2021-2023 the openage authors. See copying.md for legal info.
 
 #pragma once
 
-#include "../../../curve/discrete.h"
-#include "../internal_component.h"
+#include "curve/discrete.h"
+#include "gamestate/component/internal_component.h"
 
 namespace openage::gamestate::component {
 
-class Ownership : InternalComponent {
-public:
-	/**
-     * ID of a game entity's owner at a given time.
-     */
-	curve::Discrete<uint64_t> owner;
+using ownership_id_t = uint64_t;
 
+class Ownership : public InternalComponent {
+public:
 	/**
 	 * Creates an Ownership component.
 	 *
 	 * @param loop Event loop that all events from the component are registered on.
-	 * @param owner_id ID of the entity's owner.
+	 * @param owner_id Initial owner ID at creation time.
 	 * @param creation_time Ingame creation time of the component.
 	 */
-	Ownership(const std::shared_ptr<event::Loop> &loop,
-	          const uint64_t owner_id,
-	          const time_t &creation_time);
+	Ownership(const std::shared_ptr<openage::event::Loop> &loop,
+	          const ownership_id_t owner_id,
+	          const curve::time_t &creation_time);
 
 	component_t get_component_type() const override;
+
+private:
+	/**
+     * Owner ID storage over time.
+     */
+	curve::Discrete<ownership_id_t> owner;
 };
 
 } // namespace openage::gamestate::component
--- libopenage/gamestate/component/internal/position.h
+++ libopenage/gamestate/component/internal/position.h
@@ -1,13 +1,35 @@
-// Copyright 2021-2021 the openage authors. See copying.md for legal info.
+// Copyright 2021-2023 the openage authors. See copying.md for legal info.
 
 #pragma once
 
-#include "../internal_component.h"
+#include <memory>
+
+#include "coord/phys.h"
+#include "curve/continuous.h"
+#include "gamestate/component/internal_component.h"
 
 namespace openage::gamestate::component {
 
-class Position : InternalComponent {
+class Position : public InternalComponent {
+public:
+	/**
+     * Create a Position component.
+     *
+     * @param loop Event loop that all events from the component are registered on.
+     * @param initial_pos Initial position at creation time.
+     * @param creation_time Ingame creation time of the component.
+     */
+	Position(const std::shared_ptr<openage::event::Loop> &loop,
+	         const coord::phys3 &initial_pos,
+	         const curve::time_t &creation_time);
+
 	component_t get_component_type() const override;
+
+private:
+	/**
+     * Entity position storage over time.
+     */
+	curve::Continuous<coord::phys3> position;
 };
 
 } // namespace openage::gamestate::component
