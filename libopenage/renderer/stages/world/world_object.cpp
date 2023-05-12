25d506e872d99e6d63eeb062ffe5e1006ddbb50c
heinezen <heinezen@hotmail.de>
2023-05-12 18:44:10 +0200

    refactor: Reorder uniform settings.

--- libopenage/renderer/stages/world/world_object.cpp
+++ libopenage/renderer/stages/world/world_object.cpp
@@ -57,70 +57,73 @@ void WorldObject::fetch_updates() {
 
 void WorldObject::update_uniforms(const curve::time_t &time) {
 	// TODO: Only update uniforms that changed since last update
-	if (this->uniforms != nullptr) [[likely]] {
-		auto current_pos = this->position.get(time);
-		auto pos_frame = this->position.frame(time).second;
-		auto pos_next_frame = this->position.next_frame(time).second;
-		auto pos_delta = pos_next_frame - pos_frame;
-		auto angle_degrees = pos_delta.to_angle();
-
-		// Frame subtexture
-		auto layer = this->animation_info->get_layer(0); // TODO: Support multiple layers
-		auto angle = layer.get_direction_angle(angle_degrees);
-
-		// Current frame index considering current time
-		auto timing = layer.get_frame_timing();
-		size_t frame_idx = timing->get_mod(time, this->render_entity->get_update_time());
-
-		// Get index of texture and subtexture where the frame's pixels are located
-		auto frame_info = angle->get_frame(frame_idx);
-		auto tex_idx = frame_info->get_texture_idx();
-		auto subtex_idx = frame_info->get_subtexture_idx();
-
-		// Get the texture info
-		auto tex_info = this->animation_info->get_texture(tex_idx);
-		auto tex_manager = this->asset_manager->get_texture_manager();
-		auto texture = tex_manager->request(tex_info->get_image_path().value());
-		this->uniforms->update("tex", texture);
-
-		// Pass the new subtexture coordinates.
-		auto coords = tex_info->get_subtex_info(subtex_idx).get_tile_params();
-		this->uniforms->update("tile_params", coords);
-
-		// scale and keep widthxheight ratio of texture
-		// when the viewport size changes
-		auto screen_size = this->camera->get_viewport_size();
-		auto subtex_size = tex_info->get_subtex_info(subtex_idx).get_size();
-		auto scale = this->animation_info->get_scalefactor() / this->camera->get_zoom();
-
-		// Transformation matrices for finding object position in clip space
-		this->uniforms->update("view", this->camera->get_view_matrix());
-		this->uniforms->update("proj", this->camera->get_projection_matrix());
-
-		// Object world position
-		this->uniforms->update("obj_world_position", current_pos.to_world_space());
-
-		// Flip subtexture horizontally if angle is mirrored
-		if (angle->is_mirrored()) {
-			this->uniforms->update("flip_x", true);
-		}
-		else {
-			this->uniforms->update("flip_x", false);
-		}
-
-		// Subtex positional offset using anchor point
-		auto anchor = tex_info->get_subtex_info(subtex_idx).get_anchor_params();
-		auto anchor_offset = Eigen::Vector2f{
-			scale * (static_cast<float>(anchor[0]) / screen_size[0]),
-			scale * (static_cast<float>(anchor[1]) / screen_size[1])};
-		this->uniforms->update("anchor_offset", anchor_offset);
-
-		// Scaling with viewport size and zoom
-		auto scale_vec = Eigen::Vector2f{
-			scale * (static_cast<float>(subtex_size[0]) / screen_size[0]),
-			scale * (static_cast<float>(subtex_size[1]) / screen_size[1])};
-		this->uniforms->update("scale", scale_vec);
+	if (this->uniforms == nullptr) [[unlikely]] {
+		return;
 	}
+
+	// Object world position
+	auto current_pos = this->position.get(time);
+	this->uniforms->update("obj_world_position", current_pos.to_world_space());
+
+	// Direction angle the object is facing towards
+	// calculated from the positional waypoints before and after the current time
+	auto pos_frame = this->position.frame(time).second;
+	auto pos_next_frame = this->position.next_frame(time).second;
+	auto pos_delta = pos_next_frame - pos_frame;
+	auto angle_degrees = pos_delta.to_angle();
+
+	// Frame subtexture
+	auto layer = this->animation_info->get_layer(0); // TODO: Support multiple layers
+	auto angle = layer.get_direction_angle(angle_degrees);
+
+	// Flip subtexture horizontally if angle is mirrored
+	if (angle->is_mirrored()) {
+		this->uniforms->update("flip_x", true);
+	}
+	else {
+		this->uniforms->update("flip_x", false);
+	}
+
+	// Current frame index considering current time
+	auto timing = layer.get_frame_timing();
+	size_t frame_idx = timing->get_mod(time, this->render_entity->get_update_time());
+
+	// Index of texture and subtexture where the frame's pixels are located
+	auto frame_info = angle->get_frame(frame_idx);
+	auto tex_idx = frame_info->get_texture_idx();
+	auto subtex_idx = frame_info->get_subtexture_idx();
+
+	auto tex_info = this->animation_info->get_texture(tex_idx);
+	auto tex_manager = this->asset_manager->get_texture_manager();
+	auto texture = tex_manager->request(tex_info->get_image_path().value());
+	this->uniforms->update("tex", texture);
+
+	// Subtexture coordinates.inside texture
+	auto coords = tex_info->get_subtex_info(subtex_idx).get_tile_params();
+	this->uniforms->update("tile_params", coords);
+
+	// scale and keep width x height ratio of texture
+	// when the viewport size changes
+	auto scale = this->animation_info->get_scalefactor() / this->camera->get_zoom();
+	auto screen_size = this->camera->get_viewport_size();
+	auto subtex_size = tex_info->get_subtex_info(subtex_idx).get_size();
+
+	// Scaling with viewport size and zoom
+	auto scale_vec = Eigen::Vector2f{
+		scale * (static_cast<float>(subtex_size[0]) / screen_size[0]),
+		scale * (static_cast<float>(subtex_size[1]) / screen_size[1])};
+	this->uniforms->update("scale", scale_vec);
+
+	// Move subtexture in scene so that its anchor point is at the object's position
+	auto anchor = tex_info->get_subtex_info(subtex_idx).get_anchor_params();
+	auto anchor_offset = Eigen::Vector2f{
+		scale * (static_cast<float>(anchor[0]) / screen_size[0]),
+		scale * (static_cast<float>(anchor[1]) / screen_size[1])};
+	this->uniforms->update("anchor_offset", anchor_offset);
+
+	// Transformation matrices from camera
+	this->uniforms->update("view", this->camera->get_view_matrix());
+	this->uniforms->update("proj", this->camera->get_projection_matrix());
 }
 
 uint32_t WorldObject::get_id() {
