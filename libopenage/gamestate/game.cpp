5fb09d50ab36c36a9e31da0ab7f62b06eec03389
heinezen <heinezen@hotmail.de>
2023-05-23 03:58:25 +0200

    gamestate: Load nyan API from modpack.

--- libopenage/gamestate/game.cpp
+++ libopenage/gamestate/game.cpp
@@ -3,6 +3,7 @@
 #include "game.h"
 
 #include <nyan/database.h>
+#include <nyan/file.h>
 
 #include "event/time_loop.h"
 #include "gamestate/game_state.h"
@@ -15,6 +16,16 @@ Game::Game(const util::Path &root_dir,
 	db{nyan::Database::create()},
 	state{std::make_shared<GameState>(this->db, event_loop)},
 	universe{std::make_shared<Universe>(root_dir, state)} {
+	auto nyan_dir = root_dir / "assets" / "converted" / "engine";
+	auto base_path = nyan_dir.get_parent().resolve_native_path();
+	for (auto f : nyan_dir.iterdir()) {
+		if (f.is_file() && f.get_suffix() == ".nyan") {
+			this->db->load(nyan_dir.get_name() + "/" + f.get_name(),
+			               [&base_path](const std::string &filename) {
+							   return std::make_shared<nyan::File>(base_path + "/" + filename);
+						   });
+		}
+	}
 }
 
 const std::shared_ptr<GameState> &Game::get_state() const {
