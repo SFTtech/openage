4fc3cbcd4e6263771fe428fc62db2713736cfe31
heinezen <heinezen@hotmail.de>
2023-05-25 18:30:06 +0200

    assets: Enumerate modpacks.

--- libopenage/assets/mod_manager.h
+++ libopenage/assets/mod_manager.h
@@ -6,6 +6,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "log/log.h"
+
 #include "assets/modpack.h"
 #include "util/path.h"
 
@@ -34,11 +36,13 @@ public:
      *
      * @param info Modpack definition.
      */
-	void register_modpacks(const ModpackInfo &info);
+	void register_modpack(const ModpackInfo &info);
 
 	/**
      * Load all modpacks with the given IDs. Modpacks must be registered
      * with \p register_modpack() before loading.
+     *
+     * TODO: Mount modpacks into virtual filesystem.
      *
      * Note that this technically only loads modpack metadata. The actual gamedata
      * is loaded by the gamestate when a game is created or, in case of media files,
@@ -49,19 +53,18 @@ public:
 	void load_modpacks(const std::vector<std::string> &load_order);
 
 	/**
-     * Set the order in which modpack data should be loaded.
+     * Get a loaded modpack by its ID.
      *
-     * This also checks whether the given load order is valid, i.e.
-     * by checking if all dependencies/conflicts are resolved.
+     * @param modpack_id ID of the modpack to get.
      *
-     * @param load_order Load order of modpacks.
+     * @return Modpack with the given ID.
      */
-	void set_load_order(const std::vector<std::string> &load_order);
+	std::shared_ptr<Modpack> get_modpack(const std::string &modpack_id) const;
 
 	/**
      * Get the load order of modpacks.
      *
-     * @return Load order of modpacks.
+     * @return Modpack IDs in the order that they should be loaded.
      */
 	const std::vector<std::string> &get_load_order() const;
 
@@ -74,23 +77,54 @@ public:
      *
      * @return Infos of the identified modpacks.
      */
-	static std::vector<ModpackInfo> enumerate_modpacks(const util::Path &directory);
+	static std::vector<ModpackInfo> enumerate_modpacks(const util::Path &directory) {
+		std::vector<ModpackInfo> result;
+
+		if (not(directory.exists() and directory.is_dir())) {
+			throw Error{MSG(err) << "Modpack directory '" << directory << "' does not exist."};
+		}
+
+		auto dir = const_cast<util::Path &>(directory);
+		for (auto entry : dir.iterdir()) {
+			if (entry.is_dir()) {
+				auto info_file = entry / "modpack.toml";
+				if (info_file.exists()) {
+					result.push_back(parse_modepack_def(info_file));
+					log::log(INFO << "Found modpack: " << result.back().id);
+				}
+			}
+		}
+
+		return result;
+	}
 
 private:
+	/**
+     * Set the order in which modpack data should be loaded.
+     *
+     * This also checks whether the given load order is valid, i.e.
+     * by checking if all dependencies/conflicts are resolved.
+     *
+     * TODO: Dynamically resolve load order?
+     *
+     * @param load_order Load order of modpacks.
+     */
+	void set_load_order(const std::vector<std::string> &load_order);
+
 	/**
      * Loaded modpacks.
      */
 	std::unordered_map<std::string, std::shared_ptr<Modpack>> loaded;
 
 	/**
-     * Load order of modpacks.
-     */
-	std::vector<std::string> load_order;
-
-	/**
      * Available modpacks (mapping modpack ID to modpack info struct).
      */
 	std::unordered_map<std::string, ModpackInfo> available;
+
+	/**
+     * Load order of modpacks.
+     */
+	std::vector<std::string> load_order;
 };
 
 } // namespace openage::assets
