cdc338f04e0c037136330a114778bb217f27ce6f
heinezen <heinezen@hotmail.de>
2023-05-29 14:56:38 +0200

    convert: Fix RoR civ graphics patching.

--- openage/convert/processor/conversion/ror/upgrade_ability_subprocessor.py
+++ openage/convert/processor/conversion/ror/upgrade_ability_subprocessor.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2022 the openage authors. See copying.md for legal info.
+# Copyright 2020-2023 the openage authors. See copying.md for legal info.
 #
 # pylint: disable=too-many-locals,too-many-lines,too-many-statements
 # pylint: disable=too-few-public-methods,too-many-branches
