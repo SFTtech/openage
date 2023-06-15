fd463f56baeaba67be4bfe8d0aefb59793209378
heinezen <heinezen@hotmail.de>
2023-06-15 20:49:45 +0200

    gamestate: Initialize entity position in spawner.

--- libopenage/gamestate/event/spawn_entity.cpp
+++ libopenage/gamestate/event/spawn_entity.cpp
@@ -4,6 +4,7 @@
 
 #include "coord/phys.h"
 #include "gamestate/component/internal/position.h"
+#include "gamestate/definitions.h"
 #include "gamestate/entity_factory.h"
 #include "gamestate/game_entity.h"
 #include "gamestate/game_state.h"
@@ -67,6 +68,34 @@ void SpawnEntityHandler::invoke(openage::event::EventLoop & /* loop */,
 	auto entity = this->factory->add_game_entity(this->loop, gstate, nyan_entity);
 
 	// Setup components
+	auto entity_pos = std::dynamic_pointer_cast<component::Position>(
+		entity->get_component(component::component_t::POSITION));
+
+	auto pos = params.get("pos", gamestate::WORLD_ORIGIN);
+	entity_pos->set_position(time, pos);
+
+	// TODO: Testing values
+	entity_pos->set_position(time + 1, pos + coord::phys3_delta{0, 1, 0});
+	entity_pos->set_position(time + 2, pos + coord::phys3_delta{1, 2, 0});
+	entity_pos->set_position(time + 3, pos + coord::phys3_delta{2, 2, 0});
+	entity_pos->set_position(time + 4, pos + coord::phys3_delta{3, 1, 0});
+	entity_pos->set_position(time + 5, pos + coord::phys3_delta{3, 0, 0});
+	entity_pos->set_position(time + 6, pos + coord::phys3_delta{2, -1, 0});
+	entity_pos->set_position(time + 7, pos + coord::phys3_delta{1, -1, 0});
+	entity_pos->set_position(time + 8, pos);
+
+	entity_pos->set_angle(time, coord::phys_angle_t::from_int(315));
+	entity_pos->set_angle(time + 1, coord::phys_angle_t::from_int(315));
+	entity_pos->set_angle(time + 2, coord::phys_angle_t::from_int(270));
+	entity_pos->set_angle(time + 3, coord::phys_angle_t::from_int(225));
+	entity_pos->set_angle(time + 4, coord::phys_angle_t::from_int(180));
+	entity_pos->set_angle(time + 5, coord::phys_angle_t::from_int(135));
+	entity_pos->set_angle(time + 6, coord::phys_angle_t::from_int(90));
+	entity_pos->set_angle(time + 7, coord::phys_angle_t::from_int(45));
+	entity_pos->set_angle(time + 8, coord::phys_angle_t::from_int(0));
+
+	auto test_pos = std::make_shared<component::Position>(this->loop, pos, time);
+
 	entity->push_to_render();
 
 	gstate->add_game_entity(entity);
