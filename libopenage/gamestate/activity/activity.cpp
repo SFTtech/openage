b7735ed42112e614b28ba5f41a26b8b69ec35f57
heinezen <heinezen@hotmail.de>
2023-06-28 00:49:28 +0200

    gamestate: Add 'Activity' encapsulation for node graph.

---
+++ libopenage/gamestate/activity/activity.cpp
@@ -0,0 +1,28 @@
+// Copyright 2023-2023 the openage authors. See copying.md for legal info.
+
+#include "activity.h"
+
+
+namespace openage::gamestate::activity {
+
+Activity::Activity(activity_id id,
+                   activity_label label,
+                   const std::shared_ptr<Node> &start) :
+	id{id},
+	label{label},
+	start{start} {
+}
+
+activity_id Activity::get_id() const {
+	return this->id;
+}
+
+const activity_label Activity::get_label() const {
+	return this->label;
+}
+
+const std::shared_ptr<Node> &Activity::get_start() const {
+	return this->start;
+}
+
+} // namespace openage::gamestate::activity
