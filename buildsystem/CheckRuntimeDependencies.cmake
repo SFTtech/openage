4c541edf40bed8b2004ba3d968e074c3be4f0252
zoli111 <acszoltan111@gmail.com>
2023-08-04 17:24:56 +0200

    Update copyright year in modified files

--- buildsystem/CheckRuntimeDependencies.cmake
+++ buildsystem/CheckRuntimeDependencies.cmake
@@ -1,4 +1,4 @@
-# Copyright 2015-2022 the openage authors. See copying.md for legal info.
+# Copyright 2015-2023 the openage authors. See copying.md for legal info.
 
 # python modules
 # a list of imported modules may be obtained via
