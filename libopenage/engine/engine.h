966a4bc4ed16e630bfbd7be8c2ba62ab76fadf62
heinezen <heinezen@hotmail.de>
2023-08-10 14:28:37 +0200

    engine: Fix import stop_token in llvm.

--- libopenage/engine/engine.h
+++ libopenage/engine/engine.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include <memory>
-#include <stop_token>
 #include <string>
 #include <thread>
 #include <vector>
@@ -23,6 +22,8 @@ public:
 };
 } // namespace std
 #endif
+#else
+#include <stop_token>
 #endif
 
 
