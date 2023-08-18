51cd91dee7124f70524098767895763539723f39
heinezen <heinezen@hotmail.de>
2023-08-18 02:48:34 +0200

    main: Exclude 'engine' from interactive modpacks.

--- libopenage/main/demo/interactive/interactive.cpp
+++ libopenage/main/demo/interactive/interactive.cpp
@@ -17,15 +17,31 @@ void demo(const util::Path &path) {
 	auto modpack_folder = path / "assets" / "converted";
 	auto mods = assets::ModManager::enumerate_modpacks(modpack_folder);
 
+	// check if the engine modpack exists
+	auto mod_engine = std::find_if(mods.begin(), mods.end(), [](const assets::ModpackInfo &mod) {
+		return mod.id == "engine";
+	});
+	if (mod_engine == std::end(mods)) {
+		throw Error{MSG(err) << "Modpack 'engine' not found.\n"
+		                     << "You need to export it first by running "
+		                     << "'python -m openage convert-export-api'"};
+	}
+	else {
+		// remove the engine modpack from the mod selection
+		// because it's not playable
+		mods.erase(mod_engine);
+	}
+
+	// check if there are is at least one playable modpack
+	if (mods.empty()) {
+		throw Error{MSG(err) << "No playable modpacks found.\n"
+		                     << "Try exporting some with "
+		                     << "'python -m openage convert'"};
+	}
+
 	// select one of the modpacks
 	std::cout << "Choose modpack to use:" << std::endl;
 	for (size_t i = 0; i < mods.size(); ++i) {
-		if (mods[i].id == "engine") {
-			// skip the internal engine modpack because
-			// it is loaded by default and not playable
-			continue;
-		}
-
 		std::cout << "(" << i << ")"
 				  << " " << mods[i].id
 				  << std::endl;
