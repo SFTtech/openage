4faf7179bbc2c3eabd94940196e38096d28f373e
heinezen <heinezen@hotmail.de>
2023-09-24 15:38:49 +0200

    renderer: Fix clang build not compiling.
    clang cannot correctly substitute for emplacement apparently.

--- libopenage/renderer/opengl/shader_data.cpp
+++ libopenage/renderer/opengl/shader_data.cpp
@@ -1,3 +1,10 @@
 // Copyright 2023-2023 the openage authors. See copying.md for legal info.
 
 #include "shader_data.h"
+
+
+namespace openage::renderer::opengl {
+
+// this file is intentionally empty
+
+} // namespace openage::renderer::opengl
