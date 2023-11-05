eaeb1f89b6edccc2e9a0a59ac20e0aa4bebeb358
heinezen <heinezen@hotmail.de>
2023-11-05 02:21:11 +0100

    renderer: Organize terrain into chunks.

--- libopenage/renderer/stages/terrain/terrain_model.h
+++ libopenage/renderer/stages/terrain/terrain_model.h
@@ -21,6 +21,7 @@ class AssetManager;
 namespace terrain {
 class TerrainRenderEntity;
 class TerrainRenderMesh;
+class TerrainChunk;
 
 /**
  * 3D model of the whole terrain. Combines the individual meshes
@@ -47,8 +48,10 @@ public:
 
 	/**
      * Fetch updates from the render entity.
+     *
+     * @param time Current simulation time.
      */
-	void fetch_updates();
+	void fetch_updates(const time::time_t &time = 0.0);
 
 	/**
      * Update the uniforms of the renderable associated with this object.
@@ -58,25 +61,17 @@ public:
 	void update_uniforms(const time::time_t &time = 0.0);
 
 	/**
-     * Get the meshes composing the terrain.
+     * Get the chunks composing the terrain.
      *
-     * @return Vector of terrain meshes.
+     * @return Chunks of the terrain.
      */
-	const std::vector<std::shared_ptr<TerrainRenderMesh>> &get_meshes() const;
+	const std::vector<std::shared_ptr<TerrainChunk>> &get_chunks() const;
 
 private:
 	/**
-	 * Create a terrain mesh from the data provided by the render entity.
-	 *
-	 * @return New terrain mesh.
+	 * Chunks composing the terrain.
 	 */
-	std::shared_ptr<TerrainRenderMesh> create_mesh();
-
-	/**
-	 * Meshes composing the terrain. Each mesh represents a drawable vertex surface
-	 * and a texture.
-	 */
-	std::vector<std::shared_ptr<TerrainRenderMesh>> meshes;
+	std::vector<std::shared_ptr<TerrainChunk>> chunks;
 
 	/**
 	 * Camera for view and projection uniforms.
@@ -87,12 +82,6 @@ private:
 	 * Asset manager for central accessing and loading textures.
      */
 	std::shared_ptr<renderer::resources::AssetManager> asset_manager;
-
-	/**
-     * Source for ingame terrain coordinates. These coordinates are translated into
-     * our render vertex mesh when \p update() is called.
-     */
-	std::shared_ptr<TerrainRenderEntity> render_entity;
 };
 
 } // namespace terrain
