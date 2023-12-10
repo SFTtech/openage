b62266d91aebc54fa4d4f65adc17e73aa512ac9b
heinezen <heinezen@hotmail.de>
2023-12-10 23:59:57 +0100

    convert: Export new API version as modpack.

--- openage/convert/tool/api_export.py
+++ openage/convert/tool/api_export.py
@@ -76,7 +76,7 @@ def create_modpack() -> Modpack:
 
     mod_def = modpack.get_info()
 
-    mod_def.set_info("engine", "0.4.0", repo="openage")
+    mod_def.set_info("engine", "0.4.1", "0.4.1", repo="openage")
 
     mod_def.add_include("**")
 
