21cffdb07a9c538da2758352e4ba4b88e6706836
heinezen <heinezen@hotmail.de>
2023-12-13 02:54:05 +0100

    gui: Remove obsolete method.

--- libopenage/renderer/gui/integration/private/gui_texture.cpp
+++ libopenage/renderer/gui/integration/private/gui_texture.cpp
@@ -39,33 +39,6 @@ bool GuiTexture::isAtlasTexture() const {
 	return openage::renderer::gui::isAtlasTexture(this->texture_handle);
 }
 
-namespace {
-// GLuint create_compatible_texture(GLuint texture_id, GLsizei w, GLsizei h) {
-// 	glBindTexture(GL_TEXTURE_2D, texture_id);
-
-// 	GLint min_filter;
-// 	GLint mag_filter;
-// 	GLint iformat;
-
-// 	glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, &min_filter);
-// 	glGetTexParameteriv(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, &mag_filter);
-// 	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_INTERNAL_FORMAT, &iformat);
-
-// 	GLuint new_texture_id;
-// 	glGenTextures(1, &new_texture_id);
-// 	glBindTexture(GL_TEXTURE_2D, new_texture_id);
-
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, min_filter);
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, mag_filter);
-
-// 	glTexImage2D(GL_TEXTURE_2D, 0, iformat, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
-
-// 	glBindTexture(GL_TEXTURE_2D, 0);
-
-// 	return new_texture_id;
-// }
-} // namespace
-
 QSGTexture *GuiTexture::removedFromAtlas(QRhiResourceUpdateBatch * /* resourceUpdates */ /* = nullptr */) const {
 	if (this->isAtlasTexture()) {
 		return this->standalone.get();
