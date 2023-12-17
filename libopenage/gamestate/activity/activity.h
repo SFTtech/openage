884a15f6c6537fa57f2cd9e9514e986afb0f2719
heinezen <heinezen@hotmail.de>
2023-12-17 09:49:15 +0100

    gamestate: Docstrings for activity.

--- libopenage/gamestate/activity/activity.h
+++ libopenage/gamestate/activity/activity.h
@@ -18,19 +18,52 @@ using activity_label = std::string;
  */
 class Activity {
 public:
+	/**
+     * Create a new activity.
+     *
+     * @param id Unique ID.
+     * @param label Human-readable label (optional).
+     * @param start Start node in the graph.
+     */
 	Activity(activity_id id,
 	         activity_label label = "",
-	         const std::shared_ptr<Node> &start = {});
+	         const std::shared_ptr<Node> &start = nullptr);
 
+	/**
+     * Get the unique ID of this activity.
+     *
+     * @return Unique ID.
+     */
 	activity_id get_id() const;
 
+	/**
+     * Get the human-readable label of this activity.
+     *
+     * @return Human-readable label.
+     */
 	const activity_label get_label() const;
 
+	/**
+     * Get the start node of this activity.
+     *
+     * @return Start node.
+     */
 	const std::shared_ptr<Node> &get_start() const;
 
 private:
+	/**
+     * Unique ID.
+     */
 	const activity_id id;
+
+	/**
+     * Human-readable label.
+     */
 	const activity_label label;
+
+	/**
+     * Start node.
+     */
 	std::shared_ptr<Node> start;
 };
 
