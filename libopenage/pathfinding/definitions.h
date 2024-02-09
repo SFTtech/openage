a6a81b800a58873731f0e9d4caab5a281f647d93
heinezen <heinezen@hotmail.de>
2024-02-09 23:09:49 +0100

    path: Flow field value types.

--- libopenage/pathfinding/definitions.h
+++ libopenage/pathfinding/definitions.h
@@ -42,4 +42,36 @@ const integrate_t INTEGRATE_START = 0;
  */
 constexpr integrate_t INTEGRATE_UNREACHABLE = std::numeric_limits<integrate_t>::max();
 
+
+/**
+ * Flow field direction types.
+ *
+ * Encoded into the flow_t values.
+ */
+enum class flow_dir_t : uint8_t {
+	NORTH = 0x00,
+	NORTH_EAST = 0x01,
+	EAST = 0x02,
+	SOUTH_EAST = 0x03,
+	SOUTH = 0x04,
+	SOUTH_WEST = 0x05,
+	WEST = 0x06,
+	NORTH_WEST = 0x07,
+};
+
+/**
+ * Mask for the flow direction bits in a flow_t value.
+ */
+constexpr flow_t FLOW_DIR_MASK = 0x0F;
+
+/**
+ * Mask for the pathable flag in a flow_t value.
+ */
+constexpr flow_t FLOW_PATHABLE = 0x10;
+
+/**
+ * Mask for the line of sight flag in a flow_t value.
+ */
+constexpr flow_t FLOW_LOS = 0x20;
+
 } // namespace openage::path
