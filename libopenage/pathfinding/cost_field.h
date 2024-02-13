e17d0529a35ab69287ce7666d8c016f02a087014
heinezen <heinezen@hotmail.de>
2024-02-13 18:08:24 +0100

    path: Convert indent in multi-line comments to tabs.

--- libopenage/pathfinding/cost_field.h
+++ libopenage/pathfinding/cost_field.h
@@ -16,57 +16,57 @@ namespace openage::path {
 class CostField {
 public:
 	/**
-     * Create a square cost field with a specified size.
-     *
-     * @param size Side length of the field.
-     */
+	 * Create a square cost field with a specified size.
+	 *
+	 * @param size Side length of the field.
+	 */
 	CostField(size_t size);
 
 	/**
-     * Get the size of the cost field.
-     *
-     * @return Size of the cost field.
-     */
+	 * Get the size of the cost field.
+	 *
+	 * @return Size of the cost field.
+	 */
 	size_t get_size() const;
 
 	/**
-     * Get the cost at a specified position.
-     *
-     * @param x X coordinate.
-     * @param y Y coordinate.
-     * @return Cost at the specified position.
-     */
+	 * Get the cost at a specified position.
+	 *
+	 * @param x X coordinate.
+	 * @param y Y coordinate.
+	 * @return Cost at the specified position.
+	 */
 	cost_t get_cost(size_t x, size_t y) const;
 
 	/**
-      * Get the cost at a specified position.
-      *
-      * @param idx Index of the cell.
-      * @return Cost at the specified position.
-      */
+	 * Get the cost at a specified position.
+	 *
+	 * @param idx Index of the cell.
+	 * @return Cost at the specified position.
+	 */
 	cost_t get_cost(size_t idx) const;
 
 	/**
-     * Set the cost at a specified position.
-     *
-     * @param x X coordinate.
-     * @param y Y coordinate.
-     * @param cost Cost to set.
-     */
+	 * Set the cost at a specified position.
+	 *
+	 * @param x X coordinate.
+	 * @param y Y coordinate.
+	 * @param cost Cost to set.
+	 */
 	void set_cost(size_t x, size_t y, cost_t cost);
 
 	/**
-     * Get the cost field values.
-     *
-     * @return Cost field values.
-     */
+	 * Get the cost field values.
+	 *
+	 * @return Cost field values.
+	 */
 	const std::vector<cost_t> &get_costs() const;
 
 	/**
-     * Set the cost field values.
-     *
-     * @param cells Cost field values.
-     */
+	 * Set the cost field values.
+	 *
+	 * @param cells Cost field values.
+	 */
 	void set_costs(std::vector<cost_t> &&cells);
 
 private:
@@ -76,8 +76,8 @@ private:
 	size_t size;
 
 	/**
-     * Cost field values.
-     */
+	 * Cost field values.
+	 */
 	std::vector<cost_t> cells;
 };
 
