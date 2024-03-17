8f9d9ca08a3802c1695e9c74da3b53a975be0cca
heinezen <heinezen@hotmail.de>
2024-03-18 00:29:18 +0100

    path: Path request class.

---
+++ libopenage/pathfinding/path.cpp
@@ -0,0 +1,10 @@
+// Copyright 2024-2024 the openage authors. See copying.md for legal info.
+
+#include "path.h"
+
+
+namespace openage::path {
+
+// this file is intentionally empty
+
+} // namespace openage::path
