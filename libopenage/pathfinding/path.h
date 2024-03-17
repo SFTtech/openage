b46c9fad20eaa9b72e5bca098fd2bc57e90029c7
heinezen <heinezen@hotmail.de>
2024-03-18 00:29:18 +0100

    path: Path request class.

---
+++ libopenage/pathfinding/path.h
@@ -0,0 +1,34 @@
+// Copyright 2024-2024 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <vector>
+
+#include "coord/tile.h"
+
+
+namespace openage::path {
+
+/**
+ * Path request for the pathfinder.
+ */
+struct PathRequest {
+	// ID of the grid to use for pathfinding.
+	size_t grid_id;
+	// Start position of the path.
+	coord::tile start;
+	// Target position of the path.
+	coord::tile target;
+};
+
+/**
+ * Path found by the pathfinder.
+ */
+struct Path {
+	// ID of the grid to used for pathfinding.
+	size_t grid_id;
+	// Waypoints of the path.
+	std::vector<coord::tile> waypoints;
+};
+
+} // namespace openage::path
