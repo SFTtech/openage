ca05032d4af69bd6a65374c9ce8a2f424d4f0de3
heinezen <heinezen@hotmail.de>
2024-03-23 17:06:52 +0100

    path: Store grid size in vector.

--- libopenage/pathfinding/grid.cpp
+++ libopenage/pathfinding/grid.cpp
@@ -9,27 +9,26 @@
 
 namespace openage::path {
 
-Grid::Grid(grid_id_t id, size_t width, size_t height, size_t sector_size) :
+Grid::Grid(grid_id_t id,
+           const util::Vector2s &size,
+           size_t sector_size) :
 	id{id},
-	width{width},
-	height{height} {
-	for (size_t y = 0; y < height; y++) {
-		for (size_t x = 0; x < width; x++) {
-			this->sectors.push_back(std::make_shared<Sector>(x + y * width, sector_size));
+	size{size} {
+	for (size_t y = 0; y < size[1]; y++) {
+		for (size_t x = 0; x < size[0]; x++) {
+			this->sectors.push_back(std::make_shared<Sector>(x + y * this->size[0], sector_size));
 		}
 	}
 }
 
 Grid::Grid(grid_id_t id,
-           size_t width,
-           size_t height,
+           const util::Vector2s &size,
            std::vector<std::shared_ptr<Sector>> &&sectors) :
 	id{id},
-	width{width},
-	height{height},
+	size{size},
 	sectors{std::move(sectors)} {
-	ENSURE(this->sectors.size() == width * height,
-	       "Grid has size " << width << "x" << height << " (" << width * height << " sectors), "
+	ENSURE(this->sectors.size() == size[0] * size[1],
+	       "Grid has size " << size[0] << "x" << size[1] << " (" << size[0] * size[1] << " sectors), "
 	                        << "but only " << this->sectors.size() << " sectors were provided");
 }
 
@@ -37,12 +36,12 @@ grid_id_t Grid::get_id() const {
 	return this->id;
 }
 
-std::pair<size_t, size_t> Grid::get_size() const {
-	return {this->width, this->height};
+const util::Vector2s &Grid::get_size() const {
+	return this->size;
 }
 
 const std::shared_ptr<Sector> &Grid::get_sector(size_t x, size_t y) {
-	return this->sectors.at(x + y * this->width);
+	return this->sectors.at(x + y * this->size[0]);
 }
 
 const std::shared_ptr<Sector> &Grid::get_sector(sector_id_t id) const {
