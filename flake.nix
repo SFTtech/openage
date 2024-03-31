1b479eb11e9d670039d36ead975f033b4edc7f86
Alessandro Re <ale@ale-re.net>
2024-03-31 16:54:09 +0200

    fix: added docs and using in-tree code

--- flake.nix
+++ flake.nix
@@ -1,4 +1,8 @@
 {
+  # This is a nix flake that contains a declarative definition of the openage
+  # and nyan packages, providing convenient and reproducible builds and
+  # development shells.
+
   description = "Free (as in freedom) open source clone of the Age of Empires II engine";
 
   inputs = {
@@ -10,12 +14,23 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; }; in
       {
-        packages = {
+        # This output is to build the derivation with `nix build` as well as to
+        # get development shells using `nix develop`.
+        # These are the packages provided by this flake: nyan and openage.
+        packages = rec {
+          # `nix build .#nyan` to build this
           nyan = pkgs.callPackage ./nix/nyan.nix { };
+          # `nix build .#openage` to build this
           openage = pkgs.callPackage ./nix/openage.nix {
+            # Nyan is not provided by nixpkgs, but it comes from this flake
             inherit (self.packages.${system}) nyan;
           };
+          # If no path is specified, openage is the default
+          default = openage;
         };
+
+        # This output is to run the application directly with `nix run`
+        # (or `nix run .#openage` if you want to be explicit)
         apps = rec {
           openage = flake-utils.lib.mkApp { drv = self.packages.${system}.openage; };
           default = openage;
