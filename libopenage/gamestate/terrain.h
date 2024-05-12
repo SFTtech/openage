7584db49829b4a2f4b8195f676c3b0c88804c8ed
heinezen <heinezen@hotmail.de>
2024-05-12 19:00:16 +0200

    gamestate: Verify terrain chunk size.

--- libopenage/gamestate/terrain.h
+++ libopenage/gamestate/terrain.h
@@ -6,7 +6,9 @@
 #include <string>
 #include <vector>
 
+#include "coord/chunk.h"
 #include "util/vector.h"
+
 
 namespace openage {
 namespace renderer {
@@ -28,6 +30,13 @@ public:
 	~Terrain() = default;
 
 	/**
+	 * Get the size of the terrain (in tiles).
+	 *
+	 * @return Terrain tile size (width x height).
+	 */
+	const util::Vector2s &get_size() const;
+
+	/**
 	 * Add a chunk to the terrain.
 	 *
 	 * @param chunk New chunk.
@@ -42,6 +51,24 @@ public:
 	const std::vector<std::shared_ptr<TerrainChunk>> &get_chunks() const;
 
 	/**
+	 * Get a specific chunk of the terrain.
+	 *
+	 * @param idx Index of the chunk.
+	 *
+	 * @return Terrain chunk.
+	 */
+	const std::shared_ptr<TerrainChunk> &get_chunk(size_t idx) const;
+
+	/**
+	 * Get a specific chunk of the terrain.
+	 *
+	 * @param pos Position of the chunk in the terrain.
+	 *
+	 * @return Terrain chunk.
+	 */
+	const std::shared_ptr<TerrainChunk> &get_chunk(const coord::chunk &pos) const;
+
+	/**
 	 * Attach a renderer which enables graphical display.
 	 *
 	 * TODO: We currently have to do attach this here too in addition to the terrain
@@ -54,16 +81,33 @@ public:
 	 */
 	void attach_renderer(const std::shared_ptr<renderer::RenderFactory> &render_factory);
 
+protected:
+	/**
+	 * Create a new terrain.
+	 *
+	 * Chunks must conform to these constraints:
+	 *  - All chunks that are not last in a row OR columns must have the same size (width x height).
+	 *  - The last chunk in a row may have a different width.
+	 *  - The last chunk in a column may have a different height.
+	 *
+	 * @param size Total size of the terrain in tiles (width x height).
+	 * @param chunks Terrain chunks.
+	 */
+	Terrain(const util::Vector2s &size,
+	        const std::vector<std::shared_ptr<TerrainChunk>> &&chunks);
+
 private:
 	/**
-	 * Total size of the map
-	 * origin is the left corner
-	 * x = top left edge; y = top right edge
+	 * Total size of the terrain in tiles (width x height).
+	 *
+	 * Origin is the top left corner (left corner with camera projection).
 	 */
 	util::Vector2s size;
 
 	/**
 	 * Subdivision of the main terrain entity.
+	 *
+	 * Ordered in rows from left to right, top to bottom.
 	 */
 	std::vector<std::shared_ptr<TerrainChunk>> chunks;
 };
