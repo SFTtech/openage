cb5a754a61fbf9be2ca90d0ddd9a81b04cdeb619
heinezen <heinezen@hotmail.de>
2024-05-12 18:59:36 +0200

    gamestate: Map class to store terrain and pathfinder.

---
+++ libopenage/gamestate/map.h
@@ -0,0 +1,74 @@
+// Copyright 2024-2024 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include <memory>
+
+#include "util/vector.h"
+
+
+namespace openage {
+namespace path {
+class Pathfinder;
+} // namespace path
+
+namespace gamestate {
+class Terrain;
+
+class Map {
+public:
+	/**
+	 * Create a new map from existing terrain.
+	 *
+	 * Initializes the pathfinder with the terrain path costs.
+	 *
+	 * @param terrain Terrain object.
+	 */
+	Map(const std::shared_ptr<Terrain> &terrain);
+
+	/**
+	 * Create a new map from existing terrain and pathfinder.
+	 *
+	 * @param terrain Terrain.
+	 * @param pathfinder Pathfinder.
+	 */
+	Map(const std::shared_ptr<Terrain> &terrain,
+	    const std::shared_ptr<path::Pathfinder> &pathfinder);
+
+	~Map() = default;
+
+	/**
+	 * Get the size of the map.
+	 *
+	 * @return Map size (width x height).
+	 */
+	const util::Vector2s &get_size() const;
+
+	/**
+	 * Get the terrain of the map.
+	 *
+	 * @return Terrain.
+	 */
+	const std::shared_ptr<Terrain> &get_terrain() const;
+
+	/**
+	 * Get the pathfinder for the map.
+	 *
+	 * @return Pathfinder.
+	 */
+	const std::shared_ptr<path::Pathfinder> &get_pathfinder() const;
+
+private:
+	/**
+	 * Terrain.
+	 */
+	std::shared_ptr<Terrain> terrain;
+
+	/**
+	 * Pathfinder.
+	 */
+	std::shared_ptr<path::Pathfinder> pathfinder;
+};
+
+} // namespace gamestate
+} // namespace openage
