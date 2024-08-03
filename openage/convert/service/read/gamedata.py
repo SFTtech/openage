b923bd52fd22f6ba60bd9362bc50acf70bf9fef2
heinezen <heinezen@hotmail.de>
2024-08-03 21:09:12 +0200

    convert: Log pickle cache filename before reading it.

--- openage/convert/service/read/gamedata.py
+++ openage/convert/service/read/gamedata.py
@@ -73,8 +73,8 @@ def load_gamespec(
                 # pickle.load() can fail in many ways, we need to catch all.
                 # pylint: disable=broad-except
                 try:
-                    gamespec = pickle.load(cachefile)
                     info("using cached wrapper: %s", cachefile_name)
+                    gamespec = pickle.load(cachefile)
                     return gamespec
                 except Exception:
                     warn("could not use cached wrapper:")
