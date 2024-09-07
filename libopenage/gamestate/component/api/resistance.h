cb16423150c32d4cdbb13e02bd0ef83039736ecf
heinezen <heinezen@hotmail.de>
2024-09-07 03:04:54 +0200

    gamestate: Add Resistance component.

---
+++ libopenage/gamestate/component/api/resistance.h
@@ -0,0 +1,18 @@
+// Copyright 2024-2024 the openage authors. See copying.md for legal info.
+
+#pragma once
+
+#include "gamestate/component/api_component.h"
+#include "gamestate/component/types.h"
+
+
+namespace openage::gamestate::component {
+
+class Resistance final : public APIComponent {
+public:
+	using APIComponent::APIComponent;
+
+	component_t get_type() const override;
+};
+
+} // namespace openage::gamestate::component
