6bf6a28d65f863bbf93e49a2aec5d4c9a42dcb40
heinezen <heinezen@hotmail.de>
2024-09-07 03:06:04 +0200

    gamestate: Add basic system skeleton for applying effects.

---
+++ libopenage/gamestate/system/apply_effect.cpp
@@ -0,0 +1,16 @@
+// Copyright 2024-2024 the openage authors. See copying.md for legal info.
+
+#include "apply_effect.h"
+
+
+namespace openage::gamestate::system {
+
+const time::time_t ApplyEffect::apply_effect(const std::shared_ptr<gamestate::GameEntity> &entity,
+                                             const std::shared_ptr<openage::gamestate::GameState> &state,
+                                             const std::shared_ptr<gamestate::GameEntity> &target,
+                                             const time::time_t &start_time) {
+	// TODO: Implement
+	return start_time;
+}
+
+} // namespace openage::gamestate::system
