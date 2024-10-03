231e57338e0bff990eb039fa522cd069021b3289
heinezen <heinezen@hotmail.de>
2024-10-03 13:58:18 +0200

    renderer: Add RenderEntity base class for common methods.

--- libopenage/renderer/stages/hud/render_entity.h
+++ libopenage/renderer/stages/hud/render_entity.h
@@ -3,12 +3,10 @@
 #pragma once
 
 #include <cstdint>
-#include <shared_mutex>
-#include <string>
 
 #include "coord/pixel.h"
 #include "curve/continuous.h"
-#include "time/time.h"
+#include "renderer/stages/render_entity.h"
 
 
 namespace openage::renderer::hud {
@@ -16,7 +14,7 @@ namespace openage::renderer::hud {
 /**
  * Render entity for pushing drag selection updates to the HUD renderer.
  */
-class HudDragRenderEntity {
+class HudDragRenderEntity final : public renderer::RenderEntity {
 public:
 	/**
 	 * Create a new render entity for drag selection in the HUD.
@@ -37,13 +35,6 @@ public:
 	            const time::time_t time = 0.0);
 
 	/**
-	 * Get the time of the last update.
-	 *
-	 * @return Time of last update.
-	 */
-	time::time_t get_update_time();
-
-	/**
 	 * Get the position of the dragged corner.
 	 *
 	 * @return Coordinates of the dragged corner.
@@ -57,31 +48,7 @@ public:
 	 */
 	const coord::input &get_drag_start();
 
-	/**
-	 * Check whether the render entity has received new updates.
-	 *
-	 * @return true if updates have been received, else false.
-	 */
-	bool is_changed();
-
-	/**
-	 * Clear the update flag by setting it to false.
-	 */
-	void clear_changed_flag();
-
 private:
-	/**
-	 * Flag for determining if the render entity has been updated by the
-	 * corresponding gamestate entity. Set to true every time \p update()
-	 * is called.
-	 */
-	bool changed;
-
-	/**
-	 * Time of the last update call.
-	 */
-	time::time_t last_update;
-
 	/**
 	 * Position of the dragged corner.
 	 */
@@ -91,10 +58,5 @@ private:
 	 * Position of the start corner.
 	 */
 	coord::input drag_start;
-
-	/**
-	 * Mutex for protecting threaded access.
-	 */
-	std::shared_mutex mutex;
 };
 } // namespace openage::renderer::hud
