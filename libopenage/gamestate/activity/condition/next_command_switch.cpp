7b3f8a580a5f18c866fc2113fa87f0c010eadbe8
heinezen <heinezen@hotmail.de>
2024-11-05 05:32:48 +0100

    gamestate: Make a lookup function for next comand switching.

---
+++ libopenage/gamestate/activity/condition/next_command_switch.cpp
@@ -0,0 +1,25 @@
+// Copyright 2024-2024 the openage authors. See copying.md for legal info.
+
+#include "next_command_switch.h"
+
+#include "gamestate/component/internal/command_queue.h"
+#include "gamestate/game_entity.h"
+
+
+namespace openage::gamestate::activity {
+
+int next_command_switch(const time::time_t &time,
+                        const std::shared_ptr<gamestate::GameEntity> &entity) {
+	auto command_queue = std::dynamic_pointer_cast<component::CommandQueue>(
+		entity->get_component(component::component_t::COMMANDQUEUE));
+
+	if (command_queue->get_queue().empty(time)) {
+		return -1;
+	}
+
+	auto command = command_queue->get_queue().front(time);
+
+	return static_cast<int>(command->get_type());
+}
+
+} // namespace openage::gamestate::activity
