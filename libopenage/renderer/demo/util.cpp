1ea8aefd79d41e5adfa0225880720bd76ce89afd
Alex Zhuohao He <zhuohao.he@outlook.com>
2024-11-29 21:52:27 -0500

    Minor code cleanup and formatting fixes

--- libopenage/renderer/demo/util.cpp
+++ libopenage/renderer/demo/util.cpp
@@ -1,4 +1,4 @@
-// Copyright 2015-2023 the openage authors. See copying.md for legal info.
+// Copyright 2015-2024 the openage authors. See copying.md for legal info.
 
 #include "util.h"
 #include "renderer/uniform_input.h"
