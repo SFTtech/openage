71cdc50ce8cfbcafbfdf898d3189cb5dffc79f0a
heinezen <heinezen@hotmail.de>
2024-12-25 14:43:43 +0100

    curve: Fix comments of element wrapper.

--- libopenage/curve/element_wrapper.h
+++ libopenage/curve/element_wrapper.h
@@ -7,45 +7,67 @@
 namespace openage::curve {
 
 /**
- * wrapper class for elements of a curve, store the insertion time and erase time of the element
- * aswell the data of the element.
+ * Wrapper for elements in a curve container.
+ *
+ * Stores the lifetime of the element (insertion time and erasure time) alongside the value.
  */
 template <typename T>
 struct element_wrapper {
-	// Insertion time of the element
+	/// Time of insertion of the element into the container
 	time::time_t _alive;
-	// Erase time of the element
+	/// Time of erasure of the element from the container
 	time::time_t _dead;
-	// Element value
+	/// Element value
 	T value;
 
 	/**
-	 * construct new element, set its start time and value
-	 * its end time will be set to time::TIME_MAX
+	 * Create a new element with insertion time \p time and a given value.
+	 *
+	 * Erasure time is set to time::TIME_MAX, i.e. the element is alive indefinitely.
+	 *
+	 * @param time Insertion time of the element.
+	 * @param value Element value.
 	 */
 	element_wrapper(const time::time_t &time, const T &value) :
 		_alive{time},
 		_dead{time::TIME_MAX},
 		value{value} {}
 
-	// construct new element, set its start time, end time and value
+	/**
+	 * Create a new element with insertion time \p alive and erasure time \p dead and a given value.
+	 *
+	 * @param alive Insertion time of the element.
+	 * @param dead Erasure time of the element.
+	 * @param value Element value.
+	 */
 	element_wrapper(const T &value, const time::time_t &alive, const time::time_t &dead) :
 		_alive{alive},
 		_dead{dead},
 		value{value} {}
 
-
-	// start time of this element
+	/**
+	 * Get the insertion time of this element.
+	 *
+	 * @return Time when the element was inserted into the container.
+	 */
 	const time::time_t &alive() const {
 		return _alive;
 	}
 
-	// end time of this element
+	/**
+	 * Get the erasure time of this element.
+	 *
+	 * @return Time when the element was erased from the container.
+	 */
 	const time::time_t &dead() const {
 		return _dead;
 	}
 
-	// set the end time of this element
+	/**
+	 * Set the erasure time of this element.
+	 *
+	 * @param time Time when the element was erased from the container.
+	 */
 	void set_dead(const time::time_t &time) {
 		_dead = time;
 	}
