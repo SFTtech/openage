ec72342fc42d0aeaf741dd488d022a0c3d0a1de2
heinezen <heinezen@hotmail.de>
2025-01-13 05:02:42 +0100

    path: Cleanup field cache code.
    - Make read-only methods const
    - Add get(..) method for getting entire field entry to avoid two lookups
    - Simplify lookup of entries
    - Add more docstrings

--- libopenage/pathfinding/field_cache.cpp
+++ libopenage/pathfinding/field_cache.cpp
@@ -4,27 +4,29 @@
 
 namespace openage::path {
 
-void FieldCache::add(cache_key_t cache_key,
-                     field_cache_t cache_entry) {
+void FieldCache::add(const cache_key_t cache_key,
+                     const field_cache_t cache_entry) {
 	this->cache[cache_key] = cache_entry;
 }
 
-void FieldCache::evict(cache_key_t cache_key) {
-	this->cache.erase(cache_key);
+bool FieldCache::evict(const cache_key_t cache_key) {
+	return this->cache.erase(cache_key) != 0;
 }
 
-bool FieldCache::is_cached(cache_key_t cache_key) {
+bool FieldCache::is_cached(const cache_key_t cache_key) const {
 	return this->cache.contains(cache_key);
 }
 
-std::shared_ptr<IntegrationField> FieldCache::get_integration_field(cache_key_t cache_key) {
-	auto cached = this->cache.find(cache_key);
-	return cached->second.first;
+std::shared_ptr<IntegrationField> FieldCache::get_integration_field(const cache_key_t cache_key) const {
+	return this->cache.at(cache_key).first;
 }
 
-std::shared_ptr<FlowField> FieldCache::get_flow_field(cache_key_t cache_key) {
-	auto cached = this->cache.find(cache_key);
-	return cached->second.second;
+std::shared_ptr<FlowField> FieldCache::get_flow_field(const cache_key_t cache_key) const {
+	return this->cache.at(cache_key).second;
+}
+
+field_cache_t FieldCache::get(const cache_key_t cache_key) const {
+	return this->cache.at(cache_key);
 }
 
 } // namespace openage::path
