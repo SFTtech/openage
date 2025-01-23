8316062824dfd064c88504f4566d222153a85d0a
dmwever <dmwever@crimson.ua.edu>
2025-01-23 17:30:50 -0600

    Update libopenage/pathfinding/cost_field.cpp
    Co-authored-by: jere8184 <jeremiahmorgan.bham@outlook.com>

--- libopenage/pathfinding/cost_field.cpp
+++ libopenage/pathfinding/cost_field.cpp
@@ -68,8 +68,7 @@ bool CostField::stamp(size_t idx, cost_t cost, const time::time_t &stamped_at) {
 }
 
 bool CostField::unstamp(size_t idx, const time::time_t &unstamped_at) {
-	if (!this->cost_stamps[idx].has_value()) return false;
-	if (unstamped_at < this->cost_stamps[idx]->stamp_time) return false;
+	if (!this->cost_stamps[idx].has_value() || unstamped_at < this->cost_stamps[idx]->stamp_time) return false;
 
 	cost_t original_cost = cost_stamps[idx]->original_cost;
 
