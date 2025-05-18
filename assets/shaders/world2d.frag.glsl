eab512c89805f5d3848a4b9eeff6385b9ea44ab1
Andreas <hell@void.lan>
2025-05-18 13:01:56 +0200

    Fix problamatic shaders
    For an unkown reason switch statements in the shaders were causing
    glLinkProgram() to crash

--- assets/shaders/world2d.frag.glsl
+++ assets/shaders/world2d.frag.glsl
@@ -19,25 +19,18 @@ vec2 uv = vec2(
 void main() {
 	vec4 tex_val = texture(tex, uv);
 	int alpha = int(round(tex_val.a * 255));
-	switch (alpha) {
-		case 0:
-			col = tex_val;
-			discard;
-
-			// do not save the ID
-			return;
-		case 254:
-			col = vec4(1.0f, 0.0f, 0.0f, 1.0f);
-			break;
-		case 252:
-			col = vec4(0.0f, 1.0f, 0.0f, 1.0f);
-			break;
-		case 250:
-			col = vec4(0.0f, 0.0f, 1.0f, 1.0f);
-			break;
-		default:
-			col = tex_val;
-			break;
+	if (alpha == 0) {
+		col = tex_val;
+		discard;
+		return;
+	} else if (alpha == 254) {
+		col = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+	} else if (alpha == 252) {
+		col = vec4(0.0f, 1.0f, 0.0f, 1.0f);
+	} else if (alpha == 250) {
+		col = vec4(0.0f, 0.0f, 1.0f, 1.0f);
+	} else {
+		col = tex_val;
 	}
 	id = u_id;
 }
