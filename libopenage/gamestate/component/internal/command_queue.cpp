730b7ab57bc3c08c90beca957c4014eb2efd938c
heinezen <heinezen@hotmail.de>
2025-05-18 02:41:36 +0200

    gamestate: Remove separate queue for command queue targets.

--- libopenage/gamestate/component/internal/command_queue.cpp
+++ libopenage/gamestate/component/internal/command_queue.cpp
@@ -4,14 +4,15 @@
 
 #include <deque>
 
+#include "gamestate/component/internal/commands/apply_effect.h"
+#include "gamestate/component/internal/commands/move.h"
 #include "gamestate/component/types.h"
 
 
 namespace openage::gamestate::component {
 
 CommandQueue::CommandQueue(const std::shared_ptr<openage::event::EventLoop> &loop) :
-	command_queue{loop, 0},
-	target{loop, 0} {
+	command_queue{loop, 0} {
 }
 
 inline component_t CommandQueue::get_type() const {
@@ -23,28 +24,46 @@ void CommandQueue::add_command(const time::time_t &time,
 	this->command_queue.insert(time, command);
 }
 
-curve::Queue<std::shared_ptr<command::Command>> &CommandQueue::get_queue() {
+const curve::Queue<std::shared_ptr<command::Command>> &CommandQueue::get_commands() {
 	return this->command_queue;
 }
 
-const std::shared_ptr<command::Command> CommandQueue::pop_command(const time::time_t &time) {
+void CommandQueue::clear(const time::time_t &time) {
+	this->command_queue.clear(time);
+}
+
+const std::shared_ptr<command::Command> CommandQueue::pop(const time::time_t &time) {
+	if (this->command_queue.empty(time)) {
+		return nullptr;
+	}
+
 	return this->command_queue.pop_front(time);
 }
 
-curve::Discrete<CommandQueue::optional_target_t> &CommandQueue::get_target() {
-	return this->target;
+const std::shared_ptr<command::Command> CommandQueue::front(const time::time_t &time) const {
+	if (this->command_queue.empty(time)) {
+		return nullptr;
+	}
+
+	return this->command_queue.front(time);
 }
 
-void CommandQueue::set_target(const time::time_t &time, const coord::phys3 &target) {
-	this->target.set_last(time, target);
-}
+CommandQueue::optional_target_t CommandQueue::get_target(const time::time_t &time) const {
+	if (this->command_queue.empty(time)) {
+		return std::nullopt;
+	}
 
-void CommandQueue::set_target(const time::time_t &time, const entity_id_t target) {
-	this->target.set_last(time, target);
-}
+	auto command = this->command_queue.front(time);
 
-void CommandQueue::clear_target(const time::time_t &time) {
-	this->target.set_last(time, std::nullopt);
+	// Extract the target from the command
+	switch (command->get_type()) {
+	case command::command_t::MOVE:
+		return std::dynamic_pointer_cast<command::Move>(command)->get_target();
+	case command::command_t::APPLY_EFFECT:
+		return std::dynamic_pointer_cast<command::ApplyEffect>(command)->get_target();
+	default:
+		return std::nullopt;
+	}
 }
 
 } // namespace openage::gamestate::component
