6fe2309efb48d9b794e8f3d02b7768c6c2b93295
heinezen <heinezen@hotmail.de>
2025-05-30 11:31:20 +0200

    convert: Refactor AoCModifierSubprocessor into separate files.

--- openage/convert/processor/conversion/aoc/modifier/__init__.py
+++ openage/convert/processor/conversion/aoc/modifier/__init__.py
@@ -1,5 +1,5 @@
 # Copyright 2025-2025 the openage authors. See copying.md for legal info.
 
 """
-Derives and adds abilities to game entities created from lines.
+Derives and adds modifiers to lines or civ groups.
 """
