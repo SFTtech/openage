d2f1d3f168f5c78e03ce6db07545e37ce6080563
heinezen <heinezen@hotmail.de>
2025-06-09 00:08:13 +0200

    convert: Refactor AoCUpgradeResourceSubprocessor into separate files.

---
+++ openage/convert/processor/conversion/aoc/upgrade_resource/reveal_ally.py
@@ -0,0 +1,34 @@
+# Copyright 2025-2025 the openage authors. See copying.md for legal info.
+
+"""
+Creates upgrade patches for the reveal ally upgrade in AoC.
+"""
+from __future__ import annotations
+import typing
+
+
+if typing.TYPE_CHECKING:
+    from .....entity_object.conversion.converter_object import ConverterObjectGroup
+    from .....value_object.conversion.forward_ref import ForwardRef
+    from ......nyan.nyan_structs import MemberOperator
+
+
+def reveal_ally_upgrade(
+    converter_group: ConverterObjectGroup,
+    value: typing.Any,
+    operator: MemberOperator,
+    team: bool = False
+) -> list[ForwardRef]:
+    """
+    Creates a patch for the reveal ally modify effect (ID: 50).
+
+    :param converter_group: Tech/Civ that gets the patch.
+    :param value: Value used for patching the member.
+    :param operator: Operator used for patching the member.
+    :returns: The forward references for the generated patches.
+    """
+    patches = []
+
+    # TODO: Implement
+
+    return patches
