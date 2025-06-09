d6a59d121816f4a3f1575c7cc1c530804c0afce4
heinezen <heinezen@hotmail.de>
2025-06-09 16:23:48 +0200

    convert: Refactor DE2Processor into separate files.

---
+++ openage/convert/processor/conversion/de2/main/extract/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2025-2025 the openage authors. See copying.md for legal info.
+
+"""
+Extract DE2 data from the game dataset and prepares it for conversion.
+"""
---
+++ openage/convert/processor/conversion/de2/main/extract/unit.py
@@ -0,0 +1,62 @@
+# Copyright 2025-2025 the openage authors. See copying.md for legal info.
+
+"""
+Extract units from the DE2 data.
+"""
+from __future__ import annotations
+import typing
+
+from ......entity_object.conversion.aoc.genie_unit import GenieUnitObject
+from ......value_object.read.value_members import ArrayMember, StorageType
+
+if typing.TYPE_CHECKING:
+    from ......entity_object.conversion.aoc.genie_object_container import GenieObjectContainer
+
+
+def extract_genie_units(gamespec: ArrayMember, full_data_set: GenieObjectContainer) -> None:
+    """
+    Extract units from the game data.
+
+    :param gamespec: Gamedata from empires.dat file.
+    """
+    # Units are stored in the civ container.
+    # All civs point to the same units (?) except for Gaia which has more.
+    # Gaia also seems to have the most units, so we only read from Gaia
+    #
+    # call hierarchy: wrapper[0]->civs[0]->units
+    raw_units = gamespec[0]["civs"][0]["units"].value
+
+    # Unit headers store the things units can do
+    raw_unit_headers = gamespec[0]["unit_headers"].value
+
+    for raw_unit in raw_units:
+        unit_id = raw_unit["id0"].value
+        unit_members = raw_unit.value
+
+        # Turn attack and armor into containers to make diffing work
+        if "attacks" in unit_members.keys():
+            attacks_member = unit_members.pop("attacks")
+            attacks_member = attacks_member.get_container("type_id")
+            armors_member = unit_members.pop("armors")
+            armors_member = armors_member.get_container("type_id")
+
+            unit_members.update({"attacks": attacks_member})
+            unit_members.update({"armors": armors_member})
+
+        unit = GenieUnitObject(unit_id, full_data_set, members=unit_members)
+        full_data_set.genie_units.update({unit.get_id(): unit})
+
+        # Commands
+        if "unit_commands" not in unit_members.keys():
+            # Only ActionUnits with type >= 40 should have commands
+            unit_type = raw_unit["unit_type"].value
+            if unit_type >= 40:
+                unit_commands = raw_unit_headers[unit_id]["unit_commands"]
+                unit.add_member(unit_commands)
+
+            else:
+                # Create empty member if no headers are present
+                unit_commands = ArrayMember("unit_commands",
+                                            StorageType.CONTAINER_MEMBER,
+                                            members=[])
+                unit.add_member(unit_commands)
