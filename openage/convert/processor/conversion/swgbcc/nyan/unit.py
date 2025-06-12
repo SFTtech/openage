5f3f4a415794a4a6b3c036d13034a73e184fd8a9
heinezen <heinezen@hotmail.de>
2025-06-12 14:49:32 +0200

    convert: Refactor SWGBCCNyanSubprocessor into separate files.

---
+++ openage/convert/processor/conversion/swgbcc/nyan/unit.py
@@ -0,0 +1,241 @@
+# Copyright 2025-2025 the openage authors. See copying.md for legal info.
+
+"""
+Convert unit lines to openage game entities.
+"""
+from __future__ import annotations
+import typing
+
+from .....entity_object.conversion.aoc.genie_unit import GenieVillagerGroup, \
+    GenieGarrisonMode, GenieMonkGroup
+from .....entity_object.conversion.converter_object import RawAPIObject
+from .....service.conversion import internal_name_lookups
+from ...aoc.ability_subprocessor import AoCAbilitySubprocessor
+from ..ability_subprocessor import SWGBCCAbilitySubprocessor
+from ..auxiliary_subprocessor import SWGBCCAuxiliarySubprocessor
+from .projectile import projectiles_from_line
+
+if typing.TYPE_CHECKING:
+    from .....entity_object.conversion.aoc.genie_unit import GenieUnitLineGroup
+
+
+def unit_line_to_game_entity(unit_line: GenieUnitLineGroup) -> None:
+    """
+    Creates raw API objects for a unit line.
+
+    :param unit_line: Unit line that gets converted to a game entity.
+    """
+    current_unit = unit_line.get_head_unit()
+    current_unit_id = unit_line.get_head_unit_id()
+
+    dataset = unit_line.data
+
+    name_lookup_dict = internal_name_lookups.get_entity_lookups(dataset.game_version)
+    class_lookup_dict = internal_name_lookups.get_class_lookups(dataset.game_version)
+
+    # Start with the generic GameEntity
+    game_entity_name = name_lookup_dict[current_unit_id][0]
+    obj_location = f"data/game_entity/generic/{name_lookup_dict[current_unit_id][1]}/"
+    raw_api_object = RawAPIObject(game_entity_name, game_entity_name,
+                                  dataset.nyan_api_objects)
+    raw_api_object.add_raw_parent("engine.util.game_entity.GameEntity")
+    raw_api_object.set_location(obj_location)
+    raw_api_object.set_filename(name_lookup_dict[current_unit_id][1])
+    unit_line.add_raw_api_object(raw_api_object)
+
+    # =======================================================================
+    # Game Entity Types
+    # =======================================================================
+    # we give a unit two types
+    #    - util.game_entity_type.types.Unit (if unit_type >= 70)
+    #    - util.game_entity_type.types.<Class> (depending on the class)
+    # =======================================================================
+    # Create or use existing auxiliary types
+    types_set = []
+    unit_type = current_unit["unit_type"].value
+
+    if unit_type >= 70:
+        type_obj = dataset.pregen_nyan_objects["util.game_entity_type.types.Unit"].get_nyan_object(
+        )
+        types_set.append(type_obj)
+
+    unit_class = current_unit["unit_class"].value
+    class_name = class_lookup_dict[unit_class]
+    class_obj_name = f"util.game_entity_type.types.{class_name}"
+    type_obj = dataset.pregen_nyan_objects[class_obj_name].get_nyan_object()
+    types_set.append(type_obj)
+
+    raw_api_object.add_raw_member("types", types_set, "engine.util.game_entity.GameEntity")
+
+    # =======================================================================
+    # Abilities
+    # =======================================================================
+    abilities_set = []
+
+    abilities_set.append(AoCAbilitySubprocessor.activity_ability(unit_line))
+    abilities_set.append(AoCAbilitySubprocessor.death_ability(unit_line))
+    abilities_set.append(AoCAbilitySubprocessor.delete_ability(unit_line))
+    abilities_set.append(AoCAbilitySubprocessor.despawn_ability(unit_line))
+    abilities_set.append(SWGBCCAbilitySubprocessor.idle_ability(unit_line))
+    abilities_set.append(SWGBCCAbilitySubprocessor.collision_ability(unit_line))
+    abilities_set.append(SWGBCCAbilitySubprocessor.live_ability(unit_line))
+    abilities_set.append(SWGBCCAbilitySubprocessor.los_ability(unit_line))
+    abilities_set.append(SWGBCCAbilitySubprocessor.move_ability(unit_line))
+    abilities_set.append(SWGBCCAbilitySubprocessor.named_ability(unit_line))
+    abilities_set.append(AoCAbilitySubprocessor.resistance_ability(unit_line))
+    abilities_set.extend(SWGBCCAbilitySubprocessor.selectable_ability(unit_line))
+    abilities_set.append(AoCAbilitySubprocessor.stop_ability(unit_line))
+    abilities_set.append(AoCAbilitySubprocessor.terrain_requirement_ability(unit_line))
+    abilities_set.append(SWGBCCAbilitySubprocessor.turn_ability(unit_line))
+    abilities_set.append(AoCAbilitySubprocessor.visibility_ability(unit_line))
+
+    # Creation
+    if len(unit_line.creates) > 0:
+        abilities_set.append(AoCAbilitySubprocessor.create_ability(unit_line))
+
+    # Config
+    ability = AoCAbilitySubprocessor.use_contingent_ability(unit_line)
+    if ability:
+        abilities_set.append(ability)
+
+    if unit_line.get_head_unit_id() in (8, 115, 180):
+        # Healing/Recharging attribute points (jedi/sith/berserk)
+        abilities_set.extend(SWGBCCAbilitySubprocessor.regenerate_attribute_ability(unit_line))
+
+    # Applying effects and shooting projectiles
+    if unit_line.is_projectile_shooter():
+        abilities_set.append(SWGBCCAbilitySubprocessor.shoot_projectile_ability(unit_line, 7))
+        projectiles_from_line(unit_line)
+
+    elif unit_line.is_melee() or unit_line.is_ranged():
+        if unit_line.has_command(7):
+            # Attack
+            abilities_set.append(
+                SWGBCCAbilitySubprocessor.apply_discrete_effect_ability(
+                    unit_line,
+                    7,
+                    unit_line.is_ranged()
+                )
+            )
+
+        if unit_line.has_command(101):
+            # Build
+            abilities_set.append(
+                SWGBCCAbilitySubprocessor.apply_continuous_effect_ability(
+                    unit_line,
+                    101,
+                    unit_line.is_ranged()
+                )
+            )
+
+        if unit_line.has_command(104):
+            # Convert
+            abilities_set.append(
+                SWGBCCAbilitySubprocessor.apply_discrete_effect_ability(
+                    unit_line,
+                    104,
+                    unit_line.is_ranged()
+                )
+            )
+
+        if unit_line.has_command(105):
+            # Heal
+            abilities_set.append(
+                SWGBCCAbilitySubprocessor.apply_continuous_effect_ability(
+                    unit_line,
+                    105,
+                    unit_line.is_ranged()
+                )
+            )
+
+        if unit_line.has_command(106):
+            # Repair
+            abilities_set.append(
+                SWGBCCAbilitySubprocessor.apply_continuous_effect_ability(
+                    unit_line,
+                    106,
+                    unit_line.is_ranged()
+                )
+            )
+
+    # Formation/Stance
+    if not isinstance(unit_line, GenieVillagerGroup):
+        abilities_set.append(AoCAbilitySubprocessor.formation_ability(unit_line))
+        abilities_set.append(AoCAbilitySubprocessor.game_entity_stance_ability(unit_line))
+
+    # Storage abilities
+    if unit_line.is_garrison():
+        abilities_set.append(AoCAbilitySubprocessor.storage_ability(unit_line))
+        abilities_set.append(AoCAbilitySubprocessor.remove_storage_ability(unit_line))
+
+        garrison_mode = unit_line.get_garrison_mode()
+
+        if garrison_mode == GenieGarrisonMode.MONK:
+            abilities_set.append(AoCAbilitySubprocessor.collect_storage_ability(unit_line))
+
+    if len(unit_line.garrison_locations) > 0:
+        ability = AoCAbilitySubprocessor.enter_container_ability(unit_line)
+        if ability:
+            abilities_set.append(ability)
+
+        ability = AoCAbilitySubprocessor.exit_container_ability(unit_line)
+        if ability:
+            abilities_set.append(ability)
+
+    if isinstance(unit_line, GenieMonkGroup):
+        abilities_set.append(AoCAbilitySubprocessor.transfer_storage_ability(unit_line))
+
+    # Resource abilities
+    if unit_line.is_gatherer():
+        abilities_set.append(AoCAbilitySubprocessor.drop_resources_ability(unit_line))
+        abilities_set.extend(SWGBCCAbilitySubprocessor.gather_ability(unit_line))
+
+    # Resource storage
+    if unit_line.is_gatherer() or unit_line.has_command(111):
+        abilities_set.append(SWGBCCAbilitySubprocessor.resource_storage_ability(unit_line))
+
+    if isinstance(unit_line, GenieVillagerGroup):
+        # Farm restocking
+        abilities_set.append(AoCAbilitySubprocessor.restock_ability(unit_line, 50))
+
+    if unit_line.is_harvestable():
+        abilities_set.append(AoCAbilitySubprocessor.harvestable_ability(unit_line))
+
+    if unit_type == 70 and unit_line.get_class_id() not in (1, 4, 5):
+        # Excludes cannons and animals
+        abilities_set.append(AoCAbilitySubprocessor.herd_ability(unit_line))
+
+    if unit_line.has_command(107):
+        abilities_set.append(AoCAbilitySubprocessor.herdable_ability(unit_line))
+
+    # Trade abilities
+    if unit_line.has_command(111):
+        abilities_set.append(SWGBCCAbilitySubprocessor.trade_ability(unit_line))
+
+    # =======================================================================
+    # TODO: Transform
+    # =======================================================================
+    raw_api_object.add_raw_member("abilities", abilities_set,
+                                  "engine.util.game_entity.GameEntity")
+
+    # =======================================================================
+    # TODO: Modifiers
+    # =======================================================================
+    modifiers_set = []
+
+    raw_api_object.add_raw_member("modifiers", modifiers_set,
+                                  "engine.util.game_entity.GameEntity")
+
+    # =======================================================================
+    # TODO: Variants
+    # =======================================================================
+    variants_set = []
+
+    raw_api_object.add_raw_member("variants", variants_set,
+                                  "engine.util.game_entity.GameEntity")
+
+    # =======================================================================
+    # Misc (Objects that are not used by the unit line itself, but use its values)
+    # =======================================================================
+    if unit_line.is_creatable():
+        SWGBCCAuxiliarySubprocessor.get_creatable_game_entity(unit_line)
